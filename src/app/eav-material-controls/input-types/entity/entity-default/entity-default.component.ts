fb712e1ccc8836df0e57f9e49459a6b65577325f
Ante Gadžo <ante@sistemi.corp>
2018-03-23 10:34:05 +0100

    entity-default changes #12

--- src/app/eav-material-controls/input-types/entity/entity-default/entity-default.component.ts
+++ src/app/eav-material-controls/input-types/entity/entity-default/entity-default.component.ts
@@ -4,6 +4,13 @@ import {
   AfterViewInit, ViewChild
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/fromEvent';
+import { map } from 'rxjs/operators';
+import { merge } from 'rxjs/observable/merge';
+import { of } from 'rxjs/observable/of';
 
 import { Field } from '../../../../eav-dynamic-form/model/field';
 import { FieldConfig } from '../../../../eav-dynamic-form/model/field-config';
@@ -11,19 +18,6 @@ import { InputType } from '../../../../eav-dynamic-form/decorators/input-type.de
 import { EavEntity, Item } from '../../../../shared/models/eav';
 import { EavType } from '../../../../shared/models/eav/eav-type';
 import { ItemService } from '../../../../shared/services/item.service';
-import { Observable } from 'rxjs/Observable';
-import { noUndefined } from '@angular/compiler/src/util';
-import { Subscription } from 'rxjs/Subscription';
-
-// import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/switchmap';
-import { map, startWith, switchMap } from 'rxjs/operators';
-import { merge } from 'rxjs/observable/merge';
-//
-import { of } from 'rxjs/observable/of';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -40,18 +34,17 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
 
   @Input() config: FieldConfig;
   group: FormGroup;
-  getEntityText1;
+
+
   chosenEntities: string[];
-  options: Item[];
+  // options: Item[];
   availableEntities: Item[] = []; // text name of entity and value is guid of entity
   selectEntities: Observable<Item[]>;
   entityTextDefault = 'Item not found'; // $translate.instant("FieldType.Entity.EntityNotFound");
-  selectedValue;
 
   // private temp$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(null);
 
   private subscriptions: Subscription[] = [];
-
 
   get allowMultiValue() {
     return this.config.settings.AllowMultiValue ? this.config.settings.AllowMultiValue.values[0].value : false;
@@ -64,9 +57,19 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
   get enableCreate() {
     return this.config.settings.EnableCreate ? this.config.settings.EnableCreate.values[0].value : false;
   }
-  // get chosenEntities() {
-  //   return this.group.controls[this.config.name].value;
-  // }
+
+  get enableEdit() {
+    return this.config.settings.EnableEdit ? this.config.settings.EnableEdit.values[0].value : false;
+  }
+
+  get enableRemove() {
+    return this.config.settings.EnableRemove ? this.config.settings.EnableRemove.values[0].value : false;
+  }
+
+  get enableDelete() {
+    return this.config.settings.EnableDelete ? this.config.settings.EnableDelete.values[0].value : false;
+  }
+
   constructor(private itemService: ItemService) {
   }
 
@@ -77,34 +80,12 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
 
   ngOnInit() {
 
-    // Init values for model if we dont have entity in model (call service to call list of entities)
-
-    // ContentType = list of content types of entites - can be only one or array - example 'Autors' from EntityType
-    // use Field Mask
-
-    // SelectedEntities - [] and availableEntities = []
-
-    // chosenEntities - list of entities from formcontrolVlaue
-
-    // this.setChosenEntities();
-
-    // this.chosenEntities$ = this.temp$.switchMap((data) => {
-    //   console.log('asdadsasdsad1', data);
-    //   return of(data);
-    // });
-    // this.temp$.next(this.group.controls[this.config.name].value);
-
-    // this.chosenEntities$.subscribe((data) => {
-    //   console.log('asdadsasdsaddddd', data);
-    // });
-
     this.setChosenEntities();
 
     // Temp
     this.subscriptions.push(
       this.itemService.selectAllItems().subscribe(items => {
-        this.availableEntities = this.options = [...items];
-
+        this.availableEntities = [...items];
       })
     );
   }
@@ -113,33 +94,17 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
   filter(val: string): Item[] {
     console.log('filtar:', val);
     if (val === '') {
-      return this.options;
+      return this.availableEntities;
     }
 
-    return this.options.filter(option =>
+    return this.availableEntities.filter(option =>
       option.entity.attributes['Name'] ?
         option.entity.attributes['Name'].values[0].value.toLowerCase().indexOf(val.toLowerCase()) === 0
         : option.entity.guid.toLowerCase().indexOf(val.toLowerCase()) === 0);
   }
 
   ngAfterViewInit() {
-
-    console.log('this.input:', this.input);
-    if (this.input) {
-
-      const eventNames = ['input', 'click'];
-
-      // Merge all observables into one single stream:
-      const eventStreams = eventNames.map((eventName) => {
-        return Observable.fromEvent(this.input.nativeElement, eventName);
-      });
-
-      const allEvents$ = merge(...eventStreams);
-
-      this.selectEntities = allEvents$
-        .map((value: any) => this.filter(value.target.value))
-        .do(value => console.log('test 1', value));
-    }
+    this.setSelectEntitiesObservables();
   }
 
   ngOnDestroy() {
@@ -149,14 +114,14 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
   optionSelected(event) {
     console.log('optionSelected:', event);
     this.addEntity(event.option.value);
+    this.input.nativeElement.value = null;
   }
-
 
   // TODO: to many detect changes ???
   getEntityText = (entityId): string => {
     // console.log('getEntityText', entityId);
     if (entityId === null) {
-      return 'empty slot'; // todo: i18n
+      return 'empty slot';
     }
     const entities = this.availableEntities.filter(f => f.entity.guid === entityId);
     if (entities.length > 0) {
@@ -170,6 +135,9 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
     return entityId;
   }
 
+  /**
+   * Determine is entityID in chosenEntities
+   */
   isInChosenEntities = (entityID): boolean => {
     if (this.chosenEntities.find(id => id === entityID)) {
       return true;
@@ -181,7 +149,7 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
   /**
    * set initial value and subscribe to form value changes
    */
-  setChosenEntities() {
+  private setChosenEntities() {
     this.chosenEntities = this.group.controls[this.config.name].value;
     this.subscriptions.push(
       this.group.valueChanges.subscribe((item) => {
@@ -203,19 +171,48 @@ export class EntityDefaultComponent implements Field, OnInit, OnDestroy, AfterVi
   //   }
   // }
 
+  /**
+   * selectEntities observe events from input autocomplete field
+   */
+  private setSelectEntitiesObservables() {
+    if (this.input) {
+      const eventNames = ['input', 'click'];
+      // Merge all observables into one single stream:
+      const eventStreams = eventNames.map((eventName) => {
+        return Observable.fromEvent(this.input.nativeElement, eventName);
+      });
+
+      const allEvents$ = merge(...eventStreams);
+
+      this.selectEntities = allEvents$
+        .map((value: any) => this.filter(value.target.value))
+        .do(value => console.log('test selectEntities', value));
+    }
+  }
+
+  /**
+   * Add entity to form
+   * @param value
+   */
   addEntity(value: string) {
     if (value) {
       // this.selectedValue = null;
       const entityValues: string[] = [...this.group.controls[this.config.name].value];
-      console.log('vrijednosti:', entityValues);
       entityValues.push(value);
       this.group.patchValue({ [this.config.name]: entityValues });
-      this.selectedValue = null;
     }
+  }
+
+  editEntity(value: string) {
+    console.log('TODO editEntity');
   }
 
   removeEntity(value: string) {
     const entityValues: string[] = [...this.group.controls[this.config.name].value];
     this.group.patchValue({ [this.config.name]: entityValues.filter(entity => entity !== value) });
+  }
+
+  deleteEntity(value: string) {
+    console.log('TODO deleteEntity');
   }
 }
