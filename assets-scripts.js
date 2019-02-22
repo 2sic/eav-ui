b28451e7262bd01a3e3db976a4c6063c22ebf429
PagnoDunadan <pagnodunadan@gmail.com>
2019-02-22 06:49:36 +0100

    Moved Angular scripts to assets/auto-generated #56

--- assets-scripts.js
+++ assets-scripts.js
@@ -4,28 +4,28 @@ const chalk = require('chalk');
 const chalkSuccess = chalk.green;
 
 (async function buildAngularScripts() {
-    await fs.copy('./node_modules/@webcomponents/custom-elements', './src/assets/custom-elements');
-    await fs.copy('./node_modules/core-js/client', './src/assets/core-js');
-    await fs.copy('./node_modules/zone.js/dist', './src/assets/zone.js');
-    await fs.copy('./node_modules/rxjs/bundles', './src/assets/rxjs');
-    await fs.copy('./node_modules/@angular/core/bundles', './src/assets/core/bundles');
-    await fs.copy('./node_modules/@angular/common/bundles', './src/assets/common/bundles');
-    await fs.copy('./node_modules/@angular/platform-browser/bundles', './src/assets/platform-browser/bundles');
-    await fs.copy('./node_modules/@angular/elements/bundles', './src/assets/elements/bundles');
+    await fs.copy('./node_modules/@webcomponents/custom-elements', './src/assets/auto-generated/custom-elements');
+    await fs.copy('./node_modules/core-js/client', './src/assets/auto-generated/core-js');
+    await fs.copy('./node_modules/zone.js/dist', './src/assets/auto-generated/zone.js');
+    await fs.copy('./node_modules/rxjs/bundles', './src/assets/auto-generated/rxjs');
+    await fs.copy('./node_modules/@angular/core/bundles', './src/assets/auto-generated/core/bundles');
+    await fs.copy('./node_modules/@angular/common/bundles', './src/assets/auto-generated/common/bundles');
+    await fs.copy('./node_modules/@angular/platform-browser/bundles', './src/assets/auto-generated/platform-browser/bundles');
+    await fs.copy('./node_modules/@angular/elements/bundles', './src/assets/auto-generated/elements/bundles');
 
     const files = [
-        './src/assets/custom-elements/src/native-shim.js',
-        './src/assets/custom-elements/custom-elements.min.js',
-        './src/assets/core-js/core.min.js',
-        './src/assets/zone.js/zone.min.js',
-        './src/assets/rxjs/rxjs.umd.min.js',
-        './src/assets/core/bundles/core.umd.min.js',
-        './src/assets/common/bundles/common.umd.min.js',
-        './src/assets/platform-browser/bundles/platform-browser.umd.min.js',
-        './src/assets/elements/bundles/elements.umd.min.js'
+        './src/assets/auto-generated/custom-elements/src/native-shim.js',
+        './src/assets/auto-generated/custom-elements/custom-elements.min.js',
+        './src/assets/auto-generated/core-js/core.min.js',
+        './src/assets/auto-generated/zone.js/zone.min.js',
+        './src/assets/auto-generated/rxjs/rxjs.umd.min.js',
+        './src/assets/auto-generated/core/bundles/core.umd.min.js',
+        './src/assets/auto-generated/common/bundles/common.umd.min.js',
+        './src/assets/auto-generated/platform-browser/bundles/platform-browser.umd.min.js',
+        './src/assets/auto-generated/elements/bundles/elements.umd.min.js'
     ];
-    await fs.ensureDir('./src/assets/elements');
-    await concat(files, './src/assets/elements/scripts-bundle.js');
+    await fs.ensureDir('./src/assets/auto-generated/elements');
+    await concat(files, './src/assets/auto-generated/elements/scripts-bundle.js');
     console.log(chalkSuccess('Build Angular scripts success!'));
 })();
 
