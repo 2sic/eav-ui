4a03721aef77a05b4c9d73281c930ace492bc845
PagnoDunadan <pagnodunadan@gmail.com>
2019-03-04 14:35:41 +0100

    Reorganized dropzone dragging into self executing function #55

--- src/assets/script/dropzone-dragging-helper.js
+++ src/assets/script/dropzone-dragging-helper.js
@@ -1,85 +1,122 @@
-// prevent drop on page - can only drop on dropzone, add eav-dragging class
+/**
+ * prevent drop on page - can only drop on dropzone, add eav-dragging class
+ */
 const dropzoneId = 'dropzone';
-const draggingClass = 'eav-dragging';
-let timeouts = [];
 window.addEventListener('dragover', function (event) {
   if (event.target.id !== dropzoneId) {
     event.preventDefault();
   }
-  timeouts.forEach(timeout => {
-    clearTimeout(timeout);
-  });
-  timeouts = [];
-  document.body.classList.add(draggingClass);
-  initDropzones();
 });
 window.addEventListener('drop', function (event) {
   if (event.target.id !== dropzoneId) {
     event.preventDefault();
   }
-  document.body.classList.remove(draggingClass);
 });
-window.addEventListener('dragleave', function (event) {
-  const timeout = setTimeout(function () {
+
+/**
+ * add draggingClass to body when something is dragged over
+ */
+const draggingClass = 'eav-dragging';
+(function addDraggingClassToBody() {
+  let timeouts = [];
+  window.addEventListener('dragover', function () {
+    timeouts.forEach(timeout => {
+      clearTimeout(timeout);
+    });
+    timeouts = [];
+    document.body.classList.add(draggingClass);
+  });
+  window.addEventListener('drop', function () {
     document.body.classList.remove(draggingClass);
-  }, 50);
-  timeouts.push(timeout);
-});
-
-let dropzones;
-let dropzoneTimeouts = [];
-let listeners = [];
-let activeDropzoneIndex;
-let previousDropzoneIndex;
-function initDropzones() {
-  if (dropzones) return;
-  dropzones = document.querySelectorAll('.dropzone');
-
-  dropzones.forEach((dropzone, index) => {
-    const addClassBind = addClass.bind(null, dropzone, index);
-    dropzone.addEventListener('dragover', addClassBind);
-    dropzone.addEventListener('dragleave', addClearDropzonesTimeout);
-    dropzone.addEventListener('drop', addClearDropzonesTimeout);
-    listeners.push({ el: dropzone, type: 'dragover', func: addClassBind });
-    listeners.push({ el: dropzone, type: 'dragleave', func: addClearDropzonesTimeout });
-    listeners.push({ el: dropzone, type: 'drop', func: addClearDropzonesTimeout });
   });
-}
-
-function addClass(dropzone, index) {
-  dropzoneTimeouts.forEach(dropzoneTimeout => {
-    clearTimeout(dropzoneTimeout);
+  window.addEventListener('dragleave', function () {
+    const timeout = setTimeout(function () {
+      document.body.classList.remove(draggingClass);
+    }, 50);
+    timeouts.push(timeout);
   });
-  dropzoneTimeouts = [];
-  dropzone.classList.add(draggingClass);
-  activeDropzoneIndex = index;
-  if (activeDropzoneIndex !== previousDropzoneIndex) {
-    clearAllClasses();
-    previousDropzoneIndex = index;
+})();
+
+/**
+ * add draggingClass to dropzone over which something is dragged
+ */
+(function addDraggingClassToDropzones() {
+  let dropzones;
+  let dropzoneTimeouts = [];
+  let windowTimeouts = [];
+  let listeners = [];
+  let activeDropzoneIndex;
+  let previousDropzoneIndex;
+
+  window.addEventListener('dragover', function () {
+    windowTimeouts.forEach(timeout => {
+      clearTimeout(timeout);
+    });
+    windowTimeouts = [];
+    initDropzones();
+  });
+  window.addEventListener('drop', function () {
+    clearAllDropzonesAndListeners();
+  });
+  window.addEventListener('dragleave', function () {
+    const timeout = setTimeout(function () {
+      clearAllDropzonesAndListeners()
+    }, 50);
+    windowTimeouts.push(timeout);
+  });
+
+  function initDropzones() {
+    if (dropzones) return;
+    dropzones = document.querySelectorAll('.dropzone');
+
+    dropzones.forEach((dropzone, index) => {
+      const addClassBind = addClass.bind(null, dropzone, index);
+      dropzone.addEventListener('dragover', addClassBind);
+      dropzone.addEventListener('dragleave', addClearDropzonesTimeout);
+      dropzone.addEventListener('drop', addClearDropzonesTimeout);
+      listeners.push({ el: dropzone, type: 'dragover', func: addClassBind });
+      listeners.push({ el: dropzone, type: 'dragleave', func: addClearDropzonesTimeout });
+      listeners.push({ el: dropzone, type: 'drop', func: addClearDropzonesTimeout });
+    });
   }
-}
 
-function addClearDropzonesTimeout() {
-  const timeout = setTimeout(clearAllDropzonesAndListeners, 50);
-  dropzoneTimeouts.push(timeout);
-}
-
-function clearAllDropzonesAndListeners() {
-  listeners.forEach(listener => {
-    const element = listener.el;
-    const type = listener.type;
-    const func = listener.func;
-    element.removeEventListener(type, func);
-  });
-  listeners = [];
-  if (dropzones) {
-    clearAllClasses();
+  function addClass(dropzone, index) {
+    windowTimeouts.forEach(timeout => {
+      clearTimeout(timeout);
+    });
+    windowTimeouts = [];
+    dropzoneTimeouts.forEach(dropzoneTimeout => {
+      clearTimeout(dropzoneTimeout);
+    });
+    dropzoneTimeouts = [];
+    activeDropzoneIndex = index;
+    if (activeDropzoneIndex !== previousDropzoneIndex) {
+      dropzones.forEach(dropzone => {
+        dropzone.classList.remove(draggingClass);
+      });
+      previousDropzoneIndex = index;
+    }
+    dropzone.classList.add(draggingClass);
   }
-  dropzones = null;
-}
 
-function clearAllClasses() {
-  dropzones.forEach(dropzone => {
-    dropzone.classList.remove(draggingClass);
-  });
-}
+  function addClearDropzonesTimeout() {
+    const timeout = setTimeout(clearAllDropzonesAndListeners, 50);
+    dropzoneTimeouts.push(timeout);
+  }
+
+  function clearAllDropzonesAndListeners() {
+    listeners.forEach(listener => {
+      const element = listener.el;
+      const type = listener.type;
+      const func = listener.func;
+      element.removeEventListener(type, func);
+    });
+    listeners = [];
+    if (dropzones) {
+      dropzones.forEach(dropzone => {
+        dropzone.classList.remove(draggingClass);
+      });
+    }
+    dropzones = null;
+  }
+})();
