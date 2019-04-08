3cb5360dc18b3e4a5cca2ddceb8d883d38310545
PagnoDunadan <pagnodunadan@gmail.com>
2019-04-08 11:36:21 +0200

    Fixed boolean default disabled check #44

--- src/app/eav-material-controls/input-types/boolean/boolean-default/boolean-default.component.ts
+++ src/app/eav-material-controls/input-types/boolean/boolean-default/boolean-default.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
-import { MatCheckbox } from '@angular/material/checkbox';
+import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { Field } from '../../../../eav-dynamic-form/model/field';
@@ -19,22 +18,9 @@ import { WrappersConstants } from '../../../../shared/constants/wrappers-constan
 export class BooleanDefaultComponent implements Field {
   config: FieldConfigSet;
   group: FormGroup;
-  @ViewChild(MatCheckbox) matCheckbox: MatCheckbox;
 
-  // constructor(private renderer?: Renderer2) {
-  //   super();
-  // }
+  get disabled() {
+    return this.group.controls[this.config.field.name].disabled;
+  }
 
-  // ngAfterViewInit() {
-  //   // const formField = (<any>this.field)['__formField__'];
-  //   // if (formField) {
-  //   //   formField._control.focusMonitor([this.matCheckbox._inputElement.nativeElement]);
-
-  //   //   // temporary fix for https://github.com/angular/material2/issues/7891
-  //   //   if (formField.underlineRef && this.renderer) {
-  //   //     this.renderer.removeClass(formField.underlineRef.nativeElement, 'mat-form-field-underline');
-  //   //   }
-  //   // }
-  //   //super.ngAfterViewInit();
-  // }
 }
