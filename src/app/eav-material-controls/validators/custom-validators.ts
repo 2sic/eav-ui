36bdf74f21608d9a051a8984c1364edec0f2130a
PagnoDunadan <pagnodunadan@gmail.com>
2019-04-17 13:47:42 +0200

    Updated decimals validator to be accept negative values #60

--- src/app/eav-material-controls/validators/custom-validators.ts
+++ src/app/eav-material-controls/validators/custom-validators.ts
@@ -20,7 +20,7 @@ export class CustomValidators {
       // first check if the control has a value
       if (control.value) {
         // match the control value against the regular expression
-        const matches = control.value.toString().match(`^[0-9]+(\.[0-9]{1,${decimals}})?$`);
+        const matches = control.value.toString().match(`^-?[0-9]+(\.[0-9]{1,${decimals}})?$`);
         // if there are not matches return an object, else return null.
         return !matches ? { decimals: true } : null;
       } else {
