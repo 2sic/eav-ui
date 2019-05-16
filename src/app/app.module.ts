988dfb47d0f9400c2d8b93f36b563462434aecbd
PagnoDunadan <pagnodunadan@gmail.com>
2019-05-16 15:09:20 +0200

    Using rvt in request headers #56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { EffectsModule } from '@ngrx/effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
 
+// import { DnnInterceptor, RuntimeSettings } from '@2sic.com/dnn-sxc-angular';
+// import { DnnSettings } from './dnn-settings/dnn-settings';
+
 import { AppComponent } from './app.component';
 import { ItemService } from './shared/services/item.service';
 import { ContentTypeService } from './shared/services/content-type.service';
@@ -83,6 +86,8 @@ export function createTranslateLoader(http: HttpClient) {
       useClass: HeaderInterceptor,
       multi: true
     },
+    // { provide: RuntimeSettings, useValue: DnnSettings },
+    // DnnInterceptor,
   ],
   bootstrap: [AppComponent],
 })
