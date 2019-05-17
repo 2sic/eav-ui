ce9c1288a9d79ccafcad5a916f5f176d702e1915
ijungleboy <daniel.mettler@2sic.com>
2019-05-17 17:46:22 +0200

    added package commends + renamed + special builds with external source-maps (not fully tested)

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,4 +1,7 @@
-module.exports = {
+const webpack = require('webpack');
+
+var configuration = {
+  // This ensures that much of the code is externalized into a separate package
     "externals": {
         "rxjs": "rxjs",
         "@angular/core": "ng.core",
@@ -7,3 +10,32 @@ module.exports = {
         "@angular/elements": "ng.elements"
     },
 }
+
+/*
+  2dm: change source map generation based on production mode
+  our goal is to not include source maps in the distribution
+  but have them when developing
+*/
+
+const isProd = process.env.NODE_ENV === 'production';
+const ifProd = x => isProd && x;
+var pjson = require('./package.json');
+
+console.log('isprod' + isProd + '; process.env...' + process.env.NODE_ENV);
+
+if(isProd) {
+  // devTool option is not needed anymore for prod
+  // but for development it's just easier to use then SourceMapDevToolPlugin
+  configuration.devtool = false;
+
+  configuration.plugins = [
+        // ... other plugins
+        ifProd(new webpack.SourceMapDevToolPlugin({
+            // this is the url of our local sourcemap server
+            publicPath: 'https://sources.2sxc.org/' + pjson.version + '/ng-edit/',
+            filename: '[file].map',
+        })),
+      ];
+}
+
+module.exports = configuration;
