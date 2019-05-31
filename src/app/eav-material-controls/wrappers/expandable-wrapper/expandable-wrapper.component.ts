d709410474b80a9c1289b10e735e93f5675cbd2a
PagnoDunadan <pagnodunadan@gmail.com>
2019-05-31 12:25:37 +0200

    Disable links in expandable wrapper #85

--- src/app/eav-material-controls/wrappers/expandable-wrapper/expandable-wrapper.component.ts
+++ src/app/eav-material-controls/wrappers/expandable-wrapper/expandable-wrapper.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, ViewContainerRef, ViewChild, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewContainerRef, ViewChild, Input, OnDestroy } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { FieldWrapper } from '../../../eav-dynamic-form/model/field-wrapper';
 import { FieldConfigSet } from '../../../eav-dynamic-form/model/field-config';
 import { ValidationMessagesService } from '../../validators/validation-messages-service';
 import { ContentExpandAnimation } from '../../../shared/animations/content-expand-animation';
+import { EavService } from '../../../shared/services/eav.service';
 
 @Component({
   selector: 'app-expandable-wrapper',
@@ -12,28 +14,50 @@ import { ContentExpandAnimation } from '../../../shared/animations/content-expan
   styleUrls: ['./expandable-wrapper.component.scss'],
   animations: [ContentExpandAnimation]
 })
-export class ExpandableWrapperComponent implements FieldWrapper, OnInit {
+export class ExpandableWrapperComponent implements FieldWrapper, OnInit, OnDestroy {
   @ViewChild('fieldComponent', { read: ViewContainerRef }) fieldComponent: ViewContainerRef;
 
   @Input() config: FieldConfigSet;
-  group: FormGroup;
+  @Input() group: FormGroup;
 
+  control: AbstractControl;
+  previousValue: string;
+  cleanedValue: string;
+  subscriptions: Subscription[] = [];
   dialogIsOpen = false;
 
-  get value() {
-    return this.group.controls[this.config.field.name].value
-      .replace('<hr sxc="sxc-content-block', '<hr class="sxc-content-block');
+  constructor(
+    private validationMessagesService: ValidationMessagesService,
+    private eavService: EavService,
+  ) { }
+
+  ngOnInit() {
+    this.control = this.group.controls[this.config.field.name];
+    this.previousValue = this.control.value;
+    this.cleanedValue = this.cleanValue(this.control.value);
+
+    this.subscriptions.push(
+      this.eavService.formSetValueChange$.subscribe(formSet => {
+        const newValue = formSet[this.config.field.name] as string;
+        if (this.previousValue === newValue) { return; }
+        this.previousValue = newValue;
+
+        this.cleanedValue = this.cleanValue(newValue);
+      })
+    );
   }
-  get id() { return `${this.config.entity.entityId}${this.config.field.index}`; }
-  get inputInvalid() { return this.group.controls[this.config.field.name].invalid; }
-  get touched() { return this.group.controls[this.config.field.name].touched || false; }
-  get disabled() { return this.group.controls[this.config.field.name].disabled; }
 
-  constructor(private validationMessagesService: ValidationMessagesService) { }
-
-  ngOnInit() { }
+  private cleanValue(value: string) {
+    return value
+      .replace('<hr sxc="sxc-content-block', '<hr class="sxc-content-block') // content block
+      .replace(/<a[^>]*>(.*?)<\/a>/g, '$1'); // remove href from A tag
+  }
 
   setTouched() {
-    this.group.controls[this.config.field.name].markAsTouched();
+    this.control.markAsTouched();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
