8d2f8ab881458ba7a9d4176f3b5292e72c7a23ca
Tonći Vatavuk <tonci@sistemi.hr>
2019-07-31 10:13:12 +0200

    paste wysiwyg ability

--- projects/field-string-wysiwyg/src/app/tinymce-wysiwyg/tinymce-wysiwyg.component.ts
+++ projects/field-string-wysiwyg/src/app/tinymce-wysiwyg/tinymce-wysiwyg.component.ts
@@ -71,7 +71,12 @@ export class TinymceWysiwygComponent implements OnInit, OnDestroy {
     };
 
     this.enableContentBlocksIfPossible(settings);
-    const tempOptions = Object.assign(selectorOptions, this.tinymceWysiwygConfig.getDefaultOptions(settings));
+    let tempOptions = Object.assign(selectorOptions, this.tinymceWysiwygConfig.getDefaultOptions(settings));
+
+    // todo: add paste wysiwyg ability feature if enabled
+    // '1b13e0e6-a346-4454-a1e6-2fb18c047d20'
+    tempOptions = Object.assign(tempOptions, this.tinymceWysiwygConfig.getPasteWysiwygAbilityOption());
+
 
     const currentLang = this.translateService.currentLang;
     this.options = this.tinymceWysiwygConfig.setLanguageOptions(currentLang, tempOptions);
