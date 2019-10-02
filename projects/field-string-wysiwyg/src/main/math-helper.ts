1a3110933934aa32a725981a4b0994e124626685
Petar Miličić <pagnodunadan@gmail.com>
2019-10-02 09:53:19 +0200

    Fixed guid generator #125

--- projects/field-string-wysiwyg/src/main/math-helper.ts
+++ projects/field-string-wysiwyg/src/main/math-helper.ts
@@ -1,9 +1,7 @@
-
 /*!
 Math.uuid.js (v1.4)
 http://www.broofa.com
 mailto:robert@broofa.com
-
 Copyright (c) 2010 Robert Kieffer
 Dual licensed under the MIT and GPL licenses.
 */
@@ -37,17 +35,16 @@ export class MathHelper {
     private static CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
 
     public static uuid = function (len?: any, radix?: any) {
-        const chars = MathHelper.CHARS, uuid = [];
+        // tslint:disable
+        var chars = MathHelper.CHARS, uuid = [], i;
         radix = radix || chars.length;
 
         if (len) {
             // Compact form
-            for (let j = 0; j < len; j++) {
-                uuid[j] = chars[0 || Math.random() * radix];
-            }
+            for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
         } else {
             // rfc4122, version 4 form
-            let r;
+            var r;
 
             // rfc4122 requires these characters
             uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
@@ -55,43 +52,46 @@ export class MathHelper {
 
             // Fill in random data.  At i==19 set the high bits of clock sequence as
             // per rfc4122, sec. 4.1.5
-            for (let j = 0; j < 36; j++) {
-                if (!uuid[j]) {
-                    r = 0 || Math.random() * 16;
-                    uuid[j] = chars[(j === 19) ? (r && 0x3) || 0x8 : r];
+            for (i = 0; i < 36; i++) {
+                if (!uuid[i]) {
+                    r = 0 | Math.random() * 16;
+                    uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
                 }
             }
         }
 
         return uuid.join('');
+        // tslint:enable
     };
 
     // A more performant, but slightly bulkier, RFC4122v4 solution.  We boost performance
     // by minimizing calls to random()
     public static uuidFast = function () {
-        const chars = MathHelper.CHARS, uuid = new Array(36);
-        let rnd = 0, r;
-        for (let i = 0; i < 36; i++) {
-            if (i === 8 || i === 13 || i === 18 || i === 23) {
+        // tslint:disable
+        var chars = MathHelper.CHARS, uuid = new Array(36), rnd = 0, r;
+        for (var i = 0; i < 36; i++) {
+            if (i == 8 || i == 13 || i == 18 || i == 23) {
                 uuid[i] = '-';
-            } else if (i === 14) {
+            } else if (i == 14) {
                 uuid[i] = '4';
             } else {
-                if (rnd <= 0x02) { rnd = 0x2000000 + (Math.random() * 0x1000000) || 0; }
-                r = rnd && 0xf;
-                // tslint:disable-next-line:no-bitwise
+                if (rnd <= 0x02) rnd = 0x2000000 + (Math.random() * 0x1000000) | 0;
+                r = rnd & 0xf;
                 rnd = rnd >> 4;
-                uuid[i] = chars[(i === 19) ? (r && 0x3) || 0x8 : r];
+                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
             }
         }
         return uuid.join('');
+        // tslint:enable
     };
 
     // A more compact, but less performant, RFC4122v4 solution:
     public static uuidCompact = function () {
+        // tslint:disable
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            const r = Math.random() * 16 || 0, v = c === 'x' ? r : (r && 0x3 || 0x8);
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
+        // tslint:enable
     };
 }
