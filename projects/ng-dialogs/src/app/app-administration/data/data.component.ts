ffbd812ed67f9af5d86cb719cdaaff4e24d8bbf4
Petar Miličić <pagnodunadan@gmail.com>
2019-11-29 10:06:56 +0100

    Fetch content types in app administration data #134

--- projects/ng-dialogs/src/app/app-administration/data/data.component.ts
+++ projects/ng-dialogs/src/app/app-administration/data/data.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Context } from '../../shared/context/context';
+import { ContentType } from '../shared/models/content-types.model';
 
 @Component({
   selector: 'app-data',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.component.scss']
 })
 export class DataComponent implements OnInit {
+  @Input() context: Context;
+  private scope = '2SexyContent'; // spm figure out how scope works
+  contentTypes: ContentType[];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.http.get(`/desktopmodules/2sxc/api/eav/contenttype/get/?appId=${this.context.appId}&scope=${this.scope}`)
+      .subscribe((contentTypes: ContentType[]) => {
+        this.contentTypes = contentTypes;
+      });
   }
 
 }
