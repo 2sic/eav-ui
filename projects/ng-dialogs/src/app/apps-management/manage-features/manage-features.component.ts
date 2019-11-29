a8fbb023bdf717ebfb8720aaefc4cc6e5de80819
Petar Miličić <pagnodunadan@gmail.com>
2019-11-29 11:36:18 +0100

    Fetch features in apps management #134

--- projects/ng-dialogs/src/app/apps-management/manage-features/manage-features.component.ts
+++ projects/ng-dialogs/src/app/apps-management/manage-features/manage-features.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Feature } from '../shared/feature.model';
 
 @Component({
   selector: 'app-manage-features',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-features.component.scss']
 })
 export class ManageFeaturesComponent implements OnInit {
+  features: Feature[];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.http.get(`/desktopmodules/2sxc/api/app-sys/system/features`)
+      .subscribe((features: Feature[]) => {
+        features.forEach(feature => {
+          feature.expires = new Date(feature.expires);
+        });
+        this.features = features;
+      });
   }
 
 }
---
+++ projects/ng-dialogs/src/app/apps-management/shared/feature.model.ts
@@ -0,0 +1,7 @@
+export class Feature {
+  enabled: boolean;
+  expires: string | Date;
+  id: string;
+  public: boolean;
+  ui: boolean;
+}
