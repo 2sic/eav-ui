887be90bb6baa5ad87fc12e427ed203c40bafc7d
Petar Miličić <pagnodunadan@gmail.com>
2020-01-13 13:51:23 +0100

    Fixed context scope for apps management and app administration #134

--- projects/ng-dialogs/src/app/app-administration/shared/services/eav-configuration.service.ts
+++ projects/ng-dialogs/src/app/app-administration/shared/services/eav-configuration.service.ts
@@ -10,7 +10,14 @@ export class EavConfigurationService {
   metadataOfCmsObject = 10;
 
   contentType = {
-    defaultScope: '2SexyContent'
+    // this is the main schema and the data you usually see is from here
+    defaultScope: '2SexyContent',
+    // this contains content-types for configuration, settings and resources of the app
+    app: '2SexyContent-App',
+    // this contains view-definitions, content-types etc.
+    cmsSystem: '2SexyContent-System',
+    // this contains core EAV data like input-field configurations and similar
+    system: 'System',
   };
 
   constructor() { }
