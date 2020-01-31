9e7efba31b440130c0d0a54a741798cbb5047015
Petar Miličić <pagnodunadan@gmail.com>
2020-01-31 11:11:24 +0100

    Cleared package.json and adjusted build scripts #168

--- build-helpers/build-html.js
+++ build-helpers/build-html.js
@@ -4,20 +4,8 @@ const chalk = require('chalk');
 const chalkSuccess = chalk.green;
 
 function buildHtml(name) {
-  let htmlSourcePath = './projects/edit/src/' + name + '.html';
+  let htmlSourcePath = './projects/ng-dialogs/src/' + name + '.html';
   let htmlOutputPath = './dist/' + name + '.html';
-
-  let ngDialogs = false;
-  const args = process.argv.slice(2);
-  args.forEach((val, index) => {
-    // console.log(`${index}: ${val}`);
-    if (val === '--ng-dialogs') {
-      ngDialogs = true;
-      htmlSourcePath = './projects/ng-dialogs/src/' + name + '.html';
-      htmlOutputPath = './dist/' + name + '.html';
-      fs.ensureDirSync('./dist/');
-    }
-  });
 
   fs.removeSync(htmlOutputPath);
 
@@ -25,11 +13,7 @@ function buildHtml(name) {
   const outputHtml = sourceHtml.replace(/SXC_VER/g, `${pjson.version}.${randomIntFromInterval(10000, 99999)}`);
 
   fs.writeFileSync(htmlOutputPath, outputHtml, 'utf8');
-  if (ngDialogs) {
-    console.log(chalkSuccess('Build ng-dialogs ' + name + '.html success!'));
-  } else {
-    console.log(chalkSuccess('Build ' + name + '.html success!'));
-  }
+  console.log(chalkSuccess('Build ' + name + '.html success!'));
 }
 buildHtml('ui');
 buildHtml('local');
--- build-helpers/clear-dist.js
+++ build-helpers/clear-dist.js
@@ -3,17 +3,5 @@ const fs = require('fs-extra');
 const chalk = require('chalk');
 const chalkSuccess = chalk.green;
 
-let ngDialogs = false;
-const args = process.argv.slice(2);
-args.forEach((val, index) => {
-  // console.log(`${index}: ${val}`);
-  if (val === '--ng-dialogs') { ngDialogs = true; }
-});
-
-if (ngDialogs) {
-  fs.emptyDirSync('./dist');
-  console.log(chalkSuccess('NgDialogs dist folder emptied!'));
-} else {
-  fs.emptyDirSync('./dist');
-  console.log(chalkSuccess('Dist folder emptied!'));
-}
+fs.emptyDirSync('./dist');
+console.log(chalkSuccess('Dist folder emptied!'));
