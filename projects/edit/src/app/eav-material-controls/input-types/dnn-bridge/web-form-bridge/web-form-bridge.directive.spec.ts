4201ad0483640074b91a7adffae5dbc63140fe48
Petar Miličić <pagnodunadan@gmail.com>
2020-01-31 09:28:14 +0100

    Removed .spec.ts files from edit-ui #168

--- projects/edit/src/app/eav-material-controls/input-types/dnn-bridge/web-form-bridge/web-form-bridge.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WebFormBridgeDirective } from './web-form-bridge.directive';
-
-describe('WebFormBridgeDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new WebFormBridgeDirective();
-  //   expect(directive).toBeTruthy();
-  // });
-});
