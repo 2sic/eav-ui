d454e3b0cb4096358b785df703c66841454b3cf8
Petar Miličić <pagnodunadan@gmail.com>
2020-02-18 15:07:37 +0100

    Added permissions to content types #145

--- projects/ng-dialogs/src/app/app-administration/shared/modals/permissions/permissions.component.ts
+++ projects/ng-dialogs/src/app/app-administration/shared/modals/permissions/permissions.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
-import { GridReadyEvent, GridSizeChangedEvent, ColDef, AllCommunityModules } from '@ag-grid-community/all-modules';
+import { Subscription } from 'rxjs';
+import { GridReadyEvent, GridSizeChangedEvent, ColDef, AllCommunityModules, CellClickedEvent } from '@ag-grid-community/all-modules';
 
 import { PermissionsService } from '../../services/permissions.service';
 import { Permission } from '../../models/permission.model';
 import { PermissionsGrantComponent } from '../../ag-grid-components/permissions-grant/permissions-grant.component';
 import { PermissionsGrantParams } from '../../models/permissions-grant-params';
+import { EditForm } from '../../models/edit-form.model';
+import { DialogService } from '../../../../shared/components/dialog-service/dialog.service';
+import { ITEMS_EDIT_DIALOG } from '../../../../shared/constants/dialog-names';
+import { eavConfiguration } from '../../services/eav-configuration.service';
 
 @Component({
   selector: 'app-permissions',
@@ -17,10 +22,10 @@ export class PermissionsComponent implements OnInit {
   permissions: Permission[];
 
   columnDefs: ColDef[] = [
-    { headerName: 'Name', field: 'Title', cellClass: 'clickable', onCellClicked: this.handleNameCellClicked.bind(this), },
-    { headerName: 'Condition', field: 'Identity', cellClass: 'clickable', onCellClicked: this.handleNameCellClicked.bind(this), },
+    { headerName: 'Name', field: 'Title', cellClass: 'clickable', onCellClicked: this.editPermission.bind(this), },
+    { headerName: 'Condition', field: 'Identity', cellClass: 'clickable', onCellClicked: this.editPermission.bind(this), },
     {
-      headerName: 'Grant', cellClass: 'clickable-with-button', onCellClicked: this.handleNameCellClicked.bind(this),
+      headerName: 'Grant', cellClass: 'clickable-with-button', onCellClicked: this.editPermission.bind(this),
       cellRenderer: 'permissionsGrantComponent', cellRendererParams: <PermissionsGrantParams>{
         onDelete: this.deletePermission.bind(this),
       },
@@ -31,15 +36,24 @@ export class PermissionsComponent implements OnInit {
   };
   modules = AllCommunityModules;
 
+  private subscription: Subscription = new Subscription();
+
   constructor(
     private dialogRef: MatDialogRef<PermissionsComponent>,
     private router: Router,
     private route: ActivatedRoute,
+    private dialogService: DialogService,
     private permissionsService: PermissionsService,
   ) { }
 
   ngOnInit() {
     this.fetchPermissions();
+    this.subscription.add(
+      this.dialogService.subToClosed([ITEMS_EDIT_DIALOG]).subscribe(closedDialog => {
+        console.log('Dialog closed event captured:', closedDialog);
+        this.fetchPermissions();
+      })
+    );
   }
 
   onGridReady(params: GridReadyEvent) {
@@ -50,17 +64,41 @@ export class PermissionsComponent implements OnInit {
     params.api.sizeColumnsToFit();
   }
 
-  edit(item: any) {
-    // eavAdminDialogs.openItemEditWithEntityId(item.Id, svc.liveListReload);
+  fetchPermissions() {
+    // spm clean this up using constants
+    const type = parseInt(this.route.snapshot.paramMap.get('type'), 10);
+    const keyType = this.route.snapshot.paramMap.get('keyType');
+    const contentTypeStaticName = this.route.snapshot.paramMap.get('contentTypeStaticName');
+    this.permissionsService.getAll(type, keyType, contentTypeStaticName).subscribe(permissions => {
+      this.permissions = permissions;
+    });
   }
 
-  add() {
-    // vm.openMetadata(svc.targetType, svc.keyType, svc.key, svc.ctName, svc.liveListReload);
-    this.router.navigate(['edit'], { relativeTo: this.route });
-  }
-
-  refresh() {
-    this.fetchPermissions();
+  editPermission(params: CellClickedEvent) {
+    let form: EditForm;
+    if (params === null) {
+      const contentTypeStaticName = this.route.snapshot.paramMap.get('contentTypeStaticName');
+      form = {
+        addItems: [{
+          // spm clean this up using constants
+          ContentTypeName: eavConfiguration.contentType.permissions,
+          For: {
+            Target: eavConfiguration.metadata.metadataOfEntity.value,
+            Guid: contentTypeStaticName,
+          }
+        }],
+        editItems: null,
+        persistedData: {},
+      };
+    } else {
+      const permission = <Permission>params.data;
+      form = {
+        addItems: null,
+        editItems: [{ EntityId: permission.Id.toString(), Title: permission.Title }],
+        persistedData: {},
+      };
+    }
+    this.router.navigate([`edit/${JSON.stringify(form)}`], { relativeTo: this.route });
   }
 
   private deletePermission(permission: Permission) {
@@ -73,33 +111,5 @@ export class PermissionsComponent implements OnInit {
 
   closeDialog() {
     this.dialogRef.close();
-  }
-
-  private openMetadata(targetType: any, keyType: any, key: any, contentType: any, closeCallback: any) {
-    // const items = [
-    //   {
-    //     ContentTypeName: contentType,
-    //     Metadata: {
-    //       TargetType: targetType,
-    //       KeyType: keyType,
-    //       Key: key
-    //     }
-    //   }
-    // ];
-
-    // eavAdminDialogs.openEditItems(items, closeCallback, { partOfPage: false });
-  }
-
-  private fetchPermissions() {
-    const type = parseInt(this.route.snapshot.paramMap.get('type'), 10);
-    const keyType = this.route.snapshot.paramMap.get('keyType');
-    const contentTypeStaticName = this.route.snapshot.paramMap.get('contentTypeStaticName');
-    this.permissionsService.getAll(type, keyType, contentTypeStaticName).subscribe(permissions => {
-      this.permissions = permissions;
-    });
-  }
-
-  private handleNameCellClicked() {
-    alert('Open edit');
   }
 }
