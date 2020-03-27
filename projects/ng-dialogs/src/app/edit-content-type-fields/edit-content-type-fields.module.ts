852b745029e5dee1cc2b0e06599743a7e837927b
Petar Miličić <pagnodunadan@gmail.com>
2020-03-27 16:21:33 +0100

    Moved edit-content-type-fields files #165

---
+++ projects/ng-dialogs/src/app/edit-content-type-fields/edit-content-type-fields.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditContentTypeFieldsRoutingModule } from './edit-content-type-fields-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EditContentTypeFieldsRoutingModule
+  ]
+})
+export class EditContentTypeFieldsModule { }
