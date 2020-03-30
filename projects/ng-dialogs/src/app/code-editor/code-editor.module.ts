3eca530f323bfcf3dd162a8a041b3f39a9d3e8ec
Petar Miličić <pagnodunadan@gmail.com>
2020-03-30 13:30:23 +0200

    Moved code-editor files #165

--- projects/ng-dialogs/src/app/code-editor/code-editor.module.ts
+++ projects/ng-dialogs/src/app/code-editor/code-editor.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { CodeEditorRoutingModule } from './code-editor-routing.module';
 import { SharedComponentsModule } from '../shared/components/shared-components.module';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
+import { Context } from '../shared/context/context';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,10 @@ import { CodeEditorComponent } from './code-editor/code-editor.component';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
+    MatTooltipModule,
+  ],
+  providers: [
+    Context,
   ]
 })
 export class CodeEditorModule { }
