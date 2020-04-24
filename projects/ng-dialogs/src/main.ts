635cced1665e0249e9938767bb3c058436e83125
ijungleboy <daniel.mettler@2sic.com>
2020-04-24 13:49:51 +0200

    add warning about console.log turned off

--- projects/ng-dialogs/src/main.ts
+++ projects/ng-dialogs/src/main.ts
@@ -6,6 +6,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  // Note: must be output as a log, NOT as warn. Reason is that the console has so many warnings
+  // in production related to .map files not found, another warning would simply not be visible.
+  window.console.log(`The admin/edit UI has a lot of console.log statements (we know, we\'ll have to work on that).
+  In the meantime the production build turns off console.log. So if you need to do your own logging, use console.warn.`);
   window.console.log = () => { };
 }
 
