895229176bb6166a1073b0e0fb9da3ae69646972
ijungleboy <daniel.mettler@2sic.com>
2020-04-28 22:53:06 +0200

    modify content-type now uses server scopes + naming of input-types allows for dashes

--- projects/ng-dialogs/src/app/app-administration/shared/constants/content-type.ts
+++ projects/ng-dialogs/src/app/app-administration/shared/constants/content-type.ts
@@ -1,7 +1,8 @@
 /**
  * The pattern determines what content-type names are allowed.
  * Basically it's A-Z and numbers after the first digit.
- * But there is an exception: types describing an input-type begin with an '@'
+ * But there is an exception: types describing an input-type begin with an '@' and can also contain `-` chars
+ * Note (2dm): I'm not sure why there is '?:' non-capture in the first set - I think it's a mistake
  */
-export const contentTypeNamePattern = /^[@A-Za-z](?:[A-Za-z0-9]+)*$/;
+export const contentTypeNamePattern = /^([A-Za-z](?:[A-Za-z0-9]+)*)|(\@[A-Za-z0-9\-]+)$/;
 export const contentTypeNameError = 'Standard letters and numbers are allowed. Must start with a letter.';
