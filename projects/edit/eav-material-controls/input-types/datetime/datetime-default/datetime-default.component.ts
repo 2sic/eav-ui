56e8d58f80d95890ca148c9e30b56130f8f676d4
Petar Miličić <pagnodunadan@gmail.com>
2020-05-28 14:55:09 +0200

    Reactive datetime-default #190

--- projects/edit/eav-material-controls/input-types/datetime/datetime-default/datetime-default.component.ts
+++ projects/edit/eav-material-controls/input-types/datetime/datetime-default/datetime-default.component.ts
@@ -1,48 +1,44 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { DateAdapter } from '@angular/material/core';
 import { DateTimeAdapter } from '@danielmoncada/angular-datetime-picker';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { Field } from '../../../../eav-dynamic-form/model/field';
-import { FieldConfigSet } from '../../../../eav-dynamic-form/model/field-config';
 import { InputType } from '../../../../eav-dynamic-form/decorators/input-type.decorator';
-import { ValidationMessagesService } from '../../../validators/validation-messages-service';
 import { WrappersConstants } from '../../../../shared/constants/wrappers-constants';
 import { angularConsoleLog } from '../../../../../ng-dialogs/src/app/shared/helpers/angular-console-log.helper';
+import { BaseComponent } from '../../base/base.component';
+import { EavService } from '../../../../shared/services/eav.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'datetime-default',
   templateUrl: './datetime-default.component.html',
-  styleUrls: ['./datetime-default.component.scss']
+  styleUrls: ['./datetime-default.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 @InputType({
   wrapper: [WrappersConstants.eavLocalizationWrapper],
 })
-export class DatetimeDefaultComponent implements Field {
-  config: FieldConfigSet;
-  group: FormGroup;
+export class DatetimeDefaultComponent extends BaseComponent<string> implements OnInit {
+  useTimePicker$: Observable<boolean>;
 
   constructor(
-    private validationMessagesService: ValidationMessagesService,
-    private dateAdapter: DateAdapter<any>, // material date picker
-    private dateTimeAdapter: DateTimeAdapter<any>, // owl date picker
+    eavService: EavService,
     private translate: TranslateService,
+    private dateAdapter: DateAdapter<any>,
+    private dateTimeAdapter: DateTimeAdapter<any>,
   ) {
-    // set locale for date pickers (only once because DNN language doesn't get updated during use)
-    // if locale is not recognized, falls back to 'en'
+    super(eavService);
     const currentLang = this.translate.currentLang;
     angularConsoleLog('Datepickers locale:', currentLang);
     this.dateAdapter.setLocale(currentLang);
     this.dateTimeAdapter.setLocale(currentLang);
   }
 
-  get inputInvalid() {
-    return this.group.controls[this.config.field.name].invalid;
-  }
-
-  get disabled() {
-    return this.group.controls[this.config.field.name].disabled;
+  ngOnInit() {
+    super.ngOnInit();
+    this.useTimePicker$ = this.settings$.pipe(map(settings => settings.UseTimePicker));
   }
 }
