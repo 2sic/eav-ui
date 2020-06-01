7e685fe5adec32af9e1b855487e64ebd4b75c422
Petar Miličić <pagnodunadan@gmail.com>
2020-06-01 16:28:24 +0200

    FieldSettings Type #190

--- projects/edit-types/src/FieldSettings.d.ts
+++ projects/edit-types/src/FieldSettings.d.ts
@@ -1,3 +1,178 @@
-export interface FieldSettings {
-  [key: string]: any;
+/** @All */
+interface All {
+  Name: string;
+  InputType: string;
+  DefaultValue: string;
+  Placeholder: string;
+  Notes: string;
+  VisibleInEditUI: boolean;
+  Required: boolean;
+  Disabled: boolean;
+  DisableTranslation: boolean;
+  Errors: unknown;
+  Warnings: unknown;
+  ValidationRegExJavaScript: string;
+  /** @deprecated */
+  CustomJavaScript: string;
 }
+
+/** @String */
+interface String extends All {
+  /** @deprecated */
+  DropdownValues: string;
+  /** @deprecated */
+  InputType: string;
+}
+
+/** @string-default */
+interface StringDefault extends String {
+  RowCount: number;
+}
+
+/** @string-dropdown */
+interface StringDropdown extends String {
+  DropdownValues: string;
+  EnableTextEntry: boolean;
+}
+
+/** @string-url-path */
+interface StringUrlPath extends String {
+  AutoGenerateMask: string;
+  AllowSlashes: boolean;
+}
+
+/** @string-wysiwyg */
+interface StringWysiwyg extends String {
+  Dialog: "" | "dialog" | "inline";
+  ButtonSource: "" | "true" | "false";
+  ButtonAdvanced: "" | "true" | "false";
+}
+
+/** @string-dropdown-query */
+interface StringDropdownQuery extends String {
+  Query: string;
+  StreamName: string;
+  Value: string;
+  Label: string;
+  UrlParameters: string;
+  EnableTextEntry: boolean;
+  EnableEdit: boolean;
+  EnableRemove: boolean;
+  AllowMultiValue: boolean;
+  Separator: string;
+}
+
+/** @string-font-icon-picker */
+interface StringFontIconPicker extends String {
+  CssPrefix: string;
+  PreviewCss: string;
+  Files: string;
+}
+
+/** @Number */
+interface Number extends All {
+  Decimals: number;
+  Min: number;
+  Max: number;
+  /** @deprecated */
+  AddressMask: string;
+  /** @deprecated */
+  InputType: string;
+  /** @deprecated */
+  ValidationRegEx: string;
+  /** @deprecated */
+  ValidationRegExJavaScript: string;
+}
+
+/** @Hyperlink */
+interface Hyperlink extends All {
+  FileFilter: string;
+  DefaultDialog: string;
+  ShowPagePicker: boolean;
+  ShowImageManager: boolean;
+  ShowFileManager: boolean;
+  ShowAdam: boolean;
+  Buttons: string;
+  Paths: string;
+  ServerResourceMapping: string;
+}
+
+/** @hyperlink-library */
+interface HyperlinkLibrary extends Hyperlink {
+  FolderDepth: number;
+  AllowAssetsInRoot: boolean;
+  MetadataContentTypes: string;
+}
+
+/** @Entity */
+interface Entity extends All {
+  EntityType: string;
+  AllowMultiValue: boolean;
+  EnableEdit: boolean;
+  EnableCreate: boolean;
+  EnableAddExisting: boolean;
+  EnableRemove: boolean;
+  EnableDelete: boolean;
+}
+
+/** @entity-query */
+interface EntityQuery extends Entity {
+  Query: string;
+  StreamName: string;
+  UrlParameters: string;
+}
+
+/** @empty-default */
+interface EmptyDefault extends All {
+  DefaultCollapsed: boolean;
+}
+
+/** @DateTime */
+interface DateTime extends All {
+  UseTimePicker: boolean;
+}
+
+/** @custom-json-editor */
+interface CustomJsonEditor extends All {
+  Rows: number;
+}
+
+/** @custom-gps */
+interface CustomGps extends All {
+  LatField: string;
+  LongField: string;
+  AddressMask: string;
+  /** @deprecated */
+  "Address Mask": string;
+}
+
+/** @Boolean */
+interface Boolean extends All {
+  TitleTrue: string;
+  TitleFalse: string;
+  TitleIndeterminate: string;
+}
+
+/** Properties of main collapsible in the form */
+interface ContentTypeSettings {
+  EditInstructions: string;
+}
+
+export interface FieldSettings
+  extends StringDefault,
+  StringDropdown,
+  StringUrlPath,
+  StringWysiwyg,
+  StringDropdownQuery,
+  StringFontIconPicker,
+  Number,
+  Hyperlink,
+  HyperlinkLibrary,
+  Entity,
+  EntityQuery,
+  EmptyDefault,
+  DateTime,
+  CustomJsonEditor,
+  CustomGps,
+  Boolean,
+  ContentTypeSettings { }
