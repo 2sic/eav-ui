8d3c1c7401ca636517014e9fad2d434dce46c8dc
Petar Miličić <pagnodunadan@gmail.com>
2020-06-23 16:49:03 +0200

    Use selected text as link name on adam item click in wysiwyg #190

--- projects/field-string-wysiwyg/src/connector/adam.ts
+++ projects/field-string-wysiwyg/src/connector/adam.ts
@@ -2,16 +2,19 @@ import { Adam } from '../../../edit-types';
 
 export function attachAdam(editor: any, adam: Adam) {
   adam.onItemClick = (item) => {
-    const fileName = item.Name.substr(0, item.Name.lastIndexOf('.'));
-
     const imageMode = adam.getConfig().showImagesOnly;
+    const selected = editor.selection.getContent();
+    const fileName = item.Name.substring(0, item.Name.lastIndexOf('.'));
+
     const content = imageMode
-      ? '<img src="' + item.FullPath + '" + alt="' + fileName + '">'
-      : '<a href="' + item.FullPath + '">' + fileName + '</a>';
+      ? `${selected}<img src="${item.FullPath}" alt="${fileName}">`
+      : `<a href="${item.FullPath}">${selected || fileName}</a>`;
 
     editor.insertContent(content);
   };
+
   adam.onItemUpload = adam.onItemClick;
+
   if (adam.getConfig() == null) {
     adam.setConfig({ disabled: false });
   }
