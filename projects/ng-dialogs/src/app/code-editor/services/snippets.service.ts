21987bcc7a2810611144b86744bcade6ead3a850
ijungleboy <daniel.mettler@2sic.com>
2020-06-25 19:19:52 +0200

    Fix snippets for header & presentation https://github.com/2sic/2sxc/issues/2112

--- projects/ng-dialogs/src/app/code-editor/services/snippets.service.ts
+++ projects/ng-dialogs/src/app/code-editor/services/snippets.service.ts
@@ -94,12 +94,12 @@ export class SnippetsService {
     if (templateConfiguration.HasList) {
       sets.List = Object.assign({}, sets.List, { Fields: {}, PresentationFields: {} });
       if (templateConfiguration.TypeList) {
-        this.loadContentType(sets.List.Fields, templateConfiguration.TypeList, 'ListContent', templateConfiguration, inputTypeSnippets);
+        this.loadContentType(sets.List.Fields, templateConfiguration.TypeList, 'Header', templateConfiguration, inputTypeSnippets);
       }
       if (templateConfiguration.TypeListPresentation) {
         this.loadContentType(
           sets.List.PresentationFields, templateConfiguration.TypeListPresentation,
-          'ListContent.Presentation', templateConfiguration, inputTypeSnippets,
+          'Header.Presentation', templateConfiguration, inputTypeSnippets,
         );
       }
     } else {
