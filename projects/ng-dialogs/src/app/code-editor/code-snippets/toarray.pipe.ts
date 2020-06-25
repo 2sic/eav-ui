35373a4cba1d7cbf800a25f9cee4ddbc40ff0255
Petar Miličić <pagnodunadan@gmail.com>
2020-06-25 14:28:04 +0200

    Renamed objectToArray pipe #160

--- projects/ng-dialogs/src/app/code-editor/code-snippets/toarray.pipe.ts
+++ projects/ng-dialogs/src/app/code-editor/code-snippets/toarray.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'toarray' })
-export class ToArrayPipe implements PipeTransform {
+@Pipe({ name: 'objectToArray' })
+export class ObjectToArrayPipe implements PipeTransform {
   transform(obj: object | Array<any>): Array<any> {
     if (typeof obj !== typeof {}) { return obj as Array<any>; }
     return Object.keys(obj).map(key => {
