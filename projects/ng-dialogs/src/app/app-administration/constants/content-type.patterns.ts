aac779b0e55ad0d7f7a6df2eb403066ebb0abcde
Petar Miličić <pagnodunadan@gmail.com>
2020-07-01 15:36:50 +0200

    Fixed content type name validation pattern #145

--- projects/ng-dialogs/src/app/app-administration/constants/content-type.patterns.ts
+++ projects/ng-dialogs/src/app/app-administration/constants/content-type.patterns.ts
@@ -5,8 +5,8 @@
  *  - types describing an input-type begin with an `@` and can also contain `-` chars
  *  - types beginning with an `|` are very old type names for data-sources, they can contain anything!
  */
-export const contentTypeNamePattern = /^([A-Za-z](?:[A-Za-z0-9]+)*)|(@[A-Za-z0-9\-]+)$/;
+export const contentTypeNamePattern = /(^[A-Za-z][A-Za-z0-9]+$)|(^@[A-Za-z][A-Za-z0-9-]*$)/;
 export const contentTypeNameError = 'Standard letters and numbers are allowed. Must start with a letter.';
 
 // 2020-04-29 2dm - temporarily used this pattern while renaming unique named types containing '|' chars
-// export const contentTypeNamePattern = /^([A-Za-z](?:[A-Za-z0-9]+)*)|(\@[A-Za-z0-9\-]+)|(\|.*)$/;
+// export const contentTypeNamePattern = /(^[A-Za-z][A-Za-z0-9]+$)|(^@[A-Za-z][A-Za-z0-9-]*$)|(^\|.*$)/;
