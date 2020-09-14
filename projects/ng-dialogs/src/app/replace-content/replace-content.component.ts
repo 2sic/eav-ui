c56a81df73179af3ad06e79678c084d1dde01456
Petar Miličić <pagnodunadan@gmail.com>
2020-09-14 13:48:40 +0200

    Reactive replace content #190

--- projects/ng-dialogs/src/app/replace-content/replace-content.component.ts
+++ projects/ng-dialogs/src/app/replace-content/replace-content.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding, ChangeDetectionStrategy } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Subscription } from 'rxjs';
+import { Subscription, BehaviorSubject } from 'rxjs';
 import { filter, startWith, map, pairwise } from 'rxjs/operators';
 
 import { ContentGroupService } from '../manage-content-list/services/content-group.service';
@@ -14,15 +14,22 @@ import { convertFormToUrl } from '../shared/helpers/url-prep.helper';
 @Component({
   selector: 'app-replace-content',
   templateUrl: './replace-content.component.html',
-  styleUrls: ['./replace-content.component.scss']
+  styleUrls: ['./replace-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReplaceContentComponent implements OnInit, OnDestroy {
   @HostBinding('className') hostClass = 'dialog-component';
 
-  options: ReplaceOption[];
-  item: ContentGroupAdd;
-  contentTypeName: string;
+  item$ = new BehaviorSubject<ContentGroupAdd>({
+    id: null,
+    guid: this.route.snapshot.paramMap.get('guid'),
+    part: this.route.snapshot.paramMap.get('part'),
+    index: parseInt(this.route.snapshot.paramMap.get('index'), 10),
+    add: !!this.route.snapshot.queryParamMap.get('add'),
+  });
+  options$ = new BehaviorSubject<ReplaceOption[]>(null);
 
+  private contentTypeName: string;
   private subscription = new Subscription();
 
   constructor(
@@ -31,15 +38,7 @@ export class ReplaceContentComponent implements OnInit, OnDestroy {
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
-  ) {
-    this.item = {
-      id: null,
-      guid: this.route.snapshot.paramMap.get('guid'),
-      part: this.route.snapshot.paramMap.get('part'),
-      index: parseInt(this.route.snapshot.paramMap.get('index'), 10),
-      add: !!this.route.snapshot.queryParamMap.get('add'),
-    };
-  }
+  ) { }
 
   ngOnInit() {
     this.getConfig();
@@ -47,12 +46,18 @@ export class ReplaceContentComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.item$.complete();
+    this.options$.complete();
     this.subscription.unsubscribe();
+  }
+
+  selectedChanged(id: number) {
+    this.item$.next({ ...this.item$.value, id });
   }
 
   save() {
     this.snackBar.open('Saving...');
-    this.contentGroupService.saveItem(this.item).subscribe(() => {
+    this.contentGroupService.saveItem(this.item$.value).subscribe(() => {
       this.snackBar.open('Saved', null, { duration: 2000 });
       this.closeDialog();
     });
@@ -60,7 +65,7 @@ export class ReplaceContentComponent implements OnInit, OnDestroy {
 
   copySelected() {
     const form: EditForm = {
-      items: [{ ContentTypeName: this.contentTypeName, DuplicateEntity: this.item.id }],
+      items: [{ ContentTypeName: this.contentTypeName, DuplicateEntity: this.item$.value.id }],
     };
     const formUrl = convertFormToUrl(form);
     this.router.navigate([`edit/${formUrl}`], { relativeTo: this.route });
@@ -71,17 +76,18 @@ export class ReplaceContentComponent implements OnInit, OnDestroy {
   }
 
   private getConfig() {
-    this.contentGroupService.getItems(this.item).subscribe(replaceConfig => {
+    this.contentGroupService.getItems(this.item$.value).subscribe(replaceConfig => {
+      const options: ReplaceOption[] = [];
       const itemKeys = Object.keys(replaceConfig.Items);
-      this.options = [];
       for (const key of itemKeys) {
         const nKey = parseInt(key, 10);
         const itemName = replaceConfig.Items[nKey];
-        this.options.push({ label: `${itemName} (${nKey})`, value: nKey });
+        options.push({ label: `${itemName} (${nKey})`, value: nKey });
       }
-      // don't set the ID if the dialog should be in add-mode
-      if (!this.item.id && !this.item.add) {
-        this.item.id = replaceConfig.SelectedId;
+      this.options$.next(options);
+      // don't set the ID if dialog should be in add-mode
+      if (!this.item$.value.id && !this.item$.value.add) {
+        this.item$.next({ ...this.item$.value, id: replaceConfig.SelectedId });
       }
       if (!this.contentTypeName) {
         this.contentTypeName = replaceConfig.ContentTypeName;
@@ -102,7 +108,8 @@ export class ReplaceContentComponent implements OnInit, OnDestroy {
         const navigation = this.router.getCurrentNavigation();
         const editResult = navigation.extras?.state;
         if (editResult) {
-          this.item.id = editResult[Object.keys(editResult)[0]];
+          const id = editResult[Object.keys(editResult)[0]];
+          this.item$.next({ ...this.item$.value, id });
         }
       })
     );
