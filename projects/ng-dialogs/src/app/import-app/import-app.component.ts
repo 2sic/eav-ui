0c1630c332ddb034da95406a9a56b3616ab4c1d9
Petar Miličić <pagnodunadan@gmail.com>
2020-09-16 14:08:56 +0200

    Reactive import app dialog #190

--- projects/ng-dialogs/src/app/import-app/import-app.component.ts
+++ projects/ng-dialogs/src/app/import-app/import-app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, ChangeDetectionStrategy } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatDialogRef } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
 
 import { ImportAppService } from './services/import-app.service';
 import { ImportAppResult } from './models/import-app-result.model';
@@ -8,14 +9,15 @@ import { ImportAppResult } from './models/import-app-result.model';
 @Component({
   selector: 'app-import-app',
   templateUrl: './import-app.component.html',
-  styleUrls: ['./import-app.component.scss']
+  styleUrls: ['./import-app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImportAppComponent implements OnInit {
   @HostBinding('className') hostClass = 'dialog-component';
 
-  isImporting = false;
-  importFile: File;
-  importResult: ImportAppResult;
+  isImporting$ = new BehaviorSubject(false);
+  importFile$ = new BehaviorSubject<File>(null);
+  importResult$ = new BehaviorSubject<ImportAppResult>(null);
 
   constructor(private dialogRef: MatDialogRef<ImportAppComponent>, private importAppService: ImportAppService) { }
 
@@ -23,30 +25,30 @@ export class ImportAppComponent implements OnInit {
   }
 
   importApp(changedName?: string) {
-    this.isImporting = true;
+    this.isImporting$.next(true);
 
-    this.importAppService.importApp(this.importFile, changedName).subscribe({
+    this.importAppService.importApp(this.importFile$.value, changedName).subscribe({
       next: result => {
-        this.isImporting = false;
+        this.isImporting$.next(false);
 
-        this.importResult = result;
+        this.importResult$.next(result);
         // The app could not be installed because the app-folder already exists. Install app in a different folder?
-        if (this.importResult && this.importResult.Messages && this.importResult.Messages[0]
-          && this.importResult.Messages[0].MessageType === 0) {
-          const folderName = prompt(this.importResult.Messages[0].Text + ' Would you like to install it using another folder name?');
+        if (this.importResult$.value?.Messages?.[0]?.MessageType === 0) {
+          const folderName = prompt(this.importResult$.value.Messages[0].Text + ' Would you like to install it using another folder name?');
           if (folderName) {
             this.importApp(folderName);
           }
         }
       },
       error: (error: HttpErrorResponse) => {
-        this.isImporting = false;
+        this.isImporting$.next(false);
       },
     });
   }
 
   fileChange(event: Event) {
-    this.importFile = (event.target as HTMLInputElement).files[0];
+    const importFile = (event.target as HTMLInputElement).files[0];
+    this.importFile$.next(importFile);
   }
 
   closeDialog() {
