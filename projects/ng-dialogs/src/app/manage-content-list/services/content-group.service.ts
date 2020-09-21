e4bc0f71cdcc69a3611a32eda17f73d23dbec48c
ijungleboy <daniel.mettler@2sic.com>
2020-09-21 08:48:21 +0200

    move contentgroup api to new url https://github.com/2sic/2sxc/issues/2173

--- projects/ng-dialogs/src/app/manage-content-list/services/content-group.service.ts
+++ projects/ng-dialogs/src/app/manage-content-list/services/content-group.service.ts
@@ -8,36 +8,38 @@ import { ContentGroup, ContentGroupAdd } from '../models/content-group.model';
 import { ReplaceConfig } from '../../replace-content/models/replace-config.model';
 import { GroupHeader } from '../models/group-header.model';
 
+const webApiContentGroup = 'cms/contentgroup/';
+
 @Injectable()
 export class ContentGroupService {
   constructor(private http: HttpClient, private context: Context, private dnnContext: DnnContext) { }
 
   getItems(item: ContentGroup) {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/contentgroup/replace'), {
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiContentGroup + 'replace'), {
       params: { appId: this.context.appId.toString(), guid: item.guid, part: item.part, index: item.index.toString() }
     }) as Observable<ReplaceConfig>;
   }
 
   saveItem(item: ContentGroupAdd) {
-    return this.http.post(this.dnnContext.$2sxc.http.apiUrl('app-sys/contentgroup/replace'), {}, {
+    return this.http.post(this.dnnContext.$2sxc.http.apiUrl(webApiContentGroup + 'replace'), {}, {
       params: { guid: item.guid, part: item.part, index: item.index.toString(), entityId: item.id.toString(), add: `${item.add}` }
     }) as Observable<null>;
   }
 
   getList(contentGroup: ContentGroup) {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/contentgroup/itemlist'), {
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiContentGroup + 'itemlist'), {
       params: { appId: this.context.appId.toString(), guid: contentGroup.guid, part: contentGroup.part }
     }) as Observable<GroupHeader[]>;
   }
 
   saveList(contentGroup: ContentGroup, resortedList: GroupHeader[]) {
-    return this.http.post(this.dnnContext.$2sxc.http.apiUrl('app-sys/contentgroup/itemlist'), resortedList, {
+    return this.http.post(this.dnnContext.$2sxc.http.apiUrl(webApiContentGroup + 'itemlist'), resortedList, {
       params: { appId: this.context.appId.toString(), guid: contentGroup.guid, part: contentGroup.part }
     }) as Observable<boolean>;
   }
 
   getHeader(contentGroup: ContentGroup) {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/contentgroup/header'), {
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiContentGroup + 'header'), {
       params: { appId: this.context.appId.toString(), guid: contentGroup.guid }
     }) as Observable<GroupHeader>;
   }
