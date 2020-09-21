4cc2feba5e3e4c890d61e9a52a0b6b8dccfabf90
ijungleboy <daniel.mettler@2sic.com>
2020-09-21 07:39:00 +0200

    move language activation to new url https://github.com/2sic/2sxc/issues/2173

--- projects/ng-dialogs/src/app/apps-management/services/enable-languages.service.ts
+++ projects/ng-dialogs/src/app/apps-management/services/enable-languages.service.ts
@@ -5,22 +5,25 @@ import { Context as DnnContext } from '@2sic.com/dnn-sxc-angular';
 
 import { EnableLanguage } from '../models/enable-language.model';
 
+const webApiZoneRoot = 'admin/zone/';
+
 @Injectable()
 export class EnableLanguagesService {
   constructor(private http: HttpClient, private dnnContext: DnnContext) { }
 
   getAll() {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/system/getlanguages')) as Observable<EnableLanguage[]>;
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiZoneRoot + 'getlanguages')) as Observable<EnableLanguage[]>;
   }
 
   toggle(code: string, enable: boolean) {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/system/switchlanguage'), {
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiZoneRoot + 'switchlanguage'), {
       params: { cultureCode: code, enable: enable.toString() },
     }) as Observable<null>;
   }
 
+  // todo: this code is _identical_ to toggle above. one should be removed, but I don't know where they are used
   save(code: string, enable: boolean) {
-    return this.http.get(this.dnnContext.$2sxc.http.apiUrl('app-sys/system/switchlanguage'), {
+    return this.http.get(this.dnnContext.$2sxc.http.apiUrl(webApiZoneRoot + 'switchlanguage'), {
       params: { cultureCode: code, enable: enable.toString() },
     }) as Observable<null>;
   }
