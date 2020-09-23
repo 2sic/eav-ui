7a02dcc1c26ac30a2140671f838729dac67fdd1f
Petar Miličić <pagnodunadan@gmail.com>
2020-09-23 14:30:11 +0200

    Added content type name to import single json item dialog #201

--- projects/ng-dialogs/src/app/content-items/ag-grid-components/content-item-import/content-item-import.component.ts
+++ projects/ng-dialogs/src/app/content-items/ag-grid-components/content-item-import/content-item-import.component.ts
@@ -1,8 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, HostBinding, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ContentType } from '../../../app-administration/models/content-type.model';
+import { ContentTypesService } from '../../../app-administration/services/content-types.service';
 import { ImportAppResult } from '../../../import-app/models/import-app-result.model';
 import { ContentItemsService } from '../../services/content-items.service';
 import { ContentItemImportDialogData } from './content-item-import-dialog.config';
@@ -16,6 +19,7 @@ import { ContentItemImportDialogData } from './content-item-import-dialog.config
 export class ContentItemImportComponent implements OnInit, OnDestroy {
   @HostBinding('className') hostClass = 'dialog-component';
 
+  contentType$ = new BehaviorSubject<ContentType>(null);
   private isImporting$ = new BehaviorSubject(false);
   private importFile$ = new BehaviorSubject<File>(null);
   private importResult$ = new BehaviorSubject<ImportAppResult>(null);
@@ -23,13 +27,20 @@ export class ContentItemImportComponent implements OnInit, OnDestroy {
     map(([isImporting, importFile, importResult]) => ({ isImporting, importFile, importResult })),
   );
 
+  private contentTypeStaticName = this.route.snapshot.parent.paramMap.get('contentTypeStaticName');
+
   constructor(
     @Inject(MAT_DIALOG_DATA) private dialogData: ContentItemImportDialogData,
     private dialogRef: MatDialogRef<ContentItemImportComponent>,
+    private route: ActivatedRoute,
+    private contentTypesService: ContentTypesService,
     private contentItemsService: ContentItemsService,
   ) { }
 
   ngOnInit() {
+    this.contentTypesService.retrieveContentType(this.contentTypeStaticName).subscribe(contentType => {
+      this.contentType$.next(contentType);
+    });
     if (this.dialogData.files != null) {
       this.importFile$.next(this.dialogData.files[0]);
       this.importContentItem();
@@ -37,6 +48,7 @@ export class ContentItemImportComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.contentType$.complete();
     this.isImporting$.complete();
     this.importFile$.complete();
     this.importResult$.complete();
