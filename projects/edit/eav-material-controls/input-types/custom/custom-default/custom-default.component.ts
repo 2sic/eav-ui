0ccadffe5b28cbab98e9b2b205a890dec5ae5275
Petar Miličić <pagnodunadan@gmail.com>
2020-10-28 15:09:21 +0100

    Added localization wrapper to custom-default to fix calculations not getting disabled check event #211

--- projects/edit/eav-material-controls/input-types/custom/custom-default/custom-default.component.ts
+++ projects/edit/eav-material-controls/input-types/custom/custom-default/custom-default.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { InputType } from '../../../../eav-dynamic-form/decorators/input-type.decorator';
+import { WrappersConstants } from '../../../../shared/constants/wrappers.constants';
 import { EavService } from '../../../../shared/services/eav.service';
 import { ValidationMessagesService } from '../../../validators/validation-messages-service';
 import { BaseComponent } from '../../base/base.component';
@@ -12,6 +13,7 @@ import { BaseComponent } from '../../base/base.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 @InputType({
+  wrapper: [WrappersConstants.EavLocalizationWrapper],
 })
 export class CustomDefaultComponent extends BaseComponent<null> implements OnInit, OnDestroy {
 
