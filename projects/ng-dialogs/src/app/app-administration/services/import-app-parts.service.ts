ce9a3dc08374c55ed9e4970fa6aed9004c1e5a94
ijungleboy <daniel.mettler@2sic.com>
2020-11-10 23:09:01 +0100

    implement app-reset (experimental) for https://github.com/2sic/2sxc/issues/2223

--- projects/ng-dialogs/src/app/app-administration/services/import-app-parts.service.ts
+++ projects/ng-dialogs/src/app/app-administration/services/import-app-parts.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ImportAppResult } from '../../import-app/models/import-app-result.model';
+import { webApiAppRoot } from '../../import-app/services/import-app.service';
 import { Context } from '../../shared/services/context';
 
 export const webApiAppPartsRoot = 'admin/appParts/';
@@ -18,4 +19,14 @@ export class ImportAppPartsService {
       params: { appId: this.context.appId.toString(), zoneId: this.context.zoneId.toString() }
     }) as Observable<ImportAppResult>;
   }
+
+  /**
+   * Reset the app back to the state it was in the last xml export
+   */
+  resetApp() {
+    const ctx = this.context;
+    const url = this.dnnContext.$2sxc.http.apiUrl(webApiAppRoot + `Reset?zoneid=${ctx.zoneId}&appid=${ctx.appId}`);
+    return this.http.post(url, { }) as Observable<ImportAppResult>;
+  }
+
 }
