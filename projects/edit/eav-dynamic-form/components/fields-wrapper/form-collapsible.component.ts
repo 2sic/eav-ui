d84f157725e2806191c52a7e5325f055e640ac8e
Petar Miličić <pagnodunadan@gmail.com>
2020-12-11 16:23:23 +0100

    Fixed translation content type settings #211

--- projects/edit/eav-dynamic-form/components/fields-wrapper/form-collapsible.component.ts
+++ projects/edit/eav-dynamic-form/components/fields-wrapper/form-collapsible.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, Observable } from 'rxjs';
+import { combineLatest, Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { EavHeader } from '../../../shared/models/eav';
+import { FieldsSettingsService } from '../../../shared/services/fields-settings.service';
 import { ContentTypeService } from '../../../shared/store/ngrx-data/content-type.service';
 import { ItemService } from '../../../shared/store/ngrx-data/item.service';
 import { LanguageInstanceService } from '../../../shared/store/ngrx-data/language-instance.service';
@@ -16,7 +17,7 @@ import { FormCollapsibleTemplateVars } from './form-collapsible.models';
   templateUrl: './form-collapsible.component.html',
   styleUrls: ['./form-collapsible.component.scss'],
 })
-export class FormCollapsibleComponent implements OnInit {
+export class FormCollapsibleComponent implements OnInit, OnDestroy {
   @Input() config: FieldConfigSet;
   @Input() fieldConfigs: FieldConfigSet[];
   @Input() group: FormGroup;
@@ -24,21 +25,34 @@ export class FormCollapsibleComponent implements OnInit {
   collapse = false;
   templateVars$: Observable<FormCollapsibleTemplateVars>;
 
+  private subscription: Subscription;
+
   constructor(
     private languageInstanceService: LanguageInstanceService,
     private contentTypeService: ContentTypeService,
     private itemService: ItemService,
     private router: Router,
     private route: ActivatedRoute,
+    private fieldsSettingsService: FieldsSettingsService,
   ) { }
 
   ngOnInit() {
+    this.subscription = new Subscription();
     const currentLanguage$ = this.languageInstanceService.getCurrentLanguage(this.config.form.formId);
     const defaultLanguage$ = this.languageInstanceService.getDefaultLanguage(this.config.form.formId);
     const header$ = this.itemService.selectItemHeader(this.config.entity.entityGuid);
     const contentType$ = this.contentTypeService.getContentTypeById(this.config.entity.contentTypeId);
     const settingsLogic = new FormCollapsibleLogic();
     const settings$ = settingsLogic.update(this.config.field.settings$, currentLanguage$, defaultLanguage$, header$, contentType$);
+
+    this.subscription.add(
+      combineLatest([
+        this.languageInstanceService.getCurrentLanguage(this.config.form.formId),
+        this.languageInstanceService.getDefaultLanguage(this.config.form.formId),
+      ]).subscribe(([currentLanguage, defaultLanguage]) => {
+        this.fieldsSettingsService.translateContentTypeSettings(this.config, currentLanguage, defaultLanguage);
+      })
+    );
 
     this.templateVars$ = combineLatest([
       currentLanguage$,
@@ -59,6 +73,10 @@ export class FormCollapsibleComponent implements OnInit {
         return templateVars;
       }),
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   toggleCollapse() {
