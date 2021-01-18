09e789f58ca457815e9ac7cc7c8929a4876386bb
Petar Miličić <pagnodunadan@gmail.com>
2021-01-18 15:37:26 +0100

    Removed unused JsonPackage1 model #214

--- projects/edit/shared/models/json-format-v1/index.ts
+++ projects/edit/shared/models/json-format-v1/index.ts
@@ -3,7 +3,6 @@ export { AttributeDef1 } from './attribute-def1';
 export { Entity1 } from './entity1';
 // export { JsonContentType1 } from './json-content-type1';
 export { JsonItem1 } from './json-item1';
-// export { JsonPackage1 } from './json-package1';
 // export { JsonHeader1 } from './json-header1';
 export { Attribute1 } from './attribute1';
 export { Type1 } from './type1';
