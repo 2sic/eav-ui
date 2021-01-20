4774813d71510d4e61eb64ef921e4953e571a1cb
Petar Miličić <pagnodunadan@gmail.com>
2021-01-20 14:23:47 +0100

    Fixed indentation in AttributeDef1 and ContentType1 #214

--- projects/edit/shared/models/json-format-v1/content-type1.ts
+++ projects/edit/shared/models/json-format-v1/content-type1.ts
@@ -2,19 +2,19 @@ import { AttributeDef1 } from './attribute-def1';
 import { Entity1 } from './entity1';
 
 export class ContentType1 {
-    Attributes: AttributeDef1[];
-    Description: string;
-    Id: string;
-    Metadata: Entity1[];
-    Name: string;
-    Scope: string;
+  Attributes: AttributeDef1[];
+  Description: string;
+  Id: string;
+  Metadata: Entity1[];
+  Name: string;
+  Scope: string;
 
-    constructor(Id: string, Name: string, Scope: string, Description: string, Attributes: AttributeDef1[], Metadata: Entity1[]) {
-        this.Id = Id;
-        this.Name = Name;
-        this.Scope = Scope;
-        this.Description = Description;
-        this.Attributes = Attributes;
-        this.Metadata = Metadata;
-    }
+  constructor(Id: string, Name: string, Scope: string, Description: string, Attributes: AttributeDef1[], Metadata: Entity1[]) {
+    this.Id = Id;
+    this.Name = Name;
+    this.Scope = Scope;
+    this.Description = Description;
+    this.Attributes = Attributes;
+    this.Metadata = Metadata;
+  }
 }
