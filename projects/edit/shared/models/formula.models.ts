479e1bf2e0fbb7fd9f9c593895ca748ed766c821
Petar Miličić <pagnodunadan@gmail.com>
2021-02-25 15:44:37 +0100

    Moved formula models #211

--- projects/edit/shared/models/formula.models.ts
+++ projects/edit/shared/models/formula.models.ts
@@ -1,4 +1,16 @@
-import { FormulaFunction } from '../store/ngrx-data/item.models';
+import { FieldValue, FormValues } from '../../eav-item-dialog/item-edit-form/item-edit-form.models';
+
+export interface FormulaContext {
+  data: FormulaCtxData;
+}
+
+export interface FormulaCtxData {
+  name: string;
+  value: FieldValue;
+  form: FormValues;
+}
+
+export type FormulaFunction = (context: FormulaContext) => FieldValue;
 
 export type FormulaType = 'value' | 'visible' | 'required' | 'enabled';
 
