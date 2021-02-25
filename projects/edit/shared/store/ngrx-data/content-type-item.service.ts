ba541873a536381361fd1edf9b5f2b4c03177a6f
Petar Miličić <pagnodunadan@gmail.com>
2021-02-25 14:50:02 +0100

    Added BehaviorSubject to ContentTypeItemService #211

--- projects/edit/shared/store/ngrx-data/content-type-item.service.ts
+++ projects/edit/shared/store/ngrx-data/content-type-item.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from '@ngrx/data';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 import { EavEntity } from '../../models/eav';
 import { Entity1 } from '../../models/json-format-v1';
 
 @Injectable({ providedIn: 'root' })
 export class ContentTypeItemService extends EntityCollectionServiceBase<EavEntity> {
+  private contentTypeItems$: BehaviorSubject<EavEntity[]>;
+
   constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory) {
     super('ContentTypeItem', serviceElementsFactory);
+
+    this.contentTypeItems$ = new BehaviorSubject<EavEntity[]>([]);
+    // doesn't need to be completed because store services are singletons that lives as long as the browser tab is open
+    this.entities$.subscribe(contentTypeItems => {
+      this.contentTypeItems$.next(contentTypeItems);
+    });
   }
 
-  /** Add new content type items to the store */
-  addContentTypeItems(contentTypeItems1: Entity1[]) {
+  addContentTypeItems(contentTypeItems1: Entity1[]): void {
     const contentTypeItems = EavEntity.convertMany(contentTypeItems1);
     this.upsertManyInCache(contentTypeItems);
   }
 
-  /** Get content type item observable from the store */
-  getContentTypeItemByGuid(guid: string) {
-    return this.entities$.pipe(
-      map(contentTypeItems => contentTypeItems.find(contentTypeItem => contentTypeItem.Guid === guid))
-      // maybe add distinctUntilChanged()
-    );
+  getContentTypeItem(guid: string): EavEntity {
+    return this.contentTypeItems$.value.find(contentTypeItem => contentTypeItem.Guid === guid);
   }
 }
