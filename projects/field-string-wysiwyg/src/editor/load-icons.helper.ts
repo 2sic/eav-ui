dc8017d216a633b9d3010e39d8a7c4a7f82e63dd
Petar Miličić <pagnodunadan@gmail.com>
2021-03-15 16:00:53 +0100

    Moved wysiwyg and gps icons #211

--- projects/field-string-wysiwyg/src/editor/load-icons.helper.ts
+++ projects/field-string-wysiwyg/src/editor/load-icons.helper.ts
@@ -1,28 +1,25 @@
-// TODO: SPM - shouldn't we move these files into this folder?
-// I assume they are not actually shared with any other code
-
-import * as contentBlock from '../../../edit/assets/icons/2sxc/content-block.svg';
-import * as fileDnn from '../../../edit/assets/icons/2sxc/file-dnn.svg';
-import * as imageH1 from '../../../edit/assets/icons/2sxc/H1.svg';
-import * as imageH2 from '../../../edit/assets/icons/2sxc/H2.svg';
-import * as imageH3 from '../../../edit/assets/icons/2sxc/H3.svg';
-import * as imageH4 from '../../../edit/assets/icons/2sxc/H4.svg';
-import * as imageH5 from '../../../edit/assets/icons/2sxc/H5.svg';
-import * as imageH6 from '../../../edit/assets/icons/2sxc/H6.svg';
-import * as imageDnn from '../../../edit/assets/icons/2sxc/image-dnn.svg';
-import * as imageU1 from '../../../edit/assets/icons/2sxc/U1.svg';
-import * as imageU2 from '../../../edit/assets/icons/2sxc/U2.svg';
-import * as imageU3 from '../../../edit/assets/icons/2sxc/U3.svg';
-import * as imageU4 from '../../../edit/assets/icons/2sxc/U4.svg';
-import * as imageU5 from '../../../edit/assets/icons/2sxc/U5.svg';
-import * as imageU6 from '../../../edit/assets/icons/2sxc/U6.svg';
-import * as anchor from '../../../edit/assets/icons/font-awesome/anchor.svg';
 import * as filePdf from '../../../edit/assets/icons/font-awesome/file-pdf.svg';
 import * as file from '../../../edit/assets/icons/font-awesome/file.svg';
 import * as sitemap from '../../../edit/assets/icons/font-awesome/sitemap.svg';
-import * as school from '../../../edit/assets/icons/google-material/baseline-school-24px.svg';
-import * as paragraph from '../../../edit/assets/icons/tinymce/paragraph.svg';
 import { Dictionary } from '../../../ng-dialogs/src/app/shared/models/dictionary.model';
+import * as contentBlock from '../assets/icons/2sxc/content-block.svg';
+import * as fileDnn from '../assets/icons/2sxc/file-dnn.svg';
+import * as imageH1 from '../assets/icons/2sxc/H1.svg';
+import * as imageH2 from '../assets/icons/2sxc/H2.svg';
+import * as imageH3 from '../assets/icons/2sxc/H3.svg';
+import * as imageH4 from '../assets/icons/2sxc/H4.svg';
+import * as imageH5 from '../assets/icons/2sxc/H5.svg';
+import * as imageH6 from '../assets/icons/2sxc/H6.svg';
+import * as imageDnn from '../assets/icons/2sxc/image-dnn.svg';
+import * as imageU1 from '../assets/icons/2sxc/U1.svg';
+import * as imageU2 from '../assets/icons/2sxc/U2.svg';
+import * as imageU3 from '../assets/icons/2sxc/U3.svg';
+import * as imageU4 from '../assets/icons/2sxc/U4.svg';
+import * as imageU5 from '../assets/icons/2sxc/U5.svg';
+import * as imageU6 from '../assets/icons/2sxc/U6.svg';
+import * as anchor from '../assets/icons/font-awesome/anchor.svg';
+import * as school from '../assets/icons/google-material/baseline-school-24px.svg';
+import * as paragraph from '../assets/icons/tinymce/paragraph.svg';
 import { TinyType } from '../shared/models';
 
 const customTinyMceIcons: Dictionary<string> = {
@@ -50,10 +47,7 @@ const customTinyMceIcons: Dictionary<string> = {
 };
 
 export function loadCustomIcons(editor: TinyType) {
-  Object.keys(customTinyMceIcons).forEach(key => {
-    editor.ui.registry.addIcon(
-      key,
-      customTinyMceIcons[key]
-    );
+  Object.entries(customTinyMceIcons).forEach(([name, icon]) => {
+    editor.ui.registry.addIcon(name, icon);
   });
 }
