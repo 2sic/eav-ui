73e63d3782d222f9c0a3e21f5c756809b58487a1
Petar Miličić <pagnodunadan@gmail.com>
2021-08-24 15:19:00 +0200

    Fixed guid regex

--- projects/ng-dialogs/src/app/shared/constants/guid.constants.ts
+++ projects/ng-dialogs/src/app/shared/constants/guid.constants.ts
@@ -3,5 +3,5 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test#using_test_on_a_regex_with_the_global_flag
  */
 export function guidRegex(): RegExp {
-  return /[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}/i;
+  return /^[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}$/i;
 }
