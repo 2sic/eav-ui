1af60ceeedbe9acc29d8577c653f9371c974bf07
ijungleboy <daniel.mettler@2sic.com>
2022-07-05 16:44:28 +0200

    fix formulas error https://github.com/2sic/2sxc/issues/2810

--- projects/eav-ui/src/app/edit/shared/constants/formula.constants.ts
+++ projects/eav-ui/src/app/edit/shared/constants/formula.constants.ts
@@ -1,5 +1,8 @@
 export const requiredFormulaPrefix = 'function ';
 
+// 2022-07-05 2dm had to fix 'v1' because somehow a 'v1 ' was expected
+export const formV1Prefix = 'v1';
+
 export const defaultFormulaV1 = `${requiredFormulaPrefix}v1 (data, context) {
   return data.value;
 }`;
