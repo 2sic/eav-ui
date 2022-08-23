ab2e9ca5fcfcb6a22c1326eaff69b39154592a3b
ijungleboy <daniel.mettler@2sic.com>
2022-08-23 19:14:53 +0200

    Clean up commented code #reduceEnvVars

--- projects/eav-ui/src/app/shared/constants/session.constants.ts
+++ projects/eav-ui/src/app/shared/constants/session.constants.ts
@@ -1,9 +1,5 @@
 export const prefix = 'eav-';
 export const keyZoneId = prefix + 'zoneId';
-// #reduceEnvVars
-// export const keyRequestToken = prefix + 'rvt';
-// export const keyRequestTokenHeaderName = prefix + 'rvth';
-// export const keyTabId = prefix + 'tid';
 export const keyContentBlockId = prefix + 'cbid';
 export const keyModuleId = prefix + 'mid';
 export const keyAppId = prefix + 'appId';
@@ -15,12 +11,13 @@ export const keyPartOfPage = prefix + 'partOfPage';
 export const keyPublishing = prefix + 'publishing';
 export const keyFilters = prefix + 'filters';
 export const keyPipelineId = prefix + 'pipelineId';
-// #reduceEnvVars
-// export const keyApi = prefix + 'api';
+
 /** Url which opened the dialog. Used for debugging */
 export const keyUrl = prefix + 'url';
+
 /** This is used by file editor to determine if it's editing shared files or of that portal only */
 export const keyIsShared = prefix + 'isshared';
+
 /** Contains extra options for dialogs */
 export const keyExtras = prefix + 'extras';
 
