7e5f3ac4dbb9ad6afe9abce8050feb71132fae1a
Duje Vatavuk <duje@vatavuk.com>
2022-10-05 16:31:39 +0200

    moved icons from app.component to app-icons.service

--- projects/eav-ui/src/app/app.component.ts
+++ projects/eav-ui/src/app/app.component.ts
@@ -1,108 +1,13 @@
-import codeCurly from '!raw-loader!./assets/icons/code-curly.svg';
-import moreVert from '!raw-loader!@material-design-icons/svg/outlined/more_vert.svg';
-import moreHoriz from '!raw-loader!@material-design-icons/svg/outlined/more_horiz.svg';
-import filterCenterFocus from '!raw-loader!@material-design-icons/svg/outlined/filter_center_focus.svg';
-import close from '!raw-loader!@material-design-icons/svg/outlined/close.svg';
-import info from '!raw-loader!@material-design-icons/svg/outlined/info.svg';
-import menu from '!raw-loader!@material-design-icons/svg/outlined/menu.svg';
-import filterList from '!raw-loader!@material-design-icons/svg/outlined/filter_list.svg';
-import filterIcon from '!raw-loader!@material-design-icons/svg/outlined/filter.svg';
-import layers from '!raw-loader!@material-design-icons/svg/outlined/layers.svg';
-import offlineBolt from '!raw-loader!@material-design-icons/svg/outlined/offline_bolt.svg';
-import bolt from '!raw-loader!@material-design-icons/svg/outlined/bolt.svg';
-import settingsApplications from '!raw-loader!@material-design-icons/svg/outlined/settings_applications.svg';
-import settings from '!raw-loader!@material-design-icons/svg/outlined/settings.svg';
-import edit from '!raw-loader!@material-design-icons/svg/outlined/edit.svg';
-import editAttributes from '!raw-loader!@material-design-icons/svg/outlined/edit_attributes.svg';
-import dns from '!raw-loader!@material-design-icons/svg/outlined/dns.svg';
-import share from '!raw-loader!@material-design-icons/svg/outlined/share.svg';
-import add from '!raw-loader!@material-design-icons/svg/outlined/add.svg';
-import search from '!raw-loader!@material-design-icons/svg/outlined/search.svg';
-import holidayVillage from '!raw-loader!@material-design-icons/svg/outlined/holiday_village.svg';
-import cloudDownload from '!raw-loader!@material-design-icons/svg/outlined/cloud_download.svg';
-import cloudUpload from '!raw-loader!@material-design-icons/svg/outlined/cloud_upload.svg';
-import localOffer from '!raw-loader!@material-design-icons/svg/outlined/local_offer.svg';
-import clear from '!raw-loader!@material-design-icons/svg/outlined/clear.svg';
-import arrowBack from '!raw-loader!@material-design-icons/svg/outlined/arrow_back.svg';
-import person from '!raw-loader!@material-design-icons/svg/outlined/person.svg';
-import deleteIcon from '!raw-loader!@material-design-icons/svg/outlined/delete.svg';
-import visibility from '!raw-loader!@material-design-icons/svg/outlined/visibility.svg';
-import visibilityOff from '!raw-loader!@material-design-icons/svg/outlined/visibility_off.svg';
-import formatListNumbered from '!raw-loader!@material-design-icons/svg/outlined/format_list_numbered.svg';
-import checkBox from '!raw-loader!@material-design-icons/svg/outlined/check_box.svg';
-import checkBoxOutlineBlank from '!raw-loader!@material-design-icons/svg/outlined/check_box_outline_blank.svg';
-import looksOne from '!raw-loader!@material-design-icons/svg/outlined/looks_one.svg';
-import code from '!raw-loader!@material-design-icons/svg/outlined/code.svg';
-import fileCopy from '!raw-loader!@material-design-icons/svg/outlined/file_copy.svg';
-import lock from '!raw-loader!@material-design-icons/svg/outlined/lock.svg';
-import lockOpen from '!raw-loader!@material-design-icons/svg/outlined/lock_open.svg';
-import openInNew from '!raw-loader!@material-design-icons/svg/outlined/open_in_new.svg';
-import manageSearch from '!raw-loader!@material-design-icons/svg/outlined/manage_search.svg';
-import translate from '!raw-loader!@material-design-icons/svg/outlined/translate.svg';
-import starBorder from '!raw-loader!@material-design-icons/svg/outlined/star_border.svg';
-import tune from '!raw-loader!@material-design-icons/svg/outlined/tune.svg';
-import star from '!raw-loader!@material-design-icons/svg/outlined/star.svg';
-import starOutline from '!raw-loader!@material-design-icons/svg/outlined/star_outline.svg';
-import redeem from '!raw-loader!@material-design-icons/svg/outlined/redeem.svg';
-import importExport from '!raw-loader!@material-design-icons/svg/outlined/import_export.svg';
-import keyboardArrowUp from '!raw-loader!@material-design-icons/svg/outlined/keyboard_arrow_up.svg';
-import keyboardArrowDown from '!raw-loader!@material-design-icons/svg/outlined/keyboard_arrow_down.svg';
-import keyboardArrowRight from '!raw-loader!@material-design-icons/svg/outlined/keyboard_arrow_right.svg';
-import done from '!raw-loader!@material-design-icons/svg/outlined/done.svg';
-import doneAll from '!raw-loader!@material-design-icons/svg/outlined/done_all.svg';
-import textFields from '!raw-loader!@material-design-icons/svg/outlined/text_fields.svg';
-import toggleOn from '!raw-loader!@material-design-icons/svg/outlined/toggle_on.svg';
-import link from '!raw-loader!@material-design-icons/svg/outlined/link.svg';
-import arrowDropDown from '!raw-loader!@material-design-icons/svg/outlined/arrow_drop_down.svg';
-import arrowDropUp from '!raw-loader!@material-design-icons/svg/outlined/arrow_drop_up.svg';
-import history from '!raw-loader!@material-design-icons/svg/outlined/history.svg';
-import historyToggleOff from '!raw-loader!@material-design-icons/svg/outlined/history_toggle_off.svg';
-import restore from '!raw-loader!@material-design-icons/svg/outlined/restore.svg';
-import moreTime from '!raw-loader!@material-design-icons/svg/outlined/more_time.svg';
-import circle from '!raw-loader!@material-design-icons/svg/outlined/circle.svg';
-import checkCircle from '!raw-loader!@material-design-icons/svg/outlined/check_circle.svg';
-import checkCircleOutline from '!raw-loader!@material-design-icons/svg/outlined/check_circle_outline.svg';
-import warning from '!raw-loader!@material-design-icons/svg/outlined/warning.svg';
-import menuBook from '!raw-loader!@material-design-icons/svg/outlined/menu_book.svg';
-import sort from '!raw-loader!@material-design-icons/svg/outlined/sort.svg';
-import accountCircle from '!raw-loader!@material-design-icons/svg/outlined/account_circle.svg';
-import adjust from '!raw-loader!@material-design-icons/svg/outlined/adjust.svg';
-import fiberManualRecord from '!raw-loader!@material-design-icons/svg/outlined/fiber_manual_record.svg';
-import bugReport from '!raw-loader!@material-design-icons/svg/outlined/bug_report.svg';
-import militaryTech from '!raw-loader!@material-design-icons/svg/outlined/military_tech.svg';
-import functions from '!raw-loader!@material-design-icons/svg/outlined/functions.svg';
-import speed from '!raw-loader!@material-design-icons/svg/outlined/speed.svg';
-import mobiledataOff from '!raw-loader!@material-design-icons/svg/outlined/mobiledata_off.svg';
-import foundation from '!raw-loader!@material-design-icons/svg/outlined/foundation.svg';
-import cached from '!raw-loader!@material-design-icons/svg/outlined/cached.svg';
-import cancel from '!raw-loader!@material-design-icons/svg/outlined/cancel.svg';
-import downloadForOffline from '!raw-loader!@material-design-icons/svg/outlined/download_for_offline.svg';
-import appRegistration from '!raw-loader!@material-design-icons/svg/outlined/app_registration.svg';
-import publicIcon from '!raw-loader!@material-design-icons/svg/outlined/public.svg';
-import emojiObjects from '!raw-loader!@material-design-icons/svg/outlined/emoji_objects.svg';
-import permDeviceInformation from '!raw-loader!@material-design-icons/svg/outlined/perm_device_information.svg';
-import helpOutline from '!raw-loader!@material-design-icons/svg/outlined/help_outline.svg';
-import help from '!raw-loader!@material-design-icons/svg/outlined/help.svg';
-import save from '!raw-loader!@material-design-icons/svg/outlined/save.svg';
-import playArrow from '!raw-loader!@material-design-icons/svg/outlined/play_arrow.svg';
-import dragHandle from '!raw-loader!@material-design-icons/svg/outlined/drag_handle.svg';
-import language from '!raw-loader!@material-design-icons/svg/outlined/language.svg';
 import { Context as DnnContext, DnnAppComponent } from '@2sic.com/dnn-sxc-angular';
 import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer, Title } from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter, map, Subscription } from 'rxjs';
+import { AppIconsService } from './shared/services/app-icons.service';
 import { keyContentBlockId, keyModuleId } from './shared/constants/session.constants';
 import { Context } from './shared/services/context';
 
-//Icons only used in metadata recomendation
-import thumbUp from '!raw-loader!@material-design-icons/svg/outlined/thumb_up.svg';
-import stickyNote2 from '!raw-loader!@material-design-icons/svg/outlined/sticky_note_2.svg';
-import policy from '!raw-loader!@material-design-icons/svg/outlined/policy.svg';
-import notInterested from '!raw-loader!@material-design-icons/svg/outlined/not_interested.svg';
-import loyalty from '!raw-loader!@material-design-icons/svg/outlined/loyalty.svg';
-import cropFree from '!raw-loader!@material-design-icons/svg/outlined/crop_free.svg';
+
 
 @Component({
   selector: 'app-root',
@@ -120,8 +25,7 @@ export class AppComponent extends DnnAppComponent implements OnInit, OnDestroy {
     private titleService: Title,
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
+    private appIconsService: AppIconsService,
   ) {
     super(
       el,
@@ -131,7 +35,7 @@ export class AppComponent extends DnnAppComponent implements OnInit, OnDestroy {
       }),
     );
     this.context.initRoot();
-    this.loadFonts();
+    this.appIconsService.load();
   }
 
   ngOnInit() {
@@ -159,118 +63,5 @@ export class AppComponent extends DnnAppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }
-
-  private loadFonts() {
-    this.matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
-
-    const icons: Record<string, string> = {
-      'code-curly': codeCurly,
-    };
-
-    const iconsMaterial: Record<string, string> = {
-      'account-circle': accountCircle,
-      add,
-      adjust,
-      'app-regostration': appRegistration,
-      'arrow-back': arrowBack,
-      'arrow-drop-down': arrowDropDown,
-      'arrow-drop-up': arrowDropUp,
-      bolt,
-      'bug-report': bugReport,
-      cached,
-      cancel,
-      'check-box-outline-blank': checkBoxOutlineBlank,
-      'check-box': checkBox,
-      'check-circle-outline': checkCircleOutline,
-      'check-circle': checkCircle,
-      circle,
-      clear,
-      close,
-      'cloud-download': cloudDownload,
-      'cloud-upload': cloudUpload,
-      code,
-      'delete': deleteIcon,
-      dns,
-      'done-all': doneAll,
-      done,
-      'download-for-offline': downloadForOffline,
-      'drag-handle': dragHandle,
-      'edit-attributes': editAttributes,
-      edit,
-      'emoji-objects': emojiObjects,
-      'fiber-manual-record': fiberManualRecord,
-      'file-copy': fileCopy,
-      'filter-center-focus': filterCenterFocus,
-      'filter-list': filterList,
-      'filter': filterIcon,
-      'format-list-numbered': formatListNumbered,
-      foundation,
-      functions,
-      'help-outline': helpOutline,
-      help,
-      'history-toggle-off': historyToggleOff,
-      history,
-      'holiday-village': holidayVillage,
-      'import-export': importExport,
-      info,
-      'keyboard-arrow-down': keyboardArrowDown,
-      'keyboard-arrow-right': keyboardArrowRight,
-      'keyboard-arrow-up': keyboardArrowUp,
-      language,
-      layers,
-      link,
-      'local-offer': localOffer,
-      'looks-one': looksOne,
-      'lock-open': lockOpen,
-      lock,
-      'manage-search': manageSearch,
-      'menu-book': menuBook,
-      menu,
-      'military-tech': militaryTech,
-      'mobiledata-off': mobiledataOff,
-      'more-horiz': moreHoriz,
-      'more-time': moreTime,
-      'more-vert': moreVert,
-      'offline-bolt': offlineBolt,
-      'open-in-new': openInNew,
-      'perm-device-information': permDeviceInformation,
-      person,
-      'play-arrow': playArrow,
-      'public': publicIcon,
-      redeem,
-      restore,
-      save,
-      search,
-      'settings-applications': settingsApplications,
-      settings,
-      share,
-      sort,
-      speed,
-      'star-border': starBorder,
-      'star-outline': starOutline,
-      star,
-      'visibility-off': visibilityOff,
-      'text-fields': textFields,
-      'toggle-on': toggleOn,
-      translate,
-      'tune': tune,
-      visibility,
-      warning,
-
-      //Icons only used in metadata recomendation
-      'crop-free': cropFree,
-      loyalty,
-      'not-interested': notInterested,
-      policy,
-      'sticky-note-2': stickyNote2,
-      'thumb-up': thumbUp,
-    };
-    Object.entries(icons).forEach(([name, svg]) => {
-      this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
-    });
-    Object.entries(iconsMaterial).forEach(([name, svg]) => {
-      this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
-    });
   }
 }
