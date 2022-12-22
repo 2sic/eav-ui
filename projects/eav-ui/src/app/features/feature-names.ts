fd5ec15da4fa50fd53b9878eb4c0b573e77b8883
ijungleboy <daniel.mettler@2sic.com>
2022-12-22 14:53:21 +0100

    minor

---
+++ projects/eav-ui/src/app/features/feature-names.ts
@@ -0,0 +1,6 @@
+// TODO: @SDV - USE THIS
+// This will make it easier for us to see where certain feature names are in use
+
+export const FeatureNames = {
+  AppSyncWithSiteFiles: 'AppSyncWithSiteFiles',
+};
