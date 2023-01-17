0a7d623ace0e5c7b60e775a0fffe6fda6af0dd78
ijungleboy <daniel.mettler@2sic.com>
2023-01-17 13:56:02 +0100

    improve toolbar config in wysiwyg

--- projects/field-string-wysiwyg/src/config/toolbars.ts
+++ projects/field-string-wysiwyg/src/config/toolbars.ts
@@ -4,101 +4,129 @@ import { TinyEavButtons, TinyEavConfig } from './tinymce-config';
 // tslint:disable-next-line: max-line-length
 import { TinyMceMode, TinyMceModeWithSwitcher, ToolbarSwitcher, WysiwygAdvanced, WysiwygDefault, WysiwygDialog, WysiwygInline, WysiwygMode, WysiwygView } from './tinymce-helper-types';
 
-type ButtonSet = Record<WysiwygMode, string>;
-type ButtonSetWithDialog = Record<WysiwygMode | 'dialog', string>;
-type ButtonSetConfig = Record<WysiwygMode, boolean>;
+type ButtonSet = Record<WysiwygMode, string | boolean>;
+type ButtonSetByView = Record<WysiwygView | 'default', ButtonSet>;
 const NoButtons = ''; // must be empty string
 
-const Bs1Intro: ButtonSet = {
+function expandSet(original: Partial<ButtonSet>): ButtonSetByView {
+  return expandSetByView({ default: original });
+}
+
+function expandSetByView(original: Partial<Record<WysiwygView | 'default', Partial<ButtonSet>>>): ButtonSetByView {
+  const origSet = original.default;
+  const defSet = buildSet(origSet.default, origSet);
+  return {
+    default: defSet,
+    inline: { ...defSet, ...original.inline },
+    dialog: { ...defSet, ...original.dialog }
+  };
+}
+
+function buildSet(def: string | boolean, defSet: Partial<ButtonSet>): ButtonSet {
+  return {
+    default: def,
+    advanced: defSet.advanced ?? def,
+    text: defSet.text ?? def,
+    media: defSet.media ?? def,
+  };
+}
+
+const BSet1Intro = expandSet({
   // #v1500-not-ready
   // default: ` ${ToolbarModeToggle} undo redo pastetext`,
   // advanced: ` ${ToolbarModeToggle} undo pastetext`,
   default: `  undo redo pastetext paste removeformat`,
-  advanced: ` undo redo pastetext paste removeformat`,
-  text: ` ${ToolbarModeToggle} undo redo pastetext paste removeformat`,
-  media: ` ${ToolbarModeToggle} undo pastimage-todo `  // TODO: create pasteimage
-};
+  // advanced: ` undo redo pastetext paste removeformat`,
+  text: `${ToolbarModeToggle} undo redo pastetext paste removeformat`,
+  media: `${ToolbarModeToggle} undo pastimage-todo `  // TODO: create pasteimage
+});
 
-const Bs2Format: ButtonSet = {
+const BSet2Format = expandSet({
   default: `bold ${ItalicWithMore}`,
   advanced: `styles bold ${ItalicWithMore}`,
   text: `bold ${ItalicWithMore}`,
   media: NoButtons,
-};
+});
 
-const Bs3Headings: ButtonSet = {
+const BSet3Headings = expandSet({
   default: `h2 ${HGroups.h3}`,
-  advanced: `h2 ${HGroups.h3}`,
+  // advanced: `h2 ${HGroups.h3}`,
   text: `h2 h3 ${HGroups.h4}`,
   media: NoButtons,
-};
+});
 
-const Bs4NumList: ButtonSet = {
+const BSet4NumList = expandSet({
   default: `numlist ${ListGroup}`,
-  advanced: `numlist ${ListGroup}`,
+  // advanced: `numlist ${ListGroup}`,
   text: 'numlist bullist outdent indent',
   media: NoButtons
-};
+});
 
-const Bs5Links: ButtonSetWithDialog = {
-  default: `${LinkGroup}`,
-  dialog: `${SxcImages} ${LinkGroupPro}`,
-  advanced: `${LinkGroupPro}`, // test
-  text: `${LinkGroup}`,
-  media: `${SxcImages} ${LinkFiles}`
-};
+const BSet5Links = expandSetByView({
+  default: {
+    default: `${LinkGroup}`,
+    advanced: `${LinkGroupPro}`, // test
+    // text: `${LinkGroup}`,
+    media: `${SxcImages} ${LinkFiles}`,
+  },
+  dialog: {
+    default: `${SxcImages} ${LinkGroupPro}`,
+    advanced: `${SxcImages} ${LinkGroupPro}`,
+  }
+});
 
-const Bs6RichMedia: ButtonSetConfig = {
+const BSet6RichMedia = expandSet({
   default: false,
   advanced: false,
   text: false,
   media: true,
-};
+});
 
-const Bs7ContentBlocks: ButtonSetConfig = {
+const BSet7ContentBlocks = expandSet({
   default: true,
   advanced: false,
   text: false,
   media: true,
-};
+});
 
-const Bs91Code = 'code';
-const Bs92Advanced: ButtonSet = {
+const BSet91Code = 'code';
+const BSet92Advanced = expandSet({
   default: ModeAdvanced,
   advanced: NoButtons,
   text: NoButtons,
   media: NoButtons,
-};
-const Bs93CloseAdvanced: ButtonSet = {
+});
+const BSet93CloseAdvanced = expandSet({
   default: NoButtons,
   advanced: ModeDefault,
   text: NoButtons,
   media: NoButtons,
-};
-const Bs99FullScreen: ButtonSet = {
-  default: ToFullscreen,
-  advanced: ToFullscreen,
-  text: NoButtons,
-  media: NoButtons,
-};
+});
+const BSet99FullScreen = expandSetByView({
+  default: {
+    default: ToFullscreen,
+    advanced: NoButtons,
+    text: NoButtons,
+    media: NoButtons,
+  },
+  dialog: {
+    default: NoButtons,
+  }
+});
 
-const BsContextMenu: ButtonSet = {
+const ButtonSetContextMenu = expandSet({
   default: 'charmap hr',
-  advanced: 'charmap hr',
+  // advanced: 'charmap hr',
   text: 'charmap hr | link',
   media: 'charmap hr | link image'
-};
+});
 
 
-function selectFromSet(set: ButtonSet | ButtonSetConfig, mode: WysiwygMode, view: WysiwygView) {
-  if (view == 'dialog')
-  {
-    var found = (set as ButtonSetWithDialog)?.['dialog'];
-    if (found) return found;
-  }
+
+function selectFromSet(groups: ButtonSetByView, mode: WysiwygMode, view: WysiwygView) {
+  const set = groups[view] ?? groups.default;
   return set?.[mode];
 }
-
 export class TinyMceToolbars implements ToolbarSwitcher {
 
   constructor(private config: TinyEavConfig) {
@@ -122,7 +150,7 @@ export class TinyMceToolbars implements ToolbarSwitcher {
       },
       menubar: mode === WysiwygAdvanced,
       toolbar: this.toolbar(mode, view, config),
-      contextmenu: selectFromSet(BsContextMenu, WysiwygAdvanced, view)
+      contextmenu: selectFromSet(ButtonSetContextMenu, WysiwygAdvanced, view)
         + (this.config.features.contentBlocks ? ` ${AddContentBlock} ` : ''),
     };
   }
@@ -130,20 +158,19 @@ export class TinyMceToolbars implements ToolbarSwitcher {
   private toolbar(mode: WysiwygMode, view: WysiwygView, config: TinyEavButtons): string {
 // console.log('2dm advanced', config.advanced, config, mode);
     const list = [
-      selectFromSet(Bs1Intro, mode, view),
-      selectFromSet(Bs2Format, mode, view),
-      selectFromSet(Bs3Headings, mode, view),
-      selectFromSet(Bs4NumList, mode, view),
-      selectFromSet(Bs5Links, mode, view),
-      (selectFromSet(Bs6RichMedia, mode, view) ? this.richContent() : null),
-      (selectFromSet(Bs7ContentBlocks, mode, view) ? this.contentBlocks() : null),
+      selectFromSet(BSet1Intro, mode, view),
+      selectFromSet(BSet2Format, mode, view),
+      selectFromSet(BSet3Headings, mode, view),
+      selectFromSet(BSet4NumList, mode, view),
+      selectFromSet(BSet5Links, mode, view),
+      (selectFromSet(BSet6RichMedia, mode, view) ? this.richContent() : null),
+      (selectFromSet(BSet7ContentBlocks, mode, view) ? this.contentBlocks() : null),
       [
-        config.source && Bs91Code,
-        config.advanced && selectFromSet(Bs92Advanced, mode, view),
-        config.advanced && selectFromSet(Bs93CloseAdvanced, mode, view),
+        config.source && BSet91Code,
+        config.advanced && selectFromSet(BSet92Advanced, mode, view),
+        config.advanced && selectFromSet(BSet93CloseAdvanced, mode, view),
         // must only allow full screen if allowed
-        view !== 'dialog' && selectFromSet(Bs99FullScreen, mode, view),
-
+        selectFromSet(BSet99FullScreen, mode, view),
       ].join(' '),
     ];
     return list.join(' | ');
