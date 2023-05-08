af9764a6bcfa3a5926aa8117a62f2218dd4e6b09
Duje Vatavuk <duje@vatavuk.com>
2023-05-08 12:15:09 +0200

    create FormulaValueCorrections from FormulaEngine

--- projects/eav-ui/src/app/edit/formulas/formula-engine.ts
+++ projects/eav-ui/src/app/edit/formulas/formula-engine.ts
@@ -2,8 +2,6 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { FieldSettings, FieldValue } from 'projects/edit-types';
 import { BehaviorSubject, Subscription } from 'rxjs';
-import { DataTypeConstants } from '../../content-type-fields/constants/data-type.constants';
-import { InputTypeConstants } from '../../content-type-fields/constants/input-type.constants';
 import { InputType } from '../../content-type-fields/models/input-type.model';
 import { FeatureSummary } from '../../features/models';
 import { FeaturesService } from '../../shared/services/features.service';
@@ -15,12 +13,13 @@ import { GlobalConfigService, ItemService, LanguageInstanceService, LanguageServ
 import { FormulaDesignerService } from './formula-designer.service';
 import { FormulaHelpers } from './formula.helpers';
 // tslint:disable-next-line: max-line-length
-import { FieldSettingPair, FieldValuePair, FormulaCacheItem, FormulaFieldValidation, FormulaFunctionDefault, FormulaFunctionV1, FormulaResultRaw, FormulaTarget, FormulaTargets, FormulaVersions, RunFormulasResult, SettingsFormulaPrefix } from './formula.models';
+import { FieldSettingPair, FieldValuePair, FormulaCacheItem, FormulaFieldValidation, FormulaFunctionDefault, FormulaFunctionV1, FormulaResultRaw, FormulaTargets, FormulaVersions, RunFormulasResult, SettingsFormulaPrefix } from './formula.models';
 import { FormulaSettingsHelper } from './formula-settings.helper';
 import { FieldLogicBase } from '../form/shared/field-logic/field-logic-base';
 import { FieldLogicTools } from '../form/shared/field-logic/field-logic-tools';
 import { ConstantFieldParts } from './constant-field-parts';
 import { consoleLogAngular } from '../../shared/helpers/console-log-angular.helper';
+import { FormulaValueCorrections } from './formula-value-corrections.helper';
 
 // TODO: @stv
 // We must refactor again, because this is much too big
@@ -188,8 +187,6 @@ export class FormulaEngine implements OnDestroy {
         continue;
       }
 
-      // { "Disab"}
-
       FormulaSettingsHelper.keepSettingsIfTypeMatches(formula.target, settingsCurrent, formulaResult.value, settingsNew);
     }
 
@@ -237,7 +234,7 @@ export class FormulaEngine implements OnDestroy {
         queue[entityGuid] = queue[entityGuid] ?? { possibleValueUpdates: {}, possibleFieldsUpdates: [], possibleSettingUpdate: [] };
         let possibleValueUpdates: FormValues = {};
         let possibleSettingUpdate: FieldSettingPair[] = [];
-        const corrected = this.correctAllValues(formula.target, result, inputType);
+        const corrected = FormulaValueCorrections.correctAllValues(formula.target, result, inputType);
 
         if (formula.target === FormulaTargets.Value) {
           possibleValueUpdates = queue[entityGuid].possibleValueUpdates ?? {};
@@ -315,7 +312,7 @@ export class FormulaEngine implements OnDestroy {
             || !formulaV1Result) {
             if (formula.target === FormulaTargets.Value) {
               const valueV1 = {
-                value: this.valueCorrection(formulaV1Result as FieldValue, inputType),
+                value: FormulaValueCorrections.valueCorrection(formulaV1Result as FieldValue, inputType),
                 fields: [], stop: null, openInDesigner: isOpenInDesigner
               } as FormulaResultRaw;
               this.formulaDesignerService.sendFormulaResultToUi(
@@ -337,7 +334,7 @@ export class FormulaEngine implements OnDestroy {
             console.log(`Running formula${formula.version.toLocaleUpperCase()} for Entity: "${ctSettings._itemTitle}", Field: "${formula.fieldName}", Target: "${formula.target}" with following arguments:`, formulaProps);
           }
           const formulaV2Result = (formula.fn as FormulaFunctionV1)(formulaProps.data, formulaProps.context, formulaProps.experimental);
-          const valueV2 = this.correctAllValues(formula.target, formulaV2Result, inputType);
+          const valueV2 = FormulaValueCorrections.correctAllValues(formula.target, formulaV2Result, inputType);
           valueV2.openInDesigner = isOpenInDesigner;
           if (valueV2.value === undefined && valueV2.promise)
             this.formulaDesignerService.sendFormulaResultToUi(
@@ -354,7 +351,7 @@ export class FormulaEngine implements OnDestroy {
             console.log(`Running formula for Entity: "${ctSettings._itemTitle}", Field: "${formula.fieldName}", Target: "${formula.target}" with following arguments:`, undefined);
           }
           const formulaDefaultResult = (formula.fn as FormulaFunctionDefault)();
-          const valueDefault = this.correctAllValues(formula.target, formulaDefaultResult, inputType);
+          const valueDefault = FormulaValueCorrections.correctAllValues(formula.target, formulaDefaultResult, inputType);
           valueDefault.openInDesigner = isOpenInDesigner;
           this.formulaDesignerService.sendFormulaResultToUi(
             formula.entityGuid, formula.fieldName, formula.target, valueDefault.value, false, false);
@@ -383,59 +380,5 @@ export class FormulaEngine implements OnDestroy {
       && designerState.fieldName === formula.fieldName
       && designerState.target === formula.target;
     return isOpenInDesigner;
-  }
-
-  // TODO: @STV - move to class FormulaValueCorrections with method "correctAllValues"
-  private correctAllValues(target: FormulaTarget, result: FieldValue | FormulaResultRaw, inputType: InputType): FormulaResultRaw {
-    const stop = (result as FormulaResultRaw)?.stop ?? null;
-    if (result === null || result === undefined)
-      return { value: result as FieldValue, fields: [], stop };
-    if (typeof result === 'object') {
-      if (result instanceof Date && target === FormulaTargets.Value)
-        return { value: this.valueCorrection(result as FieldValue, inputType), fields: [], stop };
-      if (result instanceof Promise)
-        return { value: undefined, promise: result as Promise<FormulaResultRaw>, fields: [], stop };
-      const corrected: FormulaResultRaw = (result as FormulaResultRaw);
-      corrected.stop = stop;
-      if ((result as FormulaResultRaw).value && target === FormulaTargets.Value) {
-        corrected.value = this.valueCorrection((result as FormulaResultRaw).value, inputType);
-      }
-      if ((result as FormulaResultRaw).fields) {
-        corrected.fields = (result as FormulaResultRaw).fields?.map((fields) => {
-          fields.value = this.valueCorrection(fields.value, inputType);
-          return fields;
-        });
-        return corrected;
-      }
-      return corrected;
-    }
-    const value: FormulaResultRaw = { value: result as FieldValue };
-
-    // atm we are only correcting Value formulas
-    if (target === FormulaTargets.Value) {
-      return { value: this.valueCorrection(value.value, inputType), fields: [], stop };
-    }
-    return value;
-  }
-
-  // TODO: @STV - move to class FormulaValueCorrections with private method "correctValue"
-  private valueCorrection(value: FieldValue, inputType: InputType): FieldValue {
-    if (value == null) {
-      return value;
-    } else if (inputType?.Type === InputTypeConstants.DatetimeDefault) {
-      const date = new Date(value as string | number | Date);
-
-      // if value is not ISO string, nor milliseconds, correct timezone
-      if (!(typeof value === 'string' && value.endsWith('Z')) && date.getTime() !== value) {
-        date.setTime(date.getTime() - date.getTimezoneOffset() * 60000);
-      }
-
-      date.setMilliseconds(0);
-      return date.toJSON();
-    } else if (typeof (value) !== 'string' && (inputType?.Type?.startsWith(DataTypeConstants.String.toLocaleLowerCase())
-      || inputType?.Type?.startsWith(DataTypeConstants.Hyperlink.toLocaleLowerCase()))) {
-      return value.toString();
-    }
-    return value;
   }
 }
---
+++ projects/eav-ui/src/app/edit/formulas/formula-value-corrections.helper.ts
@@ -0,0 +1,60 @@
+import { FieldValue } from "projects/edit-types";
+import { DataTypeConstants } from "../../content-type-fields/constants/data-type.constants";
+import { InputTypeConstants } from "../../content-type-fields/constants/input-type.constants";
+import { InputType } from "../../content-type-fields/models/input-type.model";
+import { FormulaTarget, FormulaResultRaw, FormulaTargets } from "./formula.models";
+
+export class FormulaValueCorrections {
+  
+  static correctAllValues(target: FormulaTarget, result: FieldValue | FormulaResultRaw, inputType: InputType): FormulaResultRaw {
+    const stop = (result as FormulaResultRaw)?.stop ?? null;
+    if (result === null || result === undefined)
+      return { value: result as FieldValue, fields: [], stop };
+    if (typeof result === 'object') {
+      if (result instanceof Date && target === FormulaTargets.Value)
+        return { value: this.valueCorrection(result as FieldValue, inputType), fields: [], stop };
+      if (result instanceof Promise)
+        return { value: undefined, promise: result as Promise<FormulaResultRaw>, fields: [], stop };
+      const corrected: FormulaResultRaw = (result as FormulaResultRaw);
+      corrected.stop = stop;
+      if ((result as FormulaResultRaw).value && target === FormulaTargets.Value) {
+        corrected.value = this.valueCorrection((result as FormulaResultRaw).value, inputType);
+      }
+      if ((result as FormulaResultRaw).fields) {
+        corrected.fields = (result as FormulaResultRaw).fields?.map((fields) => {
+          fields.value = this.valueCorrection(fields.value, inputType);
+          return fields;
+        });
+        return corrected;
+      }
+      return corrected;
+    }
+    const value: FormulaResultRaw = { value: result as FieldValue };
+
+    // atm we are only correcting Value formulas
+    if (target === FormulaTargets.Value) {
+      return { value: this.valueCorrection(value.value, inputType), fields: [], stop };
+    }
+    return value;
+  }
+
+  static valueCorrection(value: FieldValue, inputType: InputType): FieldValue {
+  if (value == null) {
+    return value;
+  } else if (inputType?.Type === InputTypeConstants.DatetimeDefault) {
+    const date = new Date(value as string | number | Date);
+
+    // if value is not ISO string, nor milliseconds, correct timezone
+    if (!(typeof value === 'string' && value.endsWith('Z')) && date.getTime() !== value) {
+      date.setTime(date.getTime() - date.getTimezoneOffset() * 60000);
+    }
+
+    date.setMilliseconds(0);
+    return date.toJSON();
+  } else if (typeof (value) !== 'string' && (inputType?.Type?.startsWith(DataTypeConstants.String.toLocaleLowerCase())
+    || inputType?.Type?.startsWith(DataTypeConstants.Hyperlink.toLocaleLowerCase()))) {
+    return value.toString();
+  }
+  return value;
+}
+}
