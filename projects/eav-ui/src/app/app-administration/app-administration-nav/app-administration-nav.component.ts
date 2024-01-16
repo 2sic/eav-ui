7cbf4d6b4b3357fcaa5339356d2271dc3723eb1f
Dominik Graf <dominik.graf@2sic.com>
2024-01-16 11:49:36 +0100

    wip

--- projects/eav-ui/src/app/app-administration/app-administration-nav/app-administration-nav.component.ts
+++ projects/eav-ui/src/app/app-administration/app-administration-nav/app-administration-nav.component.ts
@@ -11,38 +11,41 @@ import { DialogSettings } from '../../shared/models/dialog-settings.model';
 import { AppDialogConfigService } from '../services/app-dialog-config.service';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MediaMatcher } from '@angular/cdk/layout';
-import tippy from 'tippy.js';
 
 @Component({
   selector: 'app-app-administration-nav',
   templateUrl: './app-administration-nav.component.html',
   styleUrls: ['./app-administration-nav.component.scss'],
 })
-export class AppAdministrationNavComponent extends BaseComponent implements OnInit, OnDestroy {
+export class AppAdministrationNavComponent
+  extends BaseComponent
+  implements OnInit, OnDestroy
+{
   AppScopes = AppScopes;
-
 
   private dialogSettings$ = new BehaviorSubject<DialogSettings>(undefined);
   private tabs$ = new BehaviorSubject<string[]>(undefined);
   private tabIndex$ = combineLatest([
     this.tabs$,
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
+      filter((event) => event instanceof NavigationEnd),
       map(() => this.route.snapshot.firstChild.url[0].path),
-      startWith(this.route.snapshot.firstChild.url[0].path),
-    )
+      startWith(this.route.snapshot.firstChild.url[0].path)
+    ),
   ]).pipe(
     map(([tabs, path]) => {
-      if (tabs == null) { return; }
+      if (tabs == null) return;
+      console.error('tabs', path);
+      this.changeUrl(path);
       const tabIndex = tabs.indexOf(path);
       return tabIndex;
     }),
-    filter(tabIndex => tabIndex >= 0),
+    filter((tabIndex) => tabIndex >= 0)
   );
   viewModel$ = combineLatest([this.dialogSettings$, this.tabIndex$]).pipe(
     map(([dialogSettings, tabIndex]) => {
       return { dialogSettings, tabIndex };
-    }),
+    })
   );
 
   smallScreen: MediaQueryList = this.media.matchMedia('(max-width: 1000px)');
@@ -50,14 +53,30 @@ export class AppAdministrationNavComponent extends BaseComponent implements OnIn
   sideNavOpened = !this.smallScreen.matches;
 
   currentRoute: string;
+  isOpenMenu = false;
 
   navItems = [
     { name: 'Info', path: 'home', icon: 'info', tippy: 'App Info' },
-    { name: 'Data', path: 'data' , icon: 'menu', tippy: 'Data / Content'  },
-    { name: 'Queries', path: 'queries', icon: 'filter_list', tippy: 'Queries / Visual Query Designer'  },
-    { name: 'Views', path: 'views', icon: 'layers', tippy: 'Views / Templates'  },
-    { name: 'Web API', path: 'web-api', icon: 'offline_bolt', tippy: 'WebApi'  },
-    { name: 'App', path: 'app', icon: 'settings_applications', tippy: 'App Settings'  },
+    { name: 'Data', path: 'data', icon: 'menu', tippy: 'Data / Content' },
+    {
+      name: 'Queries',
+      path: 'queries',
+      icon: 'filter_list',
+      tippy: 'Queries / Visual Query Designer',
+    },
+    {
+      name: 'Views',
+      path: 'views',
+      icon: 'layers',
+      tippy: 'Views / Templates',
+    },
+    { name: 'Web API', path: 'web-api', icon: 'offline_bolt', tippy: 'WebApi' },
+    {
+      name: 'App',
+      path: 'app',
+      icon: 'settings_applications',
+      tippy: 'App Settings',
+    },
     { name: 'Sync', path: 'sync', icon: 'sync', tippy: 'App Export / Import' },
   ];
 
@@ -66,19 +85,39 @@ export class AppAdministrationNavComponent extends BaseComponent implements OnIn
     protected route: ActivatedRoute,
     private dialogRef: MatDialogRef<AppAdministrationNavComponent>,
     private appDialogConfigService: AppDialogConfigService,
-    private media: MediaMatcher,
+    private media: MediaMatcher
   ) {
     super(router, route);
   }
 
   ngOnInit() {
     this.fetchDialogSettings();
-    this.subscription.add(this.refreshOnChildClosedDeep().subscribe(() => { this.fetchDialogSettings(); }));
+    this.subscription.add(
+      this.refreshOnChildClosedDeep().subscribe(() => {
+        this.fetchDialogSettings();
+      })
+    );
 
     this.smallScreen.addEventListener(
       'change',
       (c) => (this.sidenav.opened = !c.matches)
     );
+
+    // this.router.events.subscribe((event: object) => {
+    //   if (
+    //     event instanceof NavigationEnd &&
+    //     this.navItems?.some((child) => {
+    //       return (
+    //         Array.isArray(child.path) &&
+    //         child.path.includes(event.urlAfterRedirects.split('/')[1])
+    //       );
+    //     })
+    //   ) {
+    //     console.error('cxx' );
+    //     this.changeUrl(event.urlAfterRedirects.split('/')[1]);
+    //     // this.isOpenMenu = true;
+    //   }
+    // });
   }
 
   ngOnDestroy() {
@@ -100,24 +139,25 @@ export class AppAdministrationNavComponent extends BaseComponent implements OnIn
   }
 
   changeUrl(path: string) {
-    // let path = this.tabs$.value[index];
     this.currentRoute = path;
-    console.error(path);
-    if (path === 'data') {
+    if (path === 'data')
       path = `data/${eavConstants.scopes.default.value}`;
-    }
     this.router.navigate([path], { relativeTo: this.route });
   }
 
   private fetchDialogSettings() {
-    this.appDialogConfigService.getShared$()/*.getDialogSettings()*/.subscribe(dialogSettings => {
-      UpdateEnvVarsFromDialogSettings(dialogSettings.Context.App);
-      this.dialogSettings$.next(dialogSettings);
+    this.appDialogConfigService
+      .getShared$() /*.getDialogSettings()*/
+      .subscribe((dialogSettings) => {
+        UpdateEnvVarsFromDialogSettings(dialogSettings.Context.App);
+        this.dialogSettings$.next(dialogSettings);
 
-      if (!dialogSettings.Context.Enable.Query)
-        this.navItems = this.navItems.filter(item => item.name !== 'Queries' && item.name !== 'Web API');
+        if (!dialogSettings.Context.Enable.Query)
+          this.navItems = this.navItems.filter(
+            (item) => item.name !== 'Queries' && item.name !== 'Web API'
+          );
 
-      this.tabs$.next(this.navItems.map(item => item.path));
-    });
+        this.tabs$.next(this.navItems.map((item) => item.path));
+      });
   }
 }
