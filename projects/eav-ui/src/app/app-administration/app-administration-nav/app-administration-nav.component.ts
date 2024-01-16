0b216985ce3280966431ae1458f4b50cf75f22c3
Dominik Graf <dominik.graf@2sic.com>
2024-01-16 16:25:37 +0100

    bug fix

--- projects/eav-ui/src/app/app-administration/app-administration-nav/app-administration-nav.component.ts
+++ projects/eav-ui/src/app/app-administration/app-administration-nav/app-administration-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { BehaviorSubject, combineLatest, filter, map, startWith } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, map, startWith, tap } from 'rxjs';
 import { BaseComponent } from '../../shared/components/base-component/base.component';
 import { eavConstants } from '../../shared/constants/eav.constants';
 import { UpdateEnvVarsFromDialogSettings } from '../../shared/helpers/update-env-vars-from-dialog-settings.helper';
@@ -10,12 +10,41 @@ import { DialogSettings } from '../../shared/models/dialog-settings.model';
 import { AppDialogConfigService } from '../services/app-dialog-config.service';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MediaMatcher } from '@angular/cdk/layout';
+
+/** Interface to store navigation object to configure menus */
 interface NavItem {
   name: string;
   path: string;
   icon: string;
   tippy: string;
 }
+
+const navItems: NavItem[] = [
+  { name: 'Info', path: 'home', icon: 'info', tippy: 'App Info' },
+  { name: 'Data', path: 'data', icon: 'menu', tippy: 'Data / Content' },
+  {
+    name: 'Queries',
+    path: 'queries',
+    icon: 'filter_list',
+    tippy: 'Queries / Visual Query Designer',
+  },
+  {
+    name: 'Views',
+    path: 'views',
+    icon: 'layers',
+    tippy: 'Views / Templates',
+  },
+  { name: 'Web API', path: 'web-api', icon: 'offline_bolt', tippy: 'WebApi' },
+  {
+    name: 'App',
+    path: 'app',
+    icon: 'settings_applications',
+    tippy: 'App Settings',
+  },
+  { name: 'Sync', path: 'sync', icon: 'sync', tippy: 'App Export / Import' },
+];
+
+
 @Component({
   selector: 'app-app-administration-nav',
   templateUrl: './app-administration-nav.component.html',
@@ -37,17 +66,28 @@ export class AppAdministrationNavComponent
       startWith(this.route.snapshot.firstChild.url[0].path)
     ),
   ]).pipe(
+    tap(([paths, currentPath]) => {
+      console.log('2dm paths', paths, currentPath);
+    }),
     map(([tabs, path]) => {
       if (tabs == null) return;
+      console.warn(path)
       const index = tabs.indexOf(path);
-      this.changeUrl(index);
-      return index;
+      console.log('2dm index/path', index, path);
+      return { index, path };
     }),
-    filter((index) => index >= 0)
+    // note: PathInfo can be undefined if the path$ is not loaded yet from settings
+    filter((pathInfo) => pathInfo?.index >= 0)
   );
+
+  // Generate View Model
   viewModel$ = combineLatest([this.dialogSettings$, this.pathIndex$]).pipe(
-    map(([dialogSettings, pathIndex]) => {
-      return { dialogSettings, pathIndex };
+    map(([dialogSettings, pathInfo]) => {
+      return {
+        dialogSettings,
+        pathIndex: pathInfo.index,
+        path: pathInfo.path,
+      };
     })
   );
 
@@ -56,30 +96,8 @@ export class AppAdministrationNavComponent
   sideNavOpened = !this.smallScreen.matches;
   currentRoute: string;
 
-  navItems: NavItem[] = [
-    { name: 'Info', path: 'home', icon: 'info', tippy: 'App Info' },
-    { name: 'Data', path: 'data', icon: 'menu', tippy: 'Data / Content' },
-    {
-      name: 'Queries',
-      path: 'queries',
-      icon: 'filter_list',
-      tippy: 'Queries / Visual Query Designer',
-    },
-    {
-      name: 'Views',
-      path: 'views',
-      icon: 'layers',
-      tippy: 'Views / Templates',
-    },
-    { name: 'Web API', path: 'web-api', icon: 'offline_bolt', tippy: 'WebApi' },
-    {
-      name: 'App',
-      path: 'app',
-      icon: 'settings_applications',
-      tippy: 'App Settings',
-    },
-    { name: 'Sync', path: 'sync', icon: 'sync', tippy: 'App Export / Import' },
-  ];
+  /** Navigation menu buttons - prefilled; may be modified after settings are loaded */
+  navItems = navItems;
 
   constructor(
     protected router: Router,
@@ -103,6 +121,10 @@ export class AppAdministrationNavComponent
       'change',
       (c) => (this.sidenav.opened = !c.matches)
     );
+
+    this.viewModel$.subscribe((viewModel) => {
+      console.warn("viewMo",viewModel)
+    })
   }
 
   ngOnDestroy() {
@@ -116,11 +138,11 @@ export class AppAdministrationNavComponent
   }
 
   changeUrl(index: number) {
-    console.error(index)
+    console.warn(index)
     let path = this.path$.value[index];
-    console.error(path)
+    console.warn(path)
     if(this.currentRoute === path) return;
-    console.error(path)
+    console.warn(path)
     this.currentRoute = path;
     if (path === 'data') path = `data/${eavConstants.scopes.default.value}`;
     this.router.navigate([path], { relativeTo: this.route });
