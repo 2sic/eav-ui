b88c5773b52bee01fb4b04c492deb6c2172c40fc
Duje Vatavuk <duje@vatavuk.com>
2024-01-17 14:47:48 +0100

    have paramsMask resolve in onAfterViewInit also as that is necessary when we already have data in the picker

--- projects/eav-ui/src/app/edit/form/fields/picker/adapters/picker-query-source-adapter.ts
+++ projects/eav-ui/src/app/edit/form/fields/picker/adapters/picker-query-source-adapter.ts
@@ -127,6 +127,7 @@ export class PickerQuerySourceAdapter extends PickerSourceEntityAdapterBase {
 
   onAfterViewInit(): void {
     super.onAfterViewInit();
+    this.queryFieldDataSource.params(this.paramsMask.resolve());
   }
 
   destroy(): void {
@@ -149,7 +150,7 @@ export class PickerQuerySourceAdapter extends PickerSourceEntityAdapterBase {
   }
 
   fetchItems(): void {
-    this.queryFieldDataSource.params(this.paramsMask.resolve());
+    // this.queryFieldDataSource.params(this.paramsMask.resolve());
     const settings = this.settings$.value;
     if (!settings.Query) {
       const errorItem: PickerItem = {
