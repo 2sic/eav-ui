557c673aa6da2abf9482940a4d433c07c0c0614c
Duje Vatavuk <duje@vatavuk.com>
2024-01-29 14:29:38 +0100

    rename templateVars into viewModel on number-dropdown component

--- projects/eav-ui/src/app/edit/form/fields/number/number-dropdown/number-dropdown.models.ts
+++ projects/eav-ui/src/app/edit/form/fields/number/number-dropdown/number-dropdown.models.ts
@@ -1,7 +1,7 @@
 import { DropdownOption } from '../../../../../../../../edit-types';
 import { BaseFieldViewModel } from '../../base/base-field-template-vars.model';
 
-export interface NumberDropdownTemplateVars extends BaseFieldViewModel {
+export interface NumberDropdownViewModel extends BaseFieldViewModel {
   enableTextEntry: boolean;
   dropdownOptions: DropdownOption[];
   freeTextMode: boolean;
