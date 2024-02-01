a21e690c421d4c5f44ad4567ead37ddea8ef5803
iJungleboy <daniel.mettler@2sic.com>
2024-02-01 16:54:48 +0100

    Tippy - allowHtml + docs/optimize

--- projects/eav-ui/src/app/shared/directives/tippy.directive.ts
+++ projects/eav-ui/src/app/shared/directives/tippy.directive.ts
@@ -4,8 +4,12 @@ import tippy, { Instance, Placement, Props } from 'tippy.js';
 // tslint:disable-next-line:directive-selector
 @Directive({ selector: '[tippy]' })
 export class TippyDirective implements OnChanges, OnDestroy {
+  /** Message to show in Tippy */
   @Input() tippy: string | null | undefined;
+
+  /** disable Tippy */
   @Input() tippyDisabled: string | boolean | null | undefined;
+
   /**
    * 'top' | 'top-start' | 'top-end' |
    * 'right' | 'right-start' | 'right-end' |
@@ -14,13 +18,19 @@ export class TippyDirective implements OnChanges, OnDestroy {
    * 'auto' | 'auto-start' | 'auto-end'
    */
   @Input() tippyPlacement: string | null | undefined;
+
+  /** delay showing Tippy in ms */
   @Input() tippyShowDelay: string | number | null | undefined;
+
+  /** enable html in Tippy */
+  @Input() tippyAllowHtml: boolean | null | undefined;
 
   private tooltip: Instance<Props>;
 
   constructor(private elementRef: ElementRef<HTMLElement>) { }
 
   ngOnChanges(changes: SimpleChanges): void {
+    // Init Tooltip on first round
     if (this.tooltip == null) {
       this.tooltip = tippy(this.elementRef.nativeElement, {
         animation: 'scale-subtle',
@@ -31,26 +41,22 @@ export class TippyDirective implements OnChanges, OnDestroy {
       });
     }
 
-    if (changes['tippy'] != null) {
+    if (changes['tippy'] != null)
       this.tooltip.setContent(this.tippy);
-    }
 
-    if (changes['tippyDisabled'] != null) {
-      if (this.tippyDisabled) {
-        this.tooltip.disable();
-      } else {
-        this.tooltip.enable();
-      }
-    }
+    if (changes['tippyDisabled'] != null)
+      this.tippyDisabled ? this.tooltip.disable() : this.tooltip.enable();
 
-    if (changes['tippyPlacement'] != null) {
+    if (changes['tippyPlacement'] != null)
       this.tooltip.setProps({ placement: this.tippyPlacement as Placement });
-    }
 
     if (changes['tippyShowDelay'] != null) {
       const showDelay = typeof this.tippyShowDelay === 'number' ? this.tippyShowDelay : parseInt(this.tippyShowDelay, 10);
       this.tooltip.setProps({ delay: [showDelay, null] });
     }
+
+    if (changes['tippyAllowHtml'] != null)
+      this.tooltip.setProps({ allowHTML: this.tippyAllowHtml });
   }
 
   ngOnDestroy(): void {
