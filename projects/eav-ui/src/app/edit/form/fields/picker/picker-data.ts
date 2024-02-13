8324db73e1b0f1d31adab82f1908ed28d3b777ec
iJungleboy <daniel.mettler@2sic.com>
2024-02-13 21:28:27 +0100

    prevent server reload on every pick

--- projects/eav-ui/src/app/edit/form/fields/picker/picker-data.ts
+++ projects/eav-ui/src/app/edit/form/fields/picker/picker-data.ts
@@ -15,7 +15,14 @@ export class PickerData {
   ) {
     // TODO: @SDV include this take(1) and remove this.subscriptions after fixing an issue of why 
     // labels don't show on on picker list (or picker search if we added new items in picker list...)
-    this.subscriptions.add(state.selectedItems$/*.pipe(take(1))*/.subscribe(items => {
+
+    // 2024-02-13 from 2dm - reactivated take(1)
+    // to ensure that the pre-selected items are set only once
+    // TODO: @SDV the note above existed before, I don't know why or how long it's been there
+    // pls review and remove if all is good
+    // previous code
+    // this.subscriptions.add(state.selectedItems$/*.pipe(take(1))*/.subscribe(items => {
+    this.subscriptions.add(state.selectedItems$.pipe(take(1)).subscribe(items => {
       source.setPrefetchData(items.map(item => item.Value));
     }));
 
