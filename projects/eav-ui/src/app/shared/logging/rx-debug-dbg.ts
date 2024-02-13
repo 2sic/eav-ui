98f90dd77126ee7498bc27b62b6ad4f6ca228168
iJungleboy <daniel.mettler@2sic.com>
2024-02-13 20:00:57 +0100

    mainly docs for rx-debug

--- projects/eav-ui/src/app/shared/logging/rx-debug-dbg.ts
+++ projects/eav-ui/src/app/shared/logging/rx-debug-dbg.ts
@@ -1,81 +1,75 @@
 import { Observable, tap } from 'rxjs';
 import { EavLogger } from './eav-logger';
 
-export enum RxJsLoggingLevel {
-  TRACE,
-  DEBUG,
-  INFO,
-  ERROR,
-}
-
-let rxjsLoggingLevel = RxJsLoggingLevel.INFO;
-
-export function setRxJsLoggingLevel(level: RxJsLoggingLevel) {
-  rxjsLoggingLevel = level;
-}
-
-// Inspired by https://blog.angular-university.io/debug-rxjs/
-// export const rxDebug =
-//   (level: number, message: string) => (source: Observable<any>) =>
-//     source.pipe(
-//       tap((val) => {
-//         if (level >= rxjsLoggingLevel) {
-//           console.log(message + ': ', val);
-//         }
-//       })
-//     );
-
-const eavRxJsDebug =
-  <T>(logger: EavLogger, message: string) =>
-  (source: Observable<T>) =>
-    source.pipe(
-      tap((data) => {
-        logger.add(message, data);
-      })
-    );
-
 /**
  * Special debug helper for RxJS observables
  */    
 export class RxTapDebug {
   /**
-   * 
+   * Constructor which will know the context and name of the stream it's for
    * @param logger the main objects logger with name etc.
    * @param name name of the stream we're logging
    * @param jsonify will JSON.stringify the data if true - this is much faster as the browser doesn't block the debugger as much 
    */
   constructor(private logger: EavLogger, private name: string, private jsonify: boolean = true) {}
 
-  private eavRxJsDebug<T>(key: string, message?: string) {
-    const label = `${this.name}/${key}`;
-    const counter = {
-      count: 0,
-    }
+  /**
+   * Generate a rxjs tap function that logs the data with the current context
+   * Inspired by https://blog.angular-university.io/debug-rxjs/
+   * @param key 
+   * @param subName 
+   * @param message 
+   * @returns 
+   */
+  private generateFn<T>(key: string, subName?: unknown, message?: string) {
+    const label = `${this.name}/${key}${subName ? `-${subName}` : ''}`;
+    // create a counter for this function
+    let counter = 0;
     // return a function that takes an observable and returns an observable
     // and logs the data, and counts every access
     return (source: Observable<T>) =>
       source.pipe(
-        tap((data) => this.logger.add(label + ` #${counter.count++}`, this.jsonify ? JSON.parse(JSON.stringify(data)) : data))
+        tap((data) => this.logger.add(label + ` #${counter++}`, this.jsonify ? JSON.parse(JSON.stringify(data)) : data))
       );
   };
 
-  start<T>() { return  this.eavRxJsDebug<T>('start'); }
 
-  pipe<T>() { return  this.eavRxJsDebug<T>('pipe'); }
+  /**
+   * Recommended to use at the beginning of a pipe to show when inbound signals came
+   * @param subName optional sub-name, to distinguish between different uses
+   */
+  pipe<T>(subName?: string) { return this.generateFn<T>('pipe', subName); }
 
-  map<T>(name: string) { return  this.eavRxJsDebug<T>(name ? `map-${name}` : 'map'); }
+  /**
+   * Recommended after map to show what the map did
+   * @param subName optional sub-name, to distinguish between different uses
+   */
+  map<T>(subName?: string) { return this.generateFn<T>('map', subName); }
 
-  end<T>() { return  this.eavRxJsDebug<T>('end'); }
+  /**
+   * Recommended after filter to show what the filter did
+   * @param subName optional sub-name, to distinguish between different uses
+   */
+  filter<T>(subName?: string) { return this.generateFn<T>('filter', subName); }
 
-  step: <T>(num: number) => (source: Observable<T>) => Observable<T> = (
-    num: number
-  ) => this.eavRxJsDebug('step' + num);
+  /**
+   * Recommended after shareReplay
+   * @param subName optional sub-name, to distinguish between different uses
+   * @returns 
+   */
+  shareReplay<T>(subName?: string) { return this.generateFn<T>('shareReplay', subName); }
 
-  part<T>(part: string) { return this.eavRxJsDebug<T>(part); };
+  /**
+   * Recommended after distinctUntilChanged
+   * @param subName optional sub-name, to distinguish between different uses
+   * @returns 
+   */
+  distinctUntilChanged<T>(subName?: string) {return this.generateFn<T>('distinctUntilChanged', subName); }
 
-  read<T>() { return this.eavRxJsDebug<T>('read'); }
 
-  shareReplay<T>() { return this.eavRxJsDebug<T>('shareReplay'); }
-
-  distinctUntilChanged<T>() {return this.eavRxJsDebug<T>('distinctUntilChanged'); }
+  start<T>() { return this.generateFn<T>('start'); }
+  end<T>() { return this.generateFn<T>('end'); }
+  step<T>(num: number) { return this.generateFn('step' + num); }
+  part<T>(subName?: string) { return this.generateFn<T>('part', subName); };
+  read<T>(subName?: string) { return this.generateFn<T>('read', subName); }
 }
