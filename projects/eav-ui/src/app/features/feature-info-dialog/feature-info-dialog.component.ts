dc4d727eeb0851ec13542296befe137d0f5712e7
Dominik Graf <dominik.graf@2sic.com>
2024-04-04 08:48:33 +0200

    features

--- projects/eav-ui/src/app/features/feature-info-dialog/feature-info-dialog.component.ts
+++ projects/eav-ui/src/app/features/feature-info-dialog/feature-info-dialog.component.ts
@@ -5,11 +5,19 @@ import { Observable, combineLatest, map } from 'rxjs';
 import { copyToClipboard } from '../../shared/helpers/copy-to-clipboard.helper';
 import { Feature } from '../models';
 import { FeatureDetailService } from '../services/feature-detail.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SharedComponentsModule } from '../../shared/shared-components.module';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-feature-info-dialog',
-  templateUrl: './feature-info-dialog.component.html',
-  styleUrls: ['./feature-info-dialog.component.scss']
+    selector: 'app-feature-info-dialog',
+    templateUrl: './feature-info-dialog.component.html',
+    styleUrls: ['./feature-info-dialog.component.scss'],
+    standalone: true,
+    imports: [MatCardModule, SharedComponentsModule, MatButtonModule, MatIconModule, AsyncPipe, TranslateModule]
 })
 export class FeatureInfoDialogComponent implements OnInit {
   viewModel$: Observable<FeatureInfoViewModel>;
