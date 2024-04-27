558cc7fac0fb96c60e9ce1466d8420756e3b0d55
iJungleboy <daniel.mettler@2sic.com>
2024-04-27 09:27:56 +0200

    minor disentangle PagePicker from Query

--- projects/eav-ui/src/app/edit/form/shared/page-picker/page-picker.models.ts
+++ projects/eav-ui/src/app/edit/form/shared/page-picker/page-picker.models.ts
@@ -1,24 +1,23 @@
 import { UntypedFormGroup } from '@angular/forms';
 import { FieldConfigSet } from '../../builder/fields-builder/field-config-set.model';
-import { QueryEntity } from '../../fields/picker/models/query-entity.model';
+import { EntityBasic } from '../../../shared/models/entity-basic';
 
 export interface PagePickerDialogData {
   config: FieldConfigSet;
   group: UntypedFormGroup;
 }
 
-export interface PageEntity extends QueryEntity {
-  Created: string;
-  Guid: string;
-  Id: number;
-  // 2024-04-26 2dm removed this, don't think it's used and believe it's a leftover #cleanup-picker
+export interface PageEntity extends EntityBasic {
+  // Created: string;
   // Modified: string;
   Name: string;
   ParentId: number;
   Path: string;
-  Title: string;
   Url: string;
   Visible: boolean;
+
+  /** If the page would be clickable in the navigation, or just a name to show (usually opening sub-pages) */
+  Clickable: boolean;
 }
 
 export interface PagePickerViewModel {
