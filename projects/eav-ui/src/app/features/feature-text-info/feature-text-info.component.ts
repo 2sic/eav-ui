58e503c5658754bb8ef5cf72760585cadcc88d00
Dominik Graf <dominik.graf@2sic.com>
2024-06-13 11:30:15 +0200

    mini bug fix featuer lang

--- projects/eav-ui/src/app/features/feature-text-info/feature-text-info.component.ts
+++ projects/eav-ui/src/app/features/feature-text-info/feature-text-info.component.ts
@@ -1,20 +1,22 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, ViewContainerRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FeaturesService } from '../../shared/services/features.service';
 import { FeatureSummary } from '../models';
 import { FeatureComponentBase } from '../shared/base-feature.component';
-import { BehaviorSubject, map, Observable, combineLatest, switchMap } from 'rxjs';
+import { BehaviorSubject, map, Observable, combineLatest } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
 import { AsyncPipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { SharedComponentsModule } from '../../shared/shared-components.module';
+import { FeatureDetailService } from '../services/feature-detail.service';
 
 @Component({
     selector: 'app-feature-text-info',
     templateUrl: './feature-text-info.component.html',
     styleUrls: ['./feature-text-info.component.scss'],
     standalone: true,
-    imports: [SharedComponentsModule, MatIconModule, AsyncPipe, TranslateModule]
+    imports: [SharedComponentsModule, MatIconModule, AsyncPipe, TranslateModule],
+    providers: [FeatureDetailService]
 })
 export class FeatureTextInfoComponent extends FeatureComponentBase {
   @Input()
@@ -30,7 +32,7 @@ export class FeatureTextInfoComponent extends FeatureComponentBase {
     changeDetectorRef: ChangeDetectorRef
   ) {
     super(dialog, viewContainerRef, changeDetectorRef , featuresService);
-    this.viewModel$ = combineLatest([this.feature$, this.asInfo$, this.show$]).pipe(map(([feature, asInfo, show]) => 
+    this.viewModel$ = combineLatest([this.feature$, this.asInfo$, this.show$]).pipe(map(([feature, asInfo, show]) =>
       ({
         feature,
         icon: asInfo ? 'info' : 'warning',
