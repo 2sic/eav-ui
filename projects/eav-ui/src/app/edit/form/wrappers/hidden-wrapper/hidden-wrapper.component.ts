685f5c0e116dbdc8baaa8dc63cbe8dcb6ba2d644
Dominik Graf <dominik.graf@2sic.com>
2024-06-17 16:14:43 +0200

    refactor HiddenWrapper to use signals

--- projects/eav-ui/src/app/edit/form/wrappers/hidden-wrapper/hidden-wrapper.component.ts
+++ projects/eav-ui/src/app/edit/form/wrappers/hidden-wrapper/hidden-wrapper.component.ts
@@ -1,40 +1,22 @@
-import { Component, OnDestroy, OnInit, signal, ViewChild, ViewContainerRef, WritableSignal } from '@angular/core';
-import { distinctUntilChanged, map, Observable, Subject } from 'rxjs';
+import { Component, computed, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { WrappersConstants } from '../../../shared/constants';
 import { FieldsSettingsService } from '../../../shared/services';
 import { FieldWrapper } from '../../builder/fields-builder/field-wrapper.model';
 import { BaseFieldComponent } from '../../fields/base/base-field.component';
-import { ItemFieldVisibility } from '../../../shared/services/item-field-visibility';
-import { AsyncPipe } from '@angular/common';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { FieldSettings } from '../../../../../../../edit-types';
 
 @Component({
   selector: WrappersConstants.HiddenWrapper,
   templateUrl: './hidden-wrapper.component.html',
   styleUrls: ['./hidden-wrapper.component.scss'],
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [],
 })
 export class HiddenWrapperComponent extends BaseFieldComponent implements FieldWrapper, OnInit, OnDestroy {
   @ViewChild('fieldComponent', { static: true, read: ViewContainerRef }) fieldComponent: ViewContainerRef;
 
-  hidden: WritableSignal<boolean> = signal(false);
+  hidden = computed(() => !this.basics().visibleAndEnabled);
 
   constructor(fieldsSettingsService: FieldsSettingsService) {
     super(fieldsSettingsService);
-  }
-
-  ngOnInit() {
-    super.ngOnInit();
-
-    this.settings$.pipe(
-      map(settings => !ItemFieldVisibility.mergedVisible(settings)),
-      distinctUntilChanged()
-    ).subscribe(value => this.hidden.set(value));
-  }
-
-  ngOnDestroy() {
-    super.ngOnDestroy();
   }
 }
