9d6bd7f70fed59b507ecc30dbf22a462ebe9abad
iJungleboy <daniel.mettler@2sic.com>
2024-06-25 18:29:37 +0200

    string-url-path without base class

--- projects/eav-ui/src/app/edit/form/fields/string/string-url-path/string-url-path.component.ts
+++ projects/eav-ui/src/app/edit/form/fields/string/string-url-path/string-url-path.component.ts
@@ -4,7 +4,6 @@ import { InputTypeConstants } from '../../../../../content-type-fields/constants
 import { WrappersLocalizationOnly } from '../../../../shared/constants/wrappers.constants';
 import { FieldMask, UrlHelpers } from '../../../../shared/helpers';
 import { FieldMetadata } from '../../../builder/fields-builder/field-metadata.decorator';
-import { BaseFieldComponent } from '../../base/base-field.component';
 import { StringUrlPathLogic } from './string-url-path-logic';
 import { AsyncPipe } from '@angular/common';
 import { FieldHelperTextComponent } from '../../../shared/field-helper-text/field-helper-text.component';
@@ -14,6 +13,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ControlHelpers } from '../../../../shared/helpers/control.helpers';
 import { FieldState } from '../../../builder/fields-builder/field-state';
 import { EavLogger } from 'projects/eav-ui/src/app/shared/logging/eav-logger';
+import { BaseComponent } from 'projects/eav-ui/src/app/shared/components/base.component';
 
 const logThis = false;
 const nameOfThis = 'StringUrlPathComponent';
@@ -28,23 +28,21 @@ const nameOfThis = 'StringUrlPathComponent';
     ReactiveFormsModule,
     MatInputModule,
     FieldHelperTextComponent,
-    AsyncPipe,
   ],
 })
 @FieldMetadata({ ...WrappersLocalizationOnly })
-export class StringUrlPathComponent extends BaseFieldComponent<string> implements OnInit, OnDestroy {
+export class StringUrlPathComponent extends BaseComponent implements OnInit, OnDestroy {
 
   protected fieldState = inject(FieldState);
 
-  protected groupTemp = this.fieldState.group;
-  protected configTemp = this.fieldState.config;
+  protected group = this.fieldState.group;
 
-  protected settingsTemp = this.fieldState.settings;
-  protected basicsTemp = this.fieldState.basics;
-  protected controlTemp = this.fieldState.control;
-
+  protected settings = this.fieldState.settings;
+  protected basics = this.fieldState.basics;
+  public control = this.fieldState.control;
 
   private fieldMask = FieldMask.createTransient(inject(Injector));
+
   /** Blocks external update if field was changed manually and doesn't match external updates. WARNING: Doesn't work on language change */
   private lastAutoCopy = '';
 
@@ -54,9 +52,7 @@ export class StringUrlPathComponent extends BaseFieldComponent<string> implement
   }
 
   ngOnInit() {
-    super.ngOnInit();
     this.log.a('ngOnInit');
-
 
     this.subscriptions.add(
       this.fieldState.settings$.pipe(
@@ -76,28 +72,28 @@ export class StringUrlPathComponent extends BaseFieldComponent<string> implement
 
     // clean on value change
     this.subscriptions.add(
-      this.controlTemp.valueChanges.subscribe(() => {
+      this.control.valueChanges.subscribe(() => {
         this.clean(false);
       })
     );
   }
 
   private onSourcesChanged(newValue: string) {
-    const value = this.controlTemp.value;
+    const value = this.control.value;
     // don't do anything if the current field is not empty and doesn't have the last copy of the stripped value
     if (value && value !== this.lastAutoCopy) return;
 
     // TODO::  this.settings$.value.AllowSlashes to signal, settings has no value or AllowSlashes
-    const cleaned = UrlHelpers.stripNonUrlCharacters(newValue, this.settings$.value.AllowSlashes, true);
+    const cleaned = UrlHelpers.stripNonUrlCharacters(newValue, this.settings().AllowSlashes, true);
     if (!cleaned) return;
     this.lastAutoCopy = cleaned;
     if (value === cleaned) return;
-    ControlHelpers.patchControlValue(this.controlTemp, cleaned);
+    ControlHelpers.patchControlValue(this.control, cleaned);
   }
 
   clean(trimEnd: boolean) {
-    const value = this.controlTemp.value;
-    const cleaned = UrlHelpers.stripNonUrlCharacters(value, this.settings$.value.AllowSlashes, trimEnd);
+    const value = this.control.value;
+    const cleaned = UrlHelpers.stripNonUrlCharacters(value, this.settings().AllowSlashes, trimEnd);
     if (value === cleaned) return;
     ControlHelpers.patchControlValue(this.control, cleaned);
   }
