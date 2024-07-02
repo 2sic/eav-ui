aa5b67d8f739b28fa8b8065c31d9e27874eeafd9
Dominik Graf <dominik.graf@2sic.com>
2024-07-02 16:09:24 +0200

    remove create file dialog

--- projects/eav-ui/src/app/create-file-dialog/index.ts
+++ projects/eav-ui/src/app/create-file-dialog/index.ts
@@ -2,4 +2,4 @@ export * from './file-location-dialog/file-location-dialog.component';
 export * from './file-location-dialog/file-location-dialog.models';
 export * from './create-file-dialog.component';
 export * from './create-file-dialog.models';
-export * from './create-file-dialog.module';
+// export * from './create-file-dialog.module';
