79ab654710dfac5444c7da8bae596179db7e3961
iJungleboy <daniel.mettler@2sic.com>
2024-08-20 20:28:50 +0200

    Switch from Material Icons to Material Symbols https://github.com/2sic/2sxc/issues/3454

--- projects/eav-ui/src/app/shared/icons/app-icons.service.ts
+++ projects/eav-ui/src/app/shared/icons/app-icons.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { iconLib, iconsFontAwesome, iconsMaterial } from '.';
+import { iconsFontAwesome } from '.';
 
 
 @Injectable()
@@ -9,7 +9,8 @@ export class AppIconsService {
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) { }
 
   load() {
-    this.matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
+    // this.matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
+    this.matIconRegistry.setDefaultFontSetClass('material-symbols-outlined');
 
     Object.entries(iconsFontAwesome).forEach(([name, svg]) => {
       this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
