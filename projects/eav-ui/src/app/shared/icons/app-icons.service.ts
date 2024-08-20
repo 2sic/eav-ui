500ce0a37550498628c3c0904bdf66c6b38e929b
iJungleboy <daniel.mettler@2sic.com>
2024-08-20 20:31:44 +0200

    clean up icons infrastructure https://github.com/2sic/2sxc/issues/3454

--- projects/eav-ui/src/app/shared/icons/app-icons.service.ts
+++ projects/eav-ui/src/app/shared/icons/app-icons.service.ts
@@ -3,25 +3,17 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { iconsFontAwesome } from '.';
 
-
 @Injectable()
 export class AppIconsService {
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) { }
 
   load() {
+    // v18.01 - changing to Material Symbols
     // this.matIconRegistry.setDefaultFontSetClass('material-icons-outlined');
     this.matIconRegistry.setDefaultFontSetClass('material-symbols-outlined');
 
     Object.entries(iconsFontAwesome).forEach(([name, svg]) => {
       this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
     });
-    // Object.entries(iconsMaterial).forEach(([name, svg]) => {
-    //   this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
-    // });
-
-    // 2dm: New api with object syntax
-    // Object.entries(iconLib).forEach(([name, svg]) => {
-    //   this.matIconRegistry.addSvgIconLiteral(name, this.domSanitizer.bypassSecurityTrustHtml(svg));
-    // });
   }
 }
