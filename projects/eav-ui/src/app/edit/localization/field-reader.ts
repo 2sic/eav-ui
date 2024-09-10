28d63e399d5211c27dad3a405177ae275f881f57
iJungleboy <daniel.mettler@2sic.com>
2024-09-10 11:59:51 +0200

    minor bugfix

--- projects/eav-ui/src/app/edit/localization/field-reader.ts
+++ projects/eav-ui/src/app/edit/localization/field-reader.ts
@@ -21,9 +21,15 @@ export class FieldReader<T = any> {
     this.log.fnIf('constructor', { field, language });
     this.#field = field;
     this.#language = typeof language === 'string' ? { current: language, primary: language } : language;
+    this.#values = field?.Values ?? [];
+    this.#noData = !field || !field.Values || field.Values.length === 0;
   }
 
+  #noData = true;
+
   #field: EavField<T>;
+  /** Values - never empty for read-safety */
+  #values: EavValue<T>[];
   #language: FormLanguage;
 
   get current(): EavValue<T> | null {
@@ -42,6 +48,7 @@ export class FieldReader<T = any> {
    * @memberof FieldReader
    */
   get currentOrDefault(): EavValue<T> {
+    if (this.#noData) return null;
     return this.current
       // note that having both languages primary will also result in checking the '*' dimension
       ?? this.ofLanguage(FormLanguage.bothPrimary(this.#language));
@@ -55,29 +62,31 @@ export class FieldReader<T = any> {
    * 4. first/any value in the system
    */
   get currentOrDefaultOrAny(): EavValue<T> | null {
-    return this.currentOrDefault ?? this.#field.Values[0] ?? null;
+    if (this.#noData) return null;
+    return this.currentOrDefault ?? this.#values[0] ?? null;
   }
 
 
   ofLanguage(language: FormLanguage): EavValue<T> | null {
-    if (!this.#field) return null;
-    return this.#field.Values.filter(val => new DimensionReader(val.Dimensions, language).hasCurrent)[0] ?? null;
+    if (this.#noData) return null;
+    return this.#values.filter(val => new DimensionReader(val.Dimensions, language).hasCurrent)[0] ?? null;
   }
 
   /** Check if there is a value on the specified primary language or on the '*' dimension */
   get hasPrimary(): boolean {
-    if (!this.#field) return false;
+    if (this.#noData) return false;
     const primary = this.#language.primary;
-    return this.#field.Values.filter(val => val.Dimensions.find(d => d.Value === primary || d.Value === '*')).length > 0;
+    return this.#values.filter(val => val.Dimensions.find(d => d.Value === primary || d.Value === '*')).length > 0;
   }
 
   get hasCurrentReadonly(): boolean {
-    if (!this.#field) return false;
-    return this.#field.Values.filter(val => new DimensionReader(val.Dimensions, this.#language).hasCurrentReadOnly).length > 0;
+    if (this.#noData) return false;
+    return this.#values.filter(val => new DimensionReader(val.Dimensions, this.#language).hasCurrentReadOnly).length > 0;
   }
 
   /** A value in specified Language is editable, if assigned to current language or to '*' (but only when on default-language) */
   get hasEditableValues(): boolean {
+    if (this.#noData) return false;
     return this.#valuesEditableOf().length > 0;
   }
 
@@ -96,20 +105,20 @@ export class FieldReader<T = any> {
    * if they are assigned to the current language or to '*' (but only when the current-language is also the primary-language)
    */
   #valuesEditableOf(language?: FormLanguage): EavValue<T>[] {
-    if (!this.#field) return [];
+    if (this.#noData) return [];
     language ??= this.#language;
-    return this.#field.Values.filter(val => new DimensionReader(val.Dimensions, language).hasCurrentWrite);
+    return this.#values.filter(val => new DimensionReader(val.Dimensions, language).hasCurrentWrite);
   }
 
-  // TODO CONTINUE HERE
   /** Value of current language which is editable. `null` if not found. */
   get currentEditable(): EavValue<T> {
+    if (this.#noData) return null;
     const dimension = this.#language.current;
-    return this.#field.Values.find(v => v.Dimensions.find(x => x.Value === dimension)) ?? null;
+    return this.#values.find(v => v.Dimensions.find(x => x.Value === dimension)) ?? null;
   }
 
   isEditableOrReadonlyTranslationExist(): boolean {
-    if (!this.#field) return false;
+    if (this.#noData) return false;
     return this.#valuesEditableOf().length > 0;
   }
 
