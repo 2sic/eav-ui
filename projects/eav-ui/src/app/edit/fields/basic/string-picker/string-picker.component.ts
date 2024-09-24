52db4e9a6766ac5ac73de74628a7cd6727cd3158
Paul Pietzko <paul.pietzko@icloud.com>
2024-09-24 08:32:29 +0200

    Deleted last import of missing picker.components.scss

--- projects/eav-ui/src/app/edit/fields/basic/string-picker/string-picker.component.ts
+++ projects/eav-ui/src/app/edit/fields/basic/string-picker/string-picker.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { StringPickerLogic } from './string-picker-logic';
 import { InputTypeCatalog } from '../../../../shared/fields/input-type-catalog';
+import { classLog } from '../../../../shared/logging/logging';
 import { PickerImports } from '../../picker/picker-providers.constant';
 import { PickerComponent } from '../../picker/picker.component';
-import { classLog } from '../../../../shared/logging/logging';
+import { StringPickerLogic } from './string-picker-logic';
 
 @Component({
   selector: InputTypeCatalog.StringPicker,
   templateUrl: '../../picker/picker.component.html',
-  styleUrls: ['../../picker/picker.component.scss'],
   standalone: true,
   imports: PickerImports,
 })
