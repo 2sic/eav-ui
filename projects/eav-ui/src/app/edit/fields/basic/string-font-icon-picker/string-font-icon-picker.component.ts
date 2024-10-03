7e0eaee1bcc87f26304827bfbe176c9c6a9e0d1b
Dominik Graf <dominik.graf@2sic.com>
2024-10-03 09:12:39 +0200

    mini fix

--- projects/eav-ui/src/app/edit/fields/basic/string-font-icon-picker/string-font-icon-picker.component.ts
+++ projects/eav-ui/src/app/edit/fields/basic/string-font-icon-picker/string-font-icon-picker.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InputTypeCatalog } from '../../../../shared/fields/input-type-catalog';
+import { classLog } from '../../../../shared/logging';
 import { FieldMetadata } from '../../field-metadata.decorator';
 import { PickerImports } from '../../picker/picker-providers.constant';
 import { PickerComponent } from '../../picker/picker.component';
