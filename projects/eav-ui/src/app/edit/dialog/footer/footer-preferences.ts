95e46091ea1e575426dc3db9b0c7552c1cb6fc69
iJungleboy <daniel.mettler@2sic.com>
2024-10-04 09:12:32 +0200

    form footer: enable pinning (to hide on demos etc.)

---
+++ projects/eav-ui/src/app/edit/dialog/footer/footer-preferences.ts
@@ -0,0 +1,19 @@
+import { Of } from 'projects/core';
+import { DebugTypes } from './edit-dialog-footer.models';
+
+/**
+ * User preferences how the footer should be displayed.
+ */
+export const footerPreferences = {
+  key: 'edit-dialog-footer',
+  data: {
+    /** Which tab is expanded (or none) */
+    tab: null as Of<typeof DebugTypes>,
+    /**  */
+    expanded: false,
+    /** Expansion size, as it can be larger (2) */
+    size: 0,
+    /** If the footer is pinned, e.g. visible on load. If null, will be prefilled with user-host-status */
+    pinned: null as boolean,
+  }
+};
