3b768840d57a105440f70863c4d6f700a888cc48
iJungleboy <daniel.mettler@2sic.com>
2024-10-07 06:11:18 +0200

    picker: number should know it can never be multi-value

--- projects/eav-ui/src/app/edit/fields/picker/adapters/state-adapter-number.ts
+++ projects/eav-ui/src/app/edit/fields/picker/adapters/state-adapter-number.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { signalObj } from 'projects/eav-ui/src/app/shared/signals/signal.utilities';
 import { classLog } from "../../../../shared/logging";
+import { PickerFeatures } from '../picker-features.model';
 import { logSpecsStateAdapter, StateAdapter } from "./state-adapter";
 import { StateUiMapperNumberArray } from './state-ui-mapper-number-array';
 
@@ -9,6 +11,9 @@ export class StateAdapterNumber extends StateAdapter {
   log = classLog({ StateAdapterNumber }, logSpecsStateAdapter);
 
   constructor() { super(); }
+
+  public override features = signalObj('features', { multiValue: false } satisfies Partial<PickerFeatures>);
+
   // Map the state (string to number array) and UI (number to string array)
   mapper = new StateUiMapperNumberArray('todo field name', this.settings);
 
