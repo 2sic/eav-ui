fe883e161ada42cdfcdf125fec9037bff106b4a6
iJungleboy <daniel.mettler@2sic.com>
2024-10-08 07:14:29 +0200

    field string: undocumented feature to create 1-line multiline-input

--- projects/eav-ui/src/app/edit/fields/basic/string-default/string-default.component.ts
+++ projects/eav-ui/src/app/edit/fields/basic/string-default/string-default.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { computedObj } from 'projects/eav-ui/src/app/shared/signals/signal.utilities';
 import { InputTypeCatalog } from '../../../../shared/fields/input-type-catalog';
 import { FieldMetadata } from '../../field-metadata.decorator';
 import { FieldState } from '../../field-state';
@@ -36,7 +37,24 @@ export class StringDefaultComponent {
   protected settings = this.fieldState.settings;
   protected basics = this.fieldState.basics;
 
-  protected rowCount = this.fieldState.setting('RowCount');
+  /**
+   * Row Count as it is in the settings
+   * It has an undocumented feature - ATM only used in icon-picker,
+   * where the row count can be negative to create a multi-line picker.
+   * This is not an official feature, as we may add a toggle to this some day instead, and then fix the icon-picker.
+   */
+  protected rowCountRaw = this.fieldState.setting('RowCount');
+
+  /**
+   * Row Count corrected and also neutralize negative numbers, as it's an undocumented feature to create multi-line with 1 line
+   */
+  protected rowCount = computedObj('rowCount', () => Math.abs(this.rowCountRaw() || 1));
+  
+  protected isMultiline = computedObj('isMultiline', () => {
+    const rc = this.rowCountRaw() || 1;
+    return rc < 0 || rc > 1;
+  });
+  
   protected inputFontFamily = this.fieldState.setting('InputFontFamily');
   protected textWrap = this.fieldState.setting('TextWrapping');
 
