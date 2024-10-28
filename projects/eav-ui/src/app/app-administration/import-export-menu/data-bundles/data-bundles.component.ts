0b4567b83a1693565bd064d35c7568b04d6e5ff6
Dominik Graf <dominik.graf@2sic.com>
2024-10-28 14:23:39 +0100

    data bundles wip, h1,h2,h3

--- projects/eav-ui/src/app/app-administration/import-export-menu/data-bundles/data-bundles.component.ts
+++ projects/eav-ui/src/app/app-administration/import-export-menu/data-bundles/data-bundles.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { GridOptions } from '@ag-grid-community/core';
+import { Component, signal } from '@angular/core';
+import { FeatureTextInfoComponent } from '../../../features/feature-text-info/feature-text-info.component';
+import { ColumnDefinitions } from '../../../shared/ag-grid/column-definitions';
+import { defaultGridOptions } from '../../../shared/constants/default-grid-options.constants';
+import { SxcGridModule } from '../../../shared/modules/sxc-grid-module/sxc-grid.module';
 
 @Component({
   selector: 'app-data-bundles',
   standalone: true,
-  imports: [],
+  imports: [
+    FeatureTextInfoComponent,
+    SxcGridModule,
+
+  ],
   templateUrl: './data-bundles.component.html',
   styleUrl: './data-bundles.component.scss'
 })
 export class DataBundlesComponent {
+
+  gridOptions = this.buildGridOptions();
+
+  dataBundles = signal([]); // get data form service
+
+
+
+  private buildGridOptions(): GridOptions {
+    const gridOptions: GridOptions = {
+      ...defaultGridOptions,
+      columnDefs: [
+        {
+          ...ColumnDefinitions.TextWideActionClass,
+          headerName: 'Key',
+          field: 'Path',
+          // cellRenderer: AnalyzeSettingsKeyComponent,
+        },
+      ],
+    };
+    return gridOptions;
+  }
 
 }
