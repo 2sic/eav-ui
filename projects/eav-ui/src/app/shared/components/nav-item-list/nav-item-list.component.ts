55e0985f8747d5b307fa02499e890ac1856bf3d1
iJungleboy <daniel.mettler@2sic.com>
2024-10-31 18:59:20 +0100

    minor code style

--- projects/eav-ui/src/app/shared/components/nav-item-list/nav-item-list.component.ts
+++ projects/eav-ui/src/app/shared/components/nav-item-list/nav-item-list.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { Component, input, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
@@ -21,24 +21,21 @@ import { NavItem } from '../../models/nav-item.model';
 export class NavItemListComponent implements OnInit {
   navItem = input<NavItem>();
   isOpenMenu = false;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.openChildMenu();
-
-    this.router.events.subscribe(() => {
-      this.openChildMenu();
-    });
+    this.#openChildMenu();
+    this.router.events.subscribe(() => this.#openChildMenu());
   }
 
-  openChildMenu() {
-    if (this.navItem().child?.length) {
+  #openChildMenu() {
+    if (!this.navItem().child?.length)
+      return;
 
-      const urlSegments = this.router.url.split('/');
-      const matchingChild = this.navItem().child.find(child => urlSegments.includes(child.path));
-
-      if (matchingChild)
-        this.isOpenMenu = true;
-    }
+    const urlSegments = this.router.url.split('/');
+    const matchingChild = this.navItem().child.find(child => urlSegments.includes(child.path));
+    if (matchingChild)
+      this.isOpenMenu = true;
   }
 }
