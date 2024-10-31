fdaa5d98fcddc1ac8c56e9c9eb132303eb5a0b46
iJungleboy <daniel.mettler@2sic.com>
2024-10-31 18:34:57 +0100

    minor: prepare to enable opening dialogs in new window

--- projects/eav-ui/src/app/params-init.factory.ts
+++ projects/eav-ui/src/app/params-init.factory.ts
@@ -5,42 +5,71 @@ import { UrlHelpers } from './edit/shared/helpers/url.helpers';
 import { DialogTypeConstants } from './shared/constants/dialog-type.constants';
 import { keyAppId, keyContentType, keyDialog, keyExtras, keyItems, keyPipelineId, keyUrl, keyZoneId, prefix } from './shared/constants/session.constants';
 import { convertFormToUrl } from './shared/helpers/url-prep.helper';
+import { classLog } from './shared/logging';
 import { ExtrasParam } from './shared/models/dialog-url-params.model';
 import { EavWindow } from './shared/models/eav-window.model';
 import { EditForm, ItemEditIdentifier, ItemInListIdentifier } from './shared/models/edit-form.model';
 
 declare const window: EavWindow;
 
+/**
+ * Simple factory which is used by Angular Routing to figure out the APP_INITIALIZER
+ * @param injector injector which is provided by Angular, required to get the Router
+ * @returns 
+ */
 export function paramsInitFactory(injector: Injector): () => void {
+
+  /** Helper to transfer various url-parameters to the session */
+  function transferParamsToSessionStorage(queryParametersFromUrl: Record<string, string>): void {
+    for (const [paramKey, paramValue] of Object.entries(queryParametersFromUrl)) {
+      if (paramValue == null) continue;
+      sessionStorage.setItem(prefix + paramKey, paramValue);
+    }
+  }
+
+  /** Log initial route so a developer can re-open the dialog with the link in the console */
+  function logInitialRoute(url?: string): void {
+    console.log('Initial route:', url ?? window.location.href);
+  }
+
+  const log = classLog({ paramsInitFactory }, null, true);
+
   return () => {
+    const l = log.fn('paramsInitFactory');
     // console.log('Setting parameters config and clearing route');
     const eavKeys = Object.keys(sessionStorage).filter(key => key.startsWith(prefix));
-    const isParamsRoute = !window.location.hash.startsWith('#/');
+    const urlHash = window.location.hash;
+    const isParamsRoute = !urlHash.startsWith('#/');
     if (isParamsRoute) {
-      console.log('Initial route:', window.location.href);
-      // clear our part of the session
-      for (const key of eavKeys) {
+      l.a('is Params Route - will process', { urlHash, eavKeys });
+      logInitialRoute();
+      // // Log initial route so a developer can re-open the dialog with the link in the console
+      // console.log('Initial route:', window.location.href);
+
+      // Flush our part of the session, just to be sure it's a clean slate
+      for (const key of eavKeys)
         sessionStorage.removeItem(key);
-      }
-      sessionStorage.setItem(keyUrl, window.location.href); // save url which opened the dialog
-      sessionStorage.setItem(keyDialog, DialogTypeConstants.Edit); // set edit dialog as the default
 
-      // save params
-      const urlHash = window.location.hash.substring(1); // substring removes first # char
-      const queryParametersFromUrl = UrlHelpers.readQueryStringParameters(urlHash);
+      // save url which opened the dialog and set edit dialog as the default
+      sessionStorage.setItem(keyUrl, window.location.href);
+      sessionStorage.setItem(keyDialog, DialogTypeConstants.Edit);
 
-      for (const [paramKey, paramValue] of Object.entries(queryParametersFromUrl)) {
-        if (paramValue == null) continue;
-        sessionStorage.setItem(prefix + paramKey, paramValue);
-      }
+      // Transfer URL params to session storage, without the leading '#' char
+      const paramsDic = UrlHelpers.urlParamsToDic(urlHash.substring(1));
+      transferParamsToSessionStorage(paramsDic);
+      // for (const [paramKey, paramValue] of Object.entries(queryParametersFromUrl)) {
+      //   if (paramValue == null) continue;
+      //   sessionStorage.setItem(prefix + paramKey, paramValue);
+      // }
 
-      // redirect
+      // Redirect to the expected dialog
       const router = injector.get(Router);
       const zoneId = sessionStorage.getItem(keyZoneId);
       const appId = sessionStorage.getItem(keyAppId);
       const dialog = sessionStorage.getItem(keyDialog) as Of<typeof DialogTypeConstants>;
       const contentType = sessionStorage.getItem(keyContentType);
       const items = sessionStorage.getItem(keyItems);
+      l.a('dialog: ' + dialog);
       switch (dialog) {
         case DialogTypeConstants.Zone:
           const extrasZone: ExtrasParam = JSON.parse(sessionStorage.getItem(keyExtras));
@@ -65,7 +94,6 @@ export function paramsInitFactory(injector: Injector): () => void {
         case DialogTypeConstants.Edit:
           const editItems: ItemEditIdentifier[] = JSON.parse(items);
           const form: EditForm = { items: editItems };
-          // const fields = sessionStorage.getItem(keyEditFields);
           const formUrl = convertFormToUrl(form);
           router.navigate([`${zoneId}/${appId}/edit/${formUrl}`]);
           break;
@@ -103,11 +131,31 @@ export function paramsInitFactory(injector: Injector): () => void {
           } catch (error) { }
       }
     } else if (eavKeys.length === 0) {
+      // if not params route and no params are saved, e.g. browser was reopened,
+      // check if we have additional context info in the url behind a ##
+      const urlWithCtx = urlHash.split('##')
+      const ctxParams = urlWithCtx[1];
+      if (ctxParams) {
+        l.a('No Params Route and no params saved, but found context info in url, will process', { urlHash, ctxParams });
+        logInitialRoute();
+        const finalRoute = urlWithCtx[0].substring(1);
+        const routeParts = finalRoute.split('/'); // url is like '/73/770/app/data/...'
+        const ctxAll = `${ctxParams}&zoneId=${routeParts[1]}&appId=${routeParts[2]}`;
+        l.a('found ##', { urlHash, ctxParams, ctxAll, finalRoute });
+        transferParamsToSessionStorage(UrlHelpers.urlParamsToDic(ctxAll));
+        const router = injector.get(Router);
+        router.navigate([finalRoute]);
+        return l.end();
+      }
+
       // if not params route and no params are saved, e.g. browser was reopened, throw error
+      l.a('No Params Route and no params saved...');
       alert('Missing required url parameters. Please reopen dialog.');
       throw new Error('Missing required url parameters. Please reopen dialog.');
     } else {
-      console.log('Initial route:', sessionStorage.getItem(keyUrl));
+      l.a('No Params Route, but params saved..., assume refresh of previously working UI');
+      // Log initial route so a developer can re-open the dialog with the link in the console
+      logInitialRoute(sessionStorage.getItem(keyUrl));
     }
   };
 }
