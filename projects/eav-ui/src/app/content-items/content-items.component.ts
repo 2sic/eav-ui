482def1c9e0d55dc16f2ef8e5e73675853b627cb
paulpietzko <paul.pietzko@icloud.com>
2024-11-15 10:27:04 +0100

    refactor content items component to use anchor tag for importing single JSON item

--- projects/eav-ui/src/app/content-items/content-items.component.ts
+++ projects/eav-ui/src/app/content-items/content-items.component.ts
@@ -248,11 +248,13 @@ export class ContentItemsComponent implements OnInit, OnDestroy {
     );
   }
 
-  // TODO: Should be a link, but is tricky to do with the current setup
-  // as it's doing somethingwith the files, which is not possible with a link
   importItem(files?: File[]) {
     const dialogData: FileUploadDialogData = { files };
     this.#dialogRouter.navRelative(['import'], { state: dialogData });
+  }
+
+  urlToImportItem() {
+    return this.#urlTo('import');
   }
 
   createMetadata() {
