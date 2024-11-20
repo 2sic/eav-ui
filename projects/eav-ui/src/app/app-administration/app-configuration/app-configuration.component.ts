19576ec983fd36fb43b389032440c459761246b0
paulpietzko <paul.pietzko@icloud.com>
2024-11-20 12:07:25 +0100

    refactor app configuration component to replace button elements with anchor tags for navigation; add URL handling for global custom settings

--- projects/eav-ui/src/app/app-administration/app-configuration/app-configuration.component.ts
+++ projects/eav-ui/src/app/app-administration/app-configuration/app-configuration.component.ts
@@ -77,6 +77,7 @@ export class AppConfigurationComponent implements OnInit, OnDestroy {
   appSiteSystemResourcesUrl = signal('');
   appContentCustomResourcesUrl = signal('');
   appGlobalCustomResourcesUrl = signal('');
+  appGlobalCustomSettingsUrl = signal('');
 
   // More proper ViewModel
   appSettingsInternal$ = new Subject<AppInternals>();
@@ -141,6 +142,7 @@ export class AppConfigurationComponent implements OnInit, OnDestroy {
     this.appContentCustomSettingsUrl = this.urlToEditDefault(eavConstants.contentTypes.settings);
     this.appContentCustomResourcesUrl = this.urlToEditDefault(eavConstants.contentTypes.resources);
     this.appGlobalCustomResourcesUrl = this.urlToEditCustom(eavConstants.contentTypes.customResources);
+    this.appGlobalCustomSettingsUrl = this.urlToEditCustom(eavConstants.contentTypes.customSettings);
   }
 
   ngOnInit() {
