36d12ddca1c308f3dee1e0f1c72e2346ebb7a399
paulpietzko <paul.pietzko@icloud.com>
2024-12-10 11:00:01 +0100

    fix: update formatted address container to input and handle value correctly

--- projects/field-custom-gps/src/shared/helpers.ts
+++ projects/field-custom-gps/src/shared/helpers.ts
@@ -9,8 +9,14 @@ export function buildTemplate(template: string, styles: string): string {
 }
 
 export function parseLatLng(value: string): google.maps.LatLngLiteral {
-  const latLng: google.maps.LatLngLiteral = JSON.parse(value.replace('latitude', 'lat').replace('longitude', 'lng'));
-  return latLng;
+  if (value && value.trim().startsWith('{') && value.trim().endsWith('}')) {
+    const latLng: google.maps.LatLngLiteral = JSON.parse(
+      value.replace('latitude', 'lat').replace('longitude', 'lng')
+    );
+    return latLng;
+  } else {
+    throw new Error('Invalid JSON string');
+  }
 }
 
 export function stringifyLatLng(latLng: google.maps.LatLngLiteral): string {
