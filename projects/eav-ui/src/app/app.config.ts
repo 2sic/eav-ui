b660355efe5de71be165ae13350620f01a128d0f
iJungleboy <daniel.mettler@2sic.com>
2025-03-20 10:51:25 +0100

    update app config to use new HttpClient registration

--- projects/eav-ui/src/app/app.config.ts
+++ projects/eav-ui/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { SxcHttpInterceptorProvider } from '@2sic.com/sxc-angular';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom, inject, Injector, provideAppInitializer } from '@angular/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { BrowserModule } from '@angular/platform-browser';
@@ -23,7 +23,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(
       BrowserModule,
       BrowserAnimationsModule,
-      HttpClientModule,
       TranslateModule.forRoot(),
       TranslateModule.forChild(buildTranslateConfiguration(translateLoaderFactory)),
     ),
@@ -34,10 +33,16 @@ export const appConfig: ApplicationConfig = {
     // But certain dialogs will want to use their own.
     FeaturesService,
 
+    // App Initializer which will convert long initial routes to nice routes
     provideAppInitializer(() => { new AppEntryRouteHandler(inject(Injector)); }),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
+
+    // Activate HTTP client & interceptors.
+    // it also uses 2sxc interceptors from the 2sxc module and some from here
+    provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: SetHeadersInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: HandleErrorsInterceptor, multi: true },
+
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } },
   ],
 };
