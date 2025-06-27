982cb792bc165ec632a551284806c05b6df3ca8c
Dominik Graf <dominik.graf@2sic.com>
2025-06-27 10:16:06 +0200

    comments and fixes

--- projects/eav-ui/src/app/app-administration/data/ux-help-info-card/ux-help.service.ts
+++ projects/eav-ui/src/app/app-administration/data/ux-help-info-card/ux-help.service.ts
@@ -5,22 +5,23 @@ import { computed, Injectable, Signal } from '@angular/core';
 })
 export class UxHelpService {
 
-   checkHelpCard<T>(refresh: Signal<number>, content: Signal<T[]>): Signal<number> {
+  //  Calculates the total height, returns the AG-Grid height, and shows/hides the help card
+  //  Content: number of rows
+  //  Signal<number> – current calculated height of the AG-Grid view
+  helpCardVisibilityHeight<T>(refresh: Signal<number>, content: Signal<T[]>): Signal<number> {
     return computed(() => {
-      refresh(); 
+      refresh();
 
-      const height = document.querySelector('.ag-row')?.clientHeight ?? 0;
-      const agGridHeigh = 64 + ((content()?.length ?? 0) * height);
+      const rowHeight = document.querySelector('.ag-row')?.clientHeight ?? 0;
+      const agGridHeigh = 64 + ((content()?.length ?? 0) * rowHeight);
 
       const helpCard = document.getElementById('ux-help-info-card');
       const helpCardHeight = helpCard?.clientHeight ?? 0;
       const wrapperHeight = document.querySelector('.grid-wrapper-dynamic')?.clientHeight ?? 0;
       const dialogAction = (document.getElementsByTagName('mat-dialog-actions')[0]?.clientHeight ?? 0) + 11;
 
-      if (helpCardHeight + agGridHeigh + dialogAction > wrapperHeight)
-        helpCard?.classList.add('hidden-uxHelpCard');
-      else
-        helpCard?.classList.remove('hidden-uxHelpCard');
+      // Show / remove the help card based on the height of the grid
+      helpCard?.classList[helpCardHeight + agGridHeigh + dialogAction > wrapperHeight ? 'add' : 'remove']('hidden-uxHelpCard');
 
       return agGridHeigh;
     });
