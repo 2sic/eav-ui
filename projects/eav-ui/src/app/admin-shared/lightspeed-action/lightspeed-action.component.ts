a24a3dbdf464cced3b1f70709b0ab50a1f95f9fe
paulpietzko <paul.pietzko@icloud.com>
2025-06-30 16:00:32 +0200

    feat: use custom MatBadgeIcon directive

--- projects/eav-ui/src/app/admin-shared/lightspeed-action/lightspeed-action.component.ts
+++ projects/eav-ui/src/app/admin-shared/lightspeed-action/lightspeed-action.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { LightSpeedInfo } from '../../apps-management/models/LightSpeedInfo';
 import { FeatureNames } from '../../features/feature-names';
 import { FeaturesService } from '../../features/features.service';
+import { MatBadgeIconDirective } from '../../shared/directives/mat-badge-icon.directive';
 import { TippyDirective } from '../../shared/directives/tippy.directive';
 import { LightSpeedActionsParams } from './lightspeed-actions.models';
 
@@ -21,6 +22,7 @@ import { LightSpeedActionsParams } from './lightspeed-actions.models';
         TippyDirective,
         MatIconModule,
         MatBadgeModule,
+        MatBadgeIconDirective,
         MatRippleModule,
     ]
 })
