145d3511446bd8a9916b211d87497f26f014b332
paulpietzko <paul.pietzko@icloud.com>
2025-08-11 15:13:31 +0200

    fix: update cell renderer for content type editing

--- projects/eav-ui/src/app/app-administration/data/data.component.ts
+++ projects/eav-ui/src/app/app-administration/data/data.component.ts
@@ -46,6 +46,7 @@ import { DataItemsComponent } from './data-items/data-items.component';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
+  styleUrl: './data.component.scss',
   imports: [
     MatDialogActions,
     MatFormFieldModule,
@@ -286,8 +287,8 @@ export class DataComponent extends BaseComponent implements OnInit, OnDestroy {
           ...ColumnDefinitions.TextWideMin100,
           field: 'Name',
           cellClass: (p) => `${p.data.EditInfo.DisableEdit ? 'no-outline' : 'primary-action highlight'}`.split(' '),
-          valueGetter: (p: { data: ContentType }) => p.data?.Name,
-          onCellClicked: (p) => this.editContentType(p.data),
+          cellRenderer: (p: { data: ContentType }) =>
+            AgGridHelper.cellLink(this.#urlTo(`${p.data.NameId}/edit`), p.data?.Name),
         },
         {
           ...ColumnDefinitions.TextWideFlex3,
