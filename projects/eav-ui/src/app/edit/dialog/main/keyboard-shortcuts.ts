5aaac21a169dd25d178c9508d197af6379ff34ea
paulpietzko <paul.pietzko@icloud.com>
2025-08-13 14:29:07 +0200

    feat: add keyboard event for ctrl + enter

--- projects/eav-ui/src/app/edit/dialog/main/keyboard-shortcuts.ts
+++ projects/eav-ui/src/app/edit/dialog/main/keyboard-shortcuts.ts
@@ -1,8 +1,26 @@
-
+/**
+ * Checks if the keyboard event is triggered by the Escape key.
+ * @param event KeyboardEvent
+ * @returns boolean
+ */
 export function isEscape(event: KeyboardEvent): boolean {
   return event.key === 'Escape';
 }
 
+/**
+ * Checks if the keyboard event is triggered by Ctrl + S (or Cmd + S on Mac).
+ * @param event KeyboardEvent
+ * @returns boolean
+ */
 export function isCtrlS(event: KeyboardEvent): boolean {
   return (event.key === 's' || event.key === 'S') && (event.ctrlKey || event.metaKey);
+}
+
+/**
+ * Checks if the keyboard event is triggered by Ctrl + Enter (or Cmd + Enter on Mac).
+ * @param event KeyboardEvent
+ * @returns boolean
+ */
+export function isCtrlEnter(event: KeyboardEvent): boolean {
+  return event.key === 'Enter' && (event.ctrlKey || event.metaKey);
 }
