78c9427b93e07b3c4971ca0b8cf6ffe9ed5047bf
paulpietzko <paul.pietzko@icloud.com>
2025-08-15 09:15:20 +0200

    refactor: save-close button components to extend base directive for less redundancy

---
+++ projects/eav-ui/src/app/shared/modules/save-close-button/save-close-button-base.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, input, output } from '@angular/core';
+
+// 2pp: needs to be a directive in order to make input, output functions work
+@Directive()
+export abstract class SaveCloseButtonBase {
+  /** Button label, can be a translation key or plain text */
+  label = input<string>('Form.Buttons.SaveAndClose');
+
+  /** Material icon name to show */
+  icon = input<string>('done');
+
+  /** Classes to apply to the button */
+  ngClass = input<string>('');
+
+  /** If true, disables the button. Can also be a function returning boolean */
+  disabled = input<boolean | (() => boolean)>(false);
+
+  /** Emits when the button is clicked */
+  action = output<Event>();
+
+  /** Derived property for disabled state */
+  get isDisabled(): boolean {
+    return typeof this.disabled === 'function' ? !!this.disabled() : !!this.disabled;
+  }
+
+  onClick(event: Event) {
+    if (!this.isDisabled) {
+      this.action.emit(event);
+    }
+  }
+}
--- projects/eav-ui/src/app/shared/modules/save-close-button/save-close-button.component.ts
+++ projects/eav-ui/src/app/shared/modules/save-close-button/save-close-button.component.ts
@@ -1,9 +1,10 @@
 import { NgClass } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { ExtendedFabSpeedDialImports } from '../extended-fab-speed-dial/extended-fab-speed-dial.imports';
+import { SaveCloseButtonBase } from './save-close-button-base.directive';
 
 @Component({
   selector: 'app-save-close-button',
@@ -17,32 +18,4 @@ import { ExtendedFabSpeedDialImports } from '../extended-fab-speed-dial/extended
   templateUrl: './save-close-button.component.html',
   styleUrls: ['./save-close-button.component.scss'],
 })
-export class SaveCloseButtonComponent {
-  /** Button label, can be a translation key or plain text */
-  label = input<string>('Form.Buttons.SaveAndClose');
-
-  /** Material icon name to show */
-  icon = input<string>('done');
-
-  /** Classes to apply to the button */
-  ngClass = input<string>('');
-
-  /** If true, disables the button. Can also be a function returning boolean */
-  disabled = input<boolean | (() => boolean)>(false);
-
-  /** Emits when the button is clicked */
-  action = output<Event>();
-
-  /** Derived property for disabled state */
-  get isDisabled(): boolean {
-    return typeof this.disabled === 'function' ? !!this.disabled() : !!this.disabled;
-  }
-
-  onClick(event: Event) {
-    if (!this.isDisabled) {
-      this.action.emit(event);
-    }
-    // Optionally blur to prevent double submit
-    (event.target as HTMLElement).blur();
-  }
-}
+export class SaveCloseButtonComponent extends SaveCloseButtonBase {}
