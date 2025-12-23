0cb77ae711a6ac6e6c7e5c3d25fe6494a396baa8
iJungleboy <daniel.mettler@2sic.com>
2025-12-23 13:51:52 +0100

    minor rename test files

--- projects/eav-ui/src/app/edit/shared/models/eav/eav-field-value.spec.ts
+++ projects/eav-ui/src/app/edit/shared/models/eav/eav-field-value.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { EavFieldValue } from './eav-field-value';
-import { EavFieldValueConvertTests, EavFieldValueCreateTests, TestDataFieldValueCreate } from './eav-field-value.ts.data';
+import { EavFieldValueConvertTests, EavFieldValueCreateTests, TestDataFieldValueCreate } from './eav-field-value.spec.data';
 
 describe('EavValue.Create', () => {
   TestDataFieldValueCreate.forEach(({ title, value, dimensions, expected }) => {
