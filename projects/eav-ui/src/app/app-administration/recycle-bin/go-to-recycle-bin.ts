2f929a10f824f6e8460c981d9cc81b6c6397fa8a
iJungleboy <daniel.mettler@2sic.com>
2026-02-22 11:38:37 +0100

    recycle bin - add feature-required indicator to menu

--- projects/eav-ui/src/app/app-administration/recycle-bin/go-to-recycle-bin.ts
+++ projects/eav-ui/src/app/app-administration/recycle-bin/go-to-recycle-bin.ts
@@ -1,13 +1,16 @@
 export class GoToRecycleBin {
   static route = 'recycle-bin';
 
+  static featureId = 'EntityUndelete';
+
   static routeDefinition(part: string) {
     return {
       name: 'Recycle Bin (Beta)',
       icon: 'delete',
       svgIcon: false,
       path: `${part}-${GoToRecycleBin.route}`,
-      tippy: 'Recycle Bin to undelete data'
+      tippy: 'Recycle Bin to undelete data',
+      featureRequired: GoToRecycleBin.featureId,
     };
   }
 }
