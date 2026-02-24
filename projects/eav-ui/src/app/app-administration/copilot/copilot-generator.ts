6309d89af2cb0ad7871247ad2e34b08626db2725
Tonći Vatavuk <tonci@sistemi.hr>
2026-02-24 22:11:59 +0100

    update data source reference to 'System.AppEditions', fix https://github.com/2sic/2sxc/issues/3732

--- projects/eav-ui/src/app/app-administration/copilot/copilot-generator.ts
+++ projects/eav-ui/src/app/app-administration/copilot/copilot-generator.ts
@@ -85,10 +85,10 @@ export class CopilotGeneratorComponent {
           cellRendererParams: {
             do: (verb: 'generate' | 'delete', item: any) => {
               switch (verb) {
-                case 'generate': 
+                case 'generate':
                 this.generateForConfiguration(item); break;
-                
-                case 'delete': 
+
+                case 'delete':
                 this.deleteConfiguration(item); break;
               }
             }
@@ -101,10 +101,10 @@ export class CopilotGeneratorComponent {
         }
         if (event.colDef.headerName === 'Actions') {
 
-          if (event.event.target?.dataset?.action === 'generate') 
+          if (event.event.target?.dataset?.action === 'generate')
             this.generateForConfiguration(event.data);
-          
-          if (event.event.target?.dataset?.action === 'delete') 
+
+          if (event.event.target?.dataset?.action === 'delete')
             this.deleteConfiguration(event.data);
         }
       },
@@ -139,8 +139,8 @@ export class CopilotGeneratorComponent {
     })),
     streams: '*', // All streams
   });
-  
-  generators = computed(() => 
+
+  generators = computed(() =>
     // 2dm: had to temporarily move the outputType filter to here, as we can't use OData till I fix something
     this.#data.value()?.default.filter(g => g.outputType === this.outputType())
       ?? []  // may still be loading...
@@ -165,7 +165,7 @@ export class CopilotGeneratorComponent {
   });
 
   #editions = this.#dataSvc.get<{ name: string; label: string; description: string, isDefault: boolean }>({
-    source: 'ToSic.Sxc.DataSources.AppEditions',
+    source: 'System.AppEditions',
   });
 
   editions = computed(() => this.#editions().map(e => ({
