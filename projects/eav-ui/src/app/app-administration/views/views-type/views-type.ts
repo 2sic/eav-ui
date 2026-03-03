c8875af2b48c3a48084f5b6b3c25a8c16355cdec
giovi <npro9021@gmail.com>
2026-03-03 11:08:20 +0100

    fix: added AgGridActionBaseComponent to ViewsTypeComponent

--- projects/eav-ui/src/app/app-administration/views/views-type/views-type.ts
+++ projects/eav-ui/src/app/app-administration/views/views-type/views-type.ts
@@ -1,30 +1,23 @@
-import { ICellRendererAngularComp } from '@ag-grid-community/angular';
-import { ICellRendererParams } from '@ag-grid-community/core';
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AgGridActionsBaseComponent } from '../../../shared/ag-grid/ag-grid-actions-base';
 import { TippyDirective } from '../../../shared/directives/tippy.directive';
 import { View } from '../../models';
 import { calculateViewType } from '../views.helpers';
 
 @Component({
-    selector: 'app-views-type',
-    templateUrl: './views-type.html',
-    imports: [MatIconModule, TippyDirective,]
+  selector: 'app-views-type',
+  templateUrl: './views-type.html',
+  imports: [MatIconModule, TippyDirective],
 })
-export class ViewsTypeComponent implements ICellRendererAngularComp {
-  value: string;
-  icon: string;
-  isShared: boolean;
+export class ViewsTypeComponent extends AgGridActionsBaseComponent<View, 'noop'> {
 
-  agInit(params: ICellRendererParams) {
-    this.value = params.value;
-    const view: View = params.data;
-    const type = calculateViewType(view);
-    this.icon = type.icon;
-    this.isShared = view.IsShared;
-  }
+  /** what ag-grid used to pass as `params.value` */
+  protected readonly value = computed(() => this.params?.value as string);
 
-  refresh(params?: any): boolean {
-    return true;
-  }
+  protected readonly type = computed(() => calculateViewType(this.data));
+
+  protected readonly icon = computed(() => this.type().icon);
+
+  protected readonly isShared = computed(() => !!this.data?.IsShared);
 }
