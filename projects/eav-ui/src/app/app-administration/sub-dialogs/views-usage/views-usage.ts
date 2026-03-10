1b9002a4765d2f02bcdced333c3ba109eeb3f327
giovi <npro9021@gmail.com>
2026-03-10 15:20:53 +0100

    fix: fix: adjusted AG-Grid actions for views-usage-id

--- projects/eav-ui/src/app/app-administration/sub-dialogs/views-usage/views-usage.ts
+++ projects/eav-ui/src/app/app-administration/sub-dialogs/views-usage/views-usage.ts
@@ -1,37 +1,38 @@
 import { GridOptions } from '@ag-grid-community/core';
 import { Component, computed } from '@angular/core';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
-import { defaultGridOptions } from '../../../shared/constants/default-grid-options.constants';
-import { ViewsService } from '../../services/views.service';
-// tslint:disable-next-line:max-line-length
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { transient } from '../../../../../../core';
 import { ColumnDefinitions } from '../../../shared/ag-grid/column-definitions';
+import { defaultGridOptions } from '../../../shared/constants/default-grid-options.constants';
 import { TippyDirective } from '../../../shared/directives/tippy.directive';
 import { SxcGridModule } from '../../../shared/modules/sxc-grid-module/sxc-grid.module';
-import { blockIdValueGetter, moduleIdClassGetter, moduleIdValueGetter, nameClassGetter, onNameClicked, pageIdClassGetter, pageIdValueGetter, statusCellRenderer } from './views-usage-grid.helpers';
-import { ViewsUsageIdComponent } from './views-usage-id/views-usage-id';
+import { ClipboardService } from '../../../shared/services/clipboard.service';
+import { ViewsService } from '../../services/views.service';
+import { blockIdValueGetter, moduleIdClassGetter, moduleIdValueGetter, nameClassGetter, onNameClicked, pageIdClassGetter, pageIdValueGetter, statusCellRenderer, } from './views-usage-grid.helpers';
+import { ViewsUsageIdActions, ViewsUsageIdComponent, } from './views-usage-id/views-usage-id';
 import { ViewsUsageStatusFilterComponent } from './views-usage-status-filter/views-usage-status-filter';
 import { buildData } from './views-usage.helpers';
 
 @Component({
-    selector: 'app-views-usage',
-    templateUrl: './views-usage.html',
-    imports: [
-        MatButtonModule,
-        MatIconModule,
-        RouterOutlet,
-        SxcGridModule,
-        TippyDirective,
-        MatDialogModule,
-    ]
+  selector: 'app-views-usage',
+  templateUrl: './views-usage.html',
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet,
+    SxcGridModule,
+    TippyDirective,
+    MatDialogModule,
+  ],
 })
 export class ViewsUsageComponent {
   gridOptions = this.buildGridOptions();
 
   private viewsService = transient(ViewsService);
+  private clipboard = transient(ClipboardService);
 
   constructor(
     private dialog: MatDialogRef<ViewsUsageComponent>,
@@ -44,24 +45,38 @@ export class ViewsUsageComponent {
 
   view = computed(() => {
     const usage = this.#usage();
-    if (usage === undefined)
+    if (usage === undefined) {
       return undefined;
+    }
 
     return {
       viewUsageName: usage[0]?.Name,
       viewTooltip: `ID: ${usage[0].Id}\nGUID: ${usage[0].Guid}`,
       data: buildData(usage[0]),
-    }
-  }
-  );
+    };
+  });
 
-
-  closeDialog() {
+  closeDialog(): void {
     this.dialog.close();
   }
 
+  private onIdAction(
+    verb: ViewsUsageIdActions,
+    data: {
+      Block?: string;
+      Module?: string;
+      Page?: string;
+    },
+  ): void {
+    switch (verb) {
+      case 'copy':
+        this.clipboard.copyToClipboard(String(data));
+        break;
+    }
+  }
+
   private buildGridOptions(): GridOptions {
-    const gridOptions: GridOptions = {
+    return {
       ...defaultGridOptions,
       columnDefs: [
         {
@@ -69,6 +84,9 @@ export class ViewsUsageComponent {
           field: 'Block',
           valueGetter: blockIdValueGetter,
           cellRenderer: ViewsUsageIdComponent,
+          cellRendererParams: {
+            do: (verb, data) => this.onIdAction(verb, data),
+          } satisfies ViewsUsageIdComponent['params'],
         },
         {
           ...ColumnDefinitions.Number,
@@ -77,6 +95,9 @@ export class ViewsUsageComponent {
           valueGetter: moduleIdValueGetter,
           cellClass: moduleIdClassGetter,
           cellRenderer: ViewsUsageIdComponent,
+          cellRendererParams: {
+            do: (verb, data) => this.onIdAction(verb, data),
+          } satisfies ViewsUsageIdComponent['params'],
         },
         {
           ...ColumnDefinitions.Number,
@@ -84,6 +105,9 @@ export class ViewsUsageComponent {
           valueGetter: pageIdValueGetter,
           cellClass: pageIdClassGetter,
           cellRenderer: ViewsUsageIdComponent,
+          cellRendererParams: {
+            do: (verb, data) => this.onIdAction(verb, data),
+          } satisfies ViewsUsageIdComponent['params'],
         },
         {
           ...ColumnDefinitions.TextWide,
@@ -106,6 +130,5 @@ export class ViewsUsageComponent {
         },
       ],
     };
-    return gridOptions;
   }
 }
