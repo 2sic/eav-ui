3701388da550aa57b03b5fecf2ed6ae9ac35d736
giovi <npro9021@gmail.com>
2026-03-11 09:30:08 +0100

    fix: adjusted AG-Grid actions for content-items-status

--- projects/eav-ui/src/app/content-items/content-items-status/content-items-status.ts
+++ projects/eav-ui/src/app/content-items/content-items-status/content-items-status.ts
@@ -1,63 +1,61 @@
-import { ICellRendererAngularComp } from '@ag-grid-community/angular';
-import { ICellRendererParams } from '@ag-grid-community/core';
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { EavForInAdminUi } from '../../edit/shared/models/eav';
+import { AgGridCellRendererBaseComponent } from '../../shared/ag-grid/ag-grid-cell-renderer-base';
 import { TippyDirective } from '../../shared/directives/tippy.directive';
 import { ContentItem } from '../models/content-item.model';
 import { PubMeta } from '../pub-meta-filter/pub-meta-filter.model';
 
-type GoToUrls = 'openMetadata'
-
 @Component({
-    selector: 'app-content-items-status',
-    templateUrl: './content-items-status.html',
-    styleUrls: ['./content-items-status.scss'],
-    imports: [
-        MatIconModule,
-        MatRippleModule,
-        NgClass,
-        MatBadgeModule,
-        TippyDirective,
-    ]
+  selector: 'app-content-items-status',
+  templateUrl: './content-items-status.html',
+  styleUrls: ['./content-items-status.scss'],
+  imports: [
+    MatIconModule,
+    MatRippleModule,
+    NgClass,
+    MatBadgeModule,
+    TippyDirective,
+  ]
 })
-export class ContentItemsStatusComponent implements ICellRendererAngularComp {
-  value: PubMeta;
-  disableMetadata: boolean;
-  metadataCount: number;
-  metadataTooltip: string;
+export class ContentItemsStatusComponent
+  extends AgGridCellRendererBaseComponent<ContentItem, PubMeta, ContentItemsStatusParams> {
 
-  protected item: ContentItem;
+  get item(): ContentItem {
+    return this.data;
+  }
 
-  public params: {
-    urlTo(verb: GoToUrls, item: ContentItem): string;
-  };
+  get disableMetadata(): boolean {
+    return this.item._EditInfo.DisableMetadata;
+  }
 
-  agInit(params: ICellRendererParams & ContentItemsStatusComponent['params']): void {
-    this.params = params;
-    this.value = params.value;
-    this.item = params.data;
-    this.disableMetadata = this.item._EditInfo.DisableMetadata;
+  get metadataCount(): number {
+    return this.item.Metadata?.length ?? 0;
+  }
 
-    this.metadataCount = this.item.Metadata?.length ?? 0;
-
-    const mdf = this.item.For as EavForInAdminUi;
-    this.metadataTooltip = mdf
+  get metadataTooltip(): string {
+    const metadataFor = this.item.For as EavForInAdminUi;
+    let tooltip = metadataFor
       ? 'This item is metadata for:'
-      + `\nTarget: ${mdf.Target}`
-      + `\nTargetType: ${mdf.TargetType}`
-      + (mdf.KeyNumber ? `\nNumber: ${mdf.KeyNumber}` : '')
-      + (mdf.KeyString ? `\nString: ${mdf.KeyString}` : '')
-      + (mdf.KeyGuid ? `\nGuid: ${mdf.KeyGuid}` : '')
-      + (mdf.Title ? `\nTitle: ${mdf.Title}` : '')
+        + `\nTarget: ${metadataFor.Target}`
+        + `\nTargetType: ${metadataFor.TargetType}`
+        + (metadataFor.KeyNumber ? `\nNumber: ${metadataFor.KeyNumber}` : '')
+        + (metadataFor.KeyString ? `\nString: ${metadataFor.KeyString}` : '')
+        + (metadataFor.KeyGuid ? `\nGuid: ${metadataFor.KeyGuid}` : '')
+        + (metadataFor.Title ? `\nTitle: ${metadataFor.Title}` : '')
       : 'This item is not metadata.';
-    this.metadataTooltip += `\n\nThis item has ${this.metadataCount > 0 ? this.metadataCount : 'no'} other metadata ${this.metadataCount === 1 ? 'item' : 'items'} attached to it.`;
-  }
 
-  refresh(params?: any): boolean {
-    return true;
+    tooltip += `\n\nThis item has ${this.metadataCount > 0 ? this.metadataCount : 'no'} other metadata ${this.metadataCount === 1 ? 'item' : 'items'} attached to it.`;
+
+    return tooltip;
   }
+}
+
+export type ContentItemsStatusVerb = 'openMetadata';
+
+export interface ContentItemsStatusParams {
+  urlTo(verb: ContentItemsStatusVerb, item: ContentItem): string;
 }
