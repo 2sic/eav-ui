0c463e6ebe92528576cf455559791f3857faa834
giovi <npro9021@gmail.com>
2026-04-24 08:54:12 +0200

    Recovered getLanguagesPermissionsLive

--- projects/eav-ui/src/app/apps-management/services/zone.service.ts
+++ projects/eav-ui/src/app/apps-management/services/zone.service.ts
@@ -8,6 +8,7 @@ import { SystemInfoSet } from '../models/system-info.model';
 
 const webApiZoneRootSwitchLanguage = 'admin/zone/SwitchLanguage';
 const webApiZoneRootGetSystemInfo = 'admin/zone/GetSystemInfo';
+const webApiAppRootLanguages = 'admin/app/languages';
 
 @Injectable()
 export class ZoneService extends HttpServiceBaseSignal {
@@ -38,10 +39,12 @@ export class ZoneService extends HttpServiceBaseSignal {
   }
 
   getLanguagesPermissionsLive(refresh: Signal<unknown>) {
-    return this.#sysData.get<SiteLanguagePermissions>({
-      refresh,
-      source: 'System.ZoneLanguages',
-      noCamel: true,
+    return httpResource<SiteLanguagePermissions[]>(() => {
+      refresh();
+      return ({
+        url: this.apiUrl(webApiAppRootLanguages),
+        params: { appId: this.appId }
+      });
     });
   }
 }
